ae40349e4e4afb5388927ec6c20f10390ab35466
Liudmyla Ilchenko <liudmyla.ilchenko7@gmail.com>
2020-07-13 14:10:40 +0300

    added table of people

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+people.forEach((person) => {
+  // insert into <tbody>
+  table.firstElementChild.insertAdjacentHTML(
+    'beforeend',
+    `<tr>
+      <td>${person.name}</td>
+      <td>${person.sex}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>`
+  );
+});
