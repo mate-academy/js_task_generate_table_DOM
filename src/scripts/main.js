82185ed4439bada1d48a01409fac36a8c7568591
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-08-13 14:25:55 +0300

    Change Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,45 +2,38 @@
 
 const people = require('./lib/people');
 
-function createStrings() {
-  const tableTree = document.querySelector('.dashboard');
+const tableTree = document.querySelector('.dashboard');
 
-  for (let i = 0; i < people.length; i++) {
-    const createTr = document.createElement('tr');
-    let createTd = document.createElement('td');
-    const age = people[i].died - people[i].born;
-    const century = Math.floor(people[i].died / 100);
+for (let i = 0; i < people.length; i++) {
+  const createTr = document.createElement('tr');
 
-    for (const key in people[i]) {
-      if (key === 'sex' && people[i][key] === 'm') {
-        createTd = document.createElement('td');
-        createTd.append('Male');
-        createTr.append(createTd);
-      } else if (key === 'sex' && people[i][key] === 'f') {
-        createTd = document.createElement('td');
-        createTd.append('Female');
-        createTr.append(createTd);
-      } else {
-        createTd = document.createElement('td');
-        createTd.append(people[i][key]);
-        createTr.append(createTd);
-      }
-
-      if (key === 'died') {
-        createTd = document.createElement('td');
-        break;
-      }
+  for (const key in people[i]) {
+    if (key === 'slug') {
+      break;
     }
 
-    createTd.append(age);
-    createTr.append(createTd);
+    const createTd = document.createElement('td');
 
-    createTd = document.createElement('td');
-    createTd.append(century);
-    createTr.append(createTd);
+    switch (key) {
+      case 'name':
+      case 'born':
+      case 'died':
+        createTd.textContent = people[i][key];
+        break;
 
-    tableTree.append(createTr);
+      case 'sex':
+        createTd.textContent = people[i][key] === 'f' ? 'Female' : 'Male';
+        break;
+
+      case 'fatherName':
+        createTd.textContent = people[i].died - people[i].born;
+        break;
+
+      case 'motherName':
+        createTd.textContent = Math.floor(people[i].died / 100);
+        break;
+    }
+    createTr.append(createTd);
   }
+  tableTree.append(createTr);
 }
-
-createStrings();
