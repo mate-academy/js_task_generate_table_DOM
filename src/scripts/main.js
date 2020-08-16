4d49a9a99bef824b4d995a2d59b96e102d761a3f
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-16 18:24:21 +0300

    optimised

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,15 @@
 
 const people = require('./lib/people');
 
-const table = document.querySelector('.dashboard tbody');
-
 for (const i of people) {
-  table.insertAdjacentHTML('beforeend',
-    `<tr>
-      <th>${i.name}</th>
-      <th>${i.sex}</th>
-      <th>${i.born}</th>
-      <th>${i.died}</th>
-      <th>${i.died - i.born}</th>
-      <th>${Math.ceil(i.died / 100)}</th>
-    </tr>`);
+  document.querySelector('.dashboard tbody')
+    .insertAdjacentHTML('beforeend',
+      `<tr>
+        <th>${i.name}</th>
+        <th>${i.sex === 'm' ? 'Male' : 'Female'}</th>
+        <th>${i.born}</th>
+        <th>${i.died}</th>
+        <th>${i.died - i.born}</th>
+        <th>${Math.ceil(i.died / 100)}</th>
+      </tr>`);
 };
