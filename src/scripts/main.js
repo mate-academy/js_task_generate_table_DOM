98de5276368aaa295a8b91141f1fbd33bb9437a5
Serhii Antonyshyn <serginio4422@gmail.com>
2020-08-20 21:46:50 +0300

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,35 +11,38 @@ for (let i = 0; i < people.length; i++) {
 
   list.append(tr);
 
-  const td1 = document.createElement('td');
+  for (const key in people[i]) {
+    const td1 = document.createElement('td');
 
-  td1.textContent = people[i].name;
-  tr.append(td1);
+    switch (key) {
+      case 'name':
+        td1.textContent = people[i].name;
+        tr.append(td1);
+        break;
+      case 'sex':
+        td1.textContent = people[i].sex;
+        tr.append(td1);
+        break;
+      case 'born':
+        td1.textContent = people[i].born;
+        tr.append(td1);
+        break;
+      case 'died':
+        td1.textContent = people[i].died;
+        tr.append(td1);
+        break;
+    }
+  }
 
   const td2 = document.createElement('td');
 
-  td2.textContent = people[i].sex;
+  td2.textContent = people[i].died - people[i].born;
   tr.append(td2);
 
   const td3 = document.createElement('td');
 
-  td3.textContent = people[i].born;
+  td3.textContent = Math.ceil(people[i].died / 100);
   tr.append(td3);
-
-  const td4 = document.createElement('td');
-
-  td4.textContent = people[i].died;
-  tr.append(td4);
-
-  const td5 = document.createElement('td');
-
-  td5.textContent = people[i].died - people[i].born;
-  tr.append(td5);
-
-  const td6 = document.createElement('td');
-
-  td6.textContent = Math.ceil(people[i].died / 100);
-  tr.append(td6);
 }
 
 // write your code here
