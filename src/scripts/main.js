9270f2de5582d6f42c84080680d6c4b517053726
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-08-20 15:05:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,16 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+table.innerHTML += (
+  people.map(person => `
+  <tr>
+    <th>${person.name}</th>
+    <th>${person.sex}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
+  </tr>
+  `).join(''));
