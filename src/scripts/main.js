52422689bb1bb0d957f81b09a680558c18fd8ccb
Elena-brv <leena.brv@gmail.com>
2020-08-21 23:38:34 +0300

    table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,27 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => {
+  const personInfo = [
+    person.name,
+    (person.sex === 'm') ? 'Male' : 'Female',
+    person.born,
+    person.died,
+    person.died - person.born,
+    Math.ceil(person.died / 100),
+  ];
+
+  const row = document.createElement('tr');
+
+  for (const info of personInfo) {
+    const cell = document.createElement('td');
+
+    cell.textContent = info;
+
+    row.append(cell);
+  }
+
+  table.append(row);
+});
