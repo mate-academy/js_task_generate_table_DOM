7ccfd2eb9de0a706c24d28b2db2fabbfd38905f3
nanccyy <lidiapriymak@gmail.com>
2020-08-24 00:42:42 +0800

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,25 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableBody = document.querySelector('tbody');
 
-// write your code here
+people.map(person => {
+  tableBody.insertAdjacentHTML('beforeend', `
+    <tr>
+      <th>${person.name}</th>
+      <th>${isFullWordForGender(person.sex)}</th>
+      <th>${person.born}</th>
+      <th>${person.died}</th>
+      <th>${person.died - person.born}</th>
+      <th>${Math.ceil(person.died / 100)}</th>
+   </tr>
+   `);
+});
+
+function isFullWordForGender(input) {
+  return input === 'm'
+    ? 'Male'
+    : input === 'f'
+      ? 'Female'
+      : input;
+}
