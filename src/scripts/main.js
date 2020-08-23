80ce64e4574eee3adf2b20054182baf03cc5590b
nanccyy <lidiapriymak@gmail.com>
2020-08-24 00:57:39 +0800

    fixed extra code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,12 @@ const tableBody = document.querySelector('tbody');
 people.map(person => {
   tableBody.insertAdjacentHTML('beforeend', `
     <tr>
-      <th>${person.name}</th>
-      <th>${person.sex === 'm' ? 'Male' : 'Female'}</th>
-      <th>${person.born}</th>
-      <th>${person.died}</th>
-      <th>${person.died - person.born}</th>
-      <th>${Math.ceil(person.died / 100)}</th>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
    </tr>
    `);
 });
