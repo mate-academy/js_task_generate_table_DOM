176444367a1b040c3c388d3d84f090748004aca8
martakupka <marta-k2008@ukr.net>
2020-08-23 23:56:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,32 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.map(person => {
+  delete person.fatherName;
+  delete person.motherName;
+  delete person.slug;
 
-// write your code here
+  if (person.sex === 'm') {
+    person.sex = 'male';
+  } else {
+    person.sex = 'female';
+  }
+
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+});
+
+const table = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const tableRow = document.createElement('tr');
+
+  for (const element in person) {
+    const tableCell = document.createElement('td');
+
+    tableCell.innerText = person[element];
+    tableRow.append(tableCell);
+  }
+
+  table.append(tableRow);
+}
