8eca40bfc4e14c79a49897dc5b75cf449426364c
Anastasiia Lukina <anlukina24@gmail.com>
2020-08-24 22:13:11 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.map(person =>
+  table.firstElementChild.insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${person.name}</td>
+      <td>${checkGender(person.sex)}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `));
 
-// write your code here
+function checkGender(gender) {
+  return gender === 'm'
+    ? 'Male'
+    : 'Female';
+}
