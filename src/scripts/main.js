d7b767ddfa29ae05a55354435b095b41247036a5
Roman <romikkus@gmail.com>
2020-08-25 18:06:28 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,26 +5,27 @@ const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
 for (let i = 0; i < people.length; i++) {
-  table.insertRow().insertAdjacentHTML('afterbegin', `
+  table.insertRow().insertAdjacentHTML(
+    'afterbegin', `
     <tr>
-        <td>
-            ${people[i].name}
-        </td>
-        <td>
-            ${people[i].sex === 'm' ? 'Male' : 'Female'}
-        </td>
-        <td>
-            ${people[i].born}
-        </td>
-        <td>
-            ${people[i].died}
-        </td>
-        <td>
-            ${people[i].died - people[i].born}
-        </td>
-        <td>
-            ${Math.ceil(people[i].died / 100)}
-        </td>
+      <td>
+        ${people[i].name}
+      </td>
+      <td>
+        ${people[i].sex === 'm' ? 'Male' : 'Female'}
+      </td>
+      <td>
+        ${people[i].born}
+      </td>
+      <td>
+        ${people[i].died}
+      </td>
+      <td>
+        ${people[i].died - people[i].born}
+      </td>
+      <td>
+        ${Math.ceil(people[i].died / 100)}
+      </td>
     </tr>
   `);
 }
