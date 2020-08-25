6b713687247e84b17a5d7b8884e5c3a5f84a64d5
Oleg <mvp.westbrook4k@gmail.com>
2020-08-25 15:27:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+people.map(person => {
+  table.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  </tr>
+  `);
+});
