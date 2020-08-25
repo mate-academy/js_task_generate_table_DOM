2619d78e2e29e90ef1d19f2927f32899e843b3be
RusTron <rusan.tron@gmail.com>
2020-08-25 20:42:31 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,37 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+const tableHead = document.querySelectorAll('th');
+
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  row.className = `${person.name}`;
+  table.append(row);
+
+  for (let j = 0; j < tableHead.length; j++) {
+    const cell = document.createElement('td');
+
+    switch (tableHead[j].innerText) {
+      case 'Age':
+        cell.innerText = person.died - person.born;
+        row.append(cell);
+        break;
+      case 'Century':
+        cell.innerText = Math.floor(person.died / 100);
+        row.append(cell);
+        break;
+      case 'Gender' :
+        person.sex === 'm'
+          ? cell.innerText = 'Male'
+          : cell.innerText = 'Famale';
+        row.append(cell);
+        break;
+      default:
+        cell.innerText = person[tableHead[j].innerText.toLowerCase()];
+        row.append(cell);
+    }
+  }
+}
