57beb75016c562537d542d715077e0f82077d2ca
Anatasia <krajnova.stasy@gmail.com>
2020-08-25 14:23:48 +0300

    some fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,19 @@
 
 const people = require('./lib/people');
 
-const dashboard = document.querySelector('.dashboard');
+const dashboard = document.querySelector('.dashboard tbody');
 
 people.map(person => {
   const { name, sex, born, died } = person;
 
   dashboard.insertAdjacentHTML('beforeend',
     `<tr>
-      <th>${name}</th>
-      <th>${sex}</th>
-      <th>${born}</th>
-      <th>${died}</th>
-      <th>${died - born}</th>
-      <th>${Math.ceil(died / 100)}</th>
+      <td>${name}</td>
+      <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${died - born}</td>
+      <td>${Math.ceil(died / 100)}</td>
     </tr>`
   );
 });
