fb7d1b495268427fe668698c8493cf47bccec9a4
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-26 13:54:08 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,20 @@
 'use strict';
 
 const people = require('./lib/people');
-const table = document.querySelector('.dashboard');
+const table = document.querySelector('tr');
 
 for (const person of people) {
-  const row = document.createElement('tr');
-  const name = document.createElement('td');
-  const gender = document.createElement('td');
-  const born = document.createElement('td');
-  const died = document.createElement('td');
-  const age = document.createElement('td');
-  const century = document.createElement('td');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
 
-  name.innerText = person.name;
-  gender.innerText = person.sex === 'f' ? 'Female' : 'Male';
-  born.innerText = person.born;
-  died.innerText = person.died;
-  age.innerText = person.died - person.born;
-  century.innerText = Math.ceil(person.died / 100);
-
-  row.append(name, gender, born, died, age, century);
-  table.append(row);
+  table.insertAdjacentHTML('afterend', `
+    <tr>
+      <td>${person.name}</td>
+      <td>${(person.sex === 'm') ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `);
 }
