5ad2f012aa294d6e674d03d851369f4317f1ae70
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-26 11:34:30 +0300

    fix gender

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,16 @@ const people = require('./lib/people');
 
 const table = document.querySelector('.dashboard');
 
+function sex(gender) {
+  return gender === 'm' ? 'Male' : 'Female';
+}
+
 for (const human of people) {
   const line = document.createElement('tr');
 
   line.innerHTML = `
     <td>${human.name}</td>
-    <td>${human.sex}</td>
+    <td>${sex(human.sex)}</td>
     <td>${human.born}</td>
     <td>${human.died}</td>
     <td>${human.died - human.born}</td>
