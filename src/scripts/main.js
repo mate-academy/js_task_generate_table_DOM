63cc899b4411624539437dd08893d88db3464d64
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-26 10:37:30 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,19 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const human of people) {
+  const line = document.createElement('tr');
+
+  line.innerHTML = `
+    <td>${human.name}</td>
+    <td>${human.sex}</td>
+    <td>${human.born}</td>
+    <td>${human.died}</td>
+    <td>${human.died - human.born}</td>
+    <td>${Math.ceil(human.died / 100)}</td>
+  `;
+
+  table.append(line);
+}
