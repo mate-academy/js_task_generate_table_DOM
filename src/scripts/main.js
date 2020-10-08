1d6f89c05e9703d785c0955f3ae3cf79caec70e3
Anastasia Chervona <av.chervona@gmail.com>
2020-10-08 17:11:57 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,33 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.querySelector('.dashboard');
+const columns = [...document.querySelectorAll('th')];
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  for (const column in columns) {
+    const cell = document.createElement('td');
+    const property = columns[column].textContent.toLowerCase();
+
+    if (person.hasOwnProperty(property)) {
+      cell.textContent = person[property];
+    } else {
+      cell.textContent = getProperty(property, person);
+    }
+    row.append(cell);
+  }
+  dashboard.append(row);
+}
+
+function getProperty(property, person) {
+  switch (property) {
+    case 'gender':
+      return person.sex === 'f' ? 'Female' : 'Male';
+    case 'age':
+      return person.died - person.born;
+    case 'century':
+      return Math.ceil(person.died / 100);
+  }
+}
