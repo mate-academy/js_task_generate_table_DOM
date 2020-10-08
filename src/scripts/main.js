70cbb5ccca39c90856fccd6895ac70f39c79a134
Yazheviks <vicsergeyeva@gmail.com>
2020-10-08 12:55:24 +0300

    better to read

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,6 @@
 
 const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
-const data = ['name', 'sex', 'born', 'died', 'age', 'century'];
 
 people.forEach(person => {
   const row = document.createElement('tr');
@@ -10,18 +9,14 @@ people.forEach(person => {
   person.age = person.died - person.born;
   person.century = Math.ceil(person.died / 100);
 
-  if (person.sex === 'm') {
-    person.sex = 'Male';
-  } else {
-    person.sex = 'Female';
-  }
-
-  for (const dataType of data) {
-    const td = document.createElement('td');
-
-    td.innerText = person[dataType];
-    row.append(td);
-  }
+  row.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.age}</td>
+    <td>${person.century}</td>
+  `;
 
   table.append(row);
 });
