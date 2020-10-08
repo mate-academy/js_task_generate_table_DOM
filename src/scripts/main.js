0fb58eed3167d14c56ff07b02740cc6138c36667
VoduDay <andriy.maniak@gmail.com>
2020-10-08 22:35:40 +0300

    table genereted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+for (const person of people) {
+  const { name, sex, born, died } = person;
+  const tableRow = document.createElement('tr');
+
+  tableRow.innerHTML = `
+    <td>${name}</td>
+    <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${born}</td>
+    <td>${died}</td>
+    <td>${died - born}</td>
+    <td>${Math.ceil(died / 100)}</td>
+    `;
+
+  table.append(tableRow);
+}
