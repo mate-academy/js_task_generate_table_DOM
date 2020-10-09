8dc1301eb3365eb03d073a9835ee12a29e9bfe32
Anastasia Chervona <av.chervona@gmail.com>
2020-10-09 15:35:08 +0300

    function deleted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,32 +3,19 @@
 const people = require('./lib/people');
 
 const dashboard = document.querySelector('.dashboard');
-const columns = [...document.querySelectorAll('th')];
 
 for (const person of people) {
   const row = document.createElement('tr');
+  const { name, sex, born, died } = person;
 
-  for (const column in columns) {
-    const cell = document.createElement('td');
-    const property = columns[column].textContent.toLowerCase();
+  row.innerHTML = `
+    <td>${name}</td>
+    <td>${sex === 'f' ? 'Female' : 'Male'}</td>
+    <td>${born}</td>
+    <td>${died}</td>
+    <td>${died - born}</td>
+    <td>${Math.ceil(died / 100)}</td>
+  `;
 
-    if (person.hasOwnProperty(property)) {
-      cell.textContent = person[property];
-    } else {
-      cell.textContent = getProperty(property, person);
-    }
-    row.append(cell);
-  }
   dashboard.append(row);
-}
-
-function getProperty(property, person) {
-  switch (property) {
-    case 'gender':
-      return person.sex === 'f' ? 'Female' : 'Male';
-    case 'age':
-      return person.died - person.born;
-    case 'century':
-      return Math.ceil(person.died / 100);
-  }
 }
