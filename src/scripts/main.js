eeb0eeeb2c3eb2b8f4540508fbdab05f99a8ec16
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-12 15:06:34 +0300

    solution generate table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,20 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const tr = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  tr.innerHTML = `
+  <td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'male' : 'female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${age}</td>
+  <td>${century}</td>
+  `;
+  table.append(tr);
+});
