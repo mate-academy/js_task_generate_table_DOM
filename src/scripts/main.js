6fedcbe36fec464ff9632d393be51a165c69cb7c
dmitry <akulinind@gmail.com>
2020-10-13 15:41:10 +0300

    add html creation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,21 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
 // write your code here
+function fillInTable(content) {
+  for (const person of content) {
+    const html
+      = '<tr><td>' + person.name + '</td><td>'
+      + person.sex + '</td><td>'
+      + person.died + '</td><td>'
+      + person.born + '</td><td>'
+      + (person.died - person.born) + '</td><td>'
+      + (Math.ceil(person.died / 100)) + '</td></tr>';
+
+    table.append(html);
+  }
+}
+
+fillInTable(people);
