8c54a8addfbdc63d28003679ae1569723e9b3321
dmitry <akulinind@gmail.com>
2020-10-13 16:02:38 +0300

    fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,7 @@ const table = document.querySelector('.dashboard');
 // write your code here
 function fillInTable(content) {
   for (const person of content) {
-    const gender = people.sex === 'm' ? 'Male' : 'Female';
+    const gender = person.sex === 'm' ? 'Male' : 'Female';
     const age = person.died - person.born;
     const century = Math.ceil(person.died / 100);
     const html = `<tr>
@@ -20,7 +20,7 @@ function fillInTable(content) {
         <td>${century}</td>
         </tr>`;
 
-    table.innerHTML = html;
+    table.innerHTML += html;
   }
 }
 
