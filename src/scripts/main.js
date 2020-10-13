7e04439954f2f8e6522f6accdbbb7bdadec9ad1e
dmitry <akulinind@gmail.com>
2020-10-13 16:00:36 +0300

    upd readme and refactor html

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,13 +8,17 @@ const table = document.querySelector('.dashboard');
 // write your code here
 function fillInTable(content) {
   for (const person of content) {
-    const html
-      = '<tr><td>' + person.name + '</td><td>'
-      + person.sex + '</td><td>'
-      + person.died + '</td><td>'
-      + person.born + '</td><td>'
-      + (person.died - person.born) + '</td><td>'
-      + (Math.ceil(person.died / 100)) + '</td></tr>';
+    const gender = people.sex === 'm' ? 'Male' : 'Female';
+    const age = person.died - person.born;
+    const century = Math.ceil(person.died / 100);
+    const html = `<tr>
+        <td>${person.name}</td>
+        <td>${gender}</td>
+        <td>${person.died}</td>
+        <td>${person.born}</td>
+        <td>${age}</td>
+        <td>${century}</td>
+        </tr>`;
 
     table.innerHTML = html;
   }
