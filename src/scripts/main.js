16c2c238a51804bc8f1696e4ab9eed97a56ed897
martakupka <marta-k2008@ukr.net>
2020-10-15 17:56:42 +0300

    Small changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,13 +7,13 @@ const reversedPeople = [...people].reverse();
 
 for (const person of reversedPeople) {
   tableLastRow.insertAdjacentHTML('afterend', `
-  <tr>
-    <td>${person.name}</td>
-    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
-    <td>${person.born}</td>
-    <td>${person.died}</td>
-    <td>${person.died - person.born}</td>
-    <td>${Math.ceil(person.died / 100)}</td>
-  </tr>
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
   `);
 }
