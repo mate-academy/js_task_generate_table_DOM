4618581e4240168120822adb5b9bef2128b37c73
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-10-15 17:32:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboard = document.querySelector('.dashboard tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const row = document.createElement('tr');
 
-// write your code here
+  row.innerHTML = `
+    <th>${person.name}</th>
+    <th>${person.sex === 'f' ? 'Female' : 'Male'}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
+  `;
+
+  dashboard.append(row);
+}
