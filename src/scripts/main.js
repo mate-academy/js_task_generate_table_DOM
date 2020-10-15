1aea5b450eed8112825388a16085ffa15d42b125
Anton <office.sklyarov@gmail.com>
2020-10-15 13:16:12 +0300

    added people data to the table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,41 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const personName = document.createElement('td');
+
+  personName.innerText = person.name;
+
+  const personGender = document.createElement('td');
+
+  personGender.innerText = (person.sex === 'm' ? 'Male' : 'Female');
+
+  const personBorn = document.createElement('td');
+
+  personBorn.innerText = person.born;
+
+  const personDied = document.createElement('td');
+
+  personDied.innerText = person.died;
+
+  const personAge = document.createElement('td');
+
+  personAge.innerText = person.died - person.born;
+
+  const personCentury = document.createElement('td');
+
+  personCentury.innerText = Math.ceil(person.died / 100);
+
+  const newTableRow = document.createElement('tr');
+
+  newTableRow.append(personName);
+  newTableRow.append(personGender);
+  newTableRow.append(personBorn);
+  newTableRow.append(personDied);
+  newTableRow.append(personAge);
+  newTableRow.append(personCentury);
+
+  table.append(newTableRow);
+}
