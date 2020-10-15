352a5616f8c53b6ec93a49e30a544c23d5337067
Anton <office.sklyarov@gmail.com>
2020-10-15 13:27:46 +0300

    code was refactored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,38 +5,19 @@ const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
 for (const person of people) {
-  const personName = document.createElement('td');
-
-  personName.innerText = person.name;
-
-  const personGender = document.createElement('td');
-
-  personGender.innerText = (person.sex === 'm' ? 'Male' : 'Female');
-
-  const personBorn = document.createElement('td');
-
-  personBorn.innerText = person.born;
-
-  const personDied = document.createElement('td');
-
-  personDied.innerText = person.died;
-
-  const personAge = document.createElement('td');
-
-  personAge.innerText = person.died - person.born;
-
-  const personCentury = document.createElement('td');
-
-  personCentury.innerText = Math.ceil(person.died / 100);
-
+  const personGender = (person.sex === 'm' ? 'Male' : 'Female');
+  const personAge = person.died - person.born;
+  const personCentury = Math.ceil(person.died / 100);
   const newTableRow = document.createElement('tr');
 
-  newTableRow.append(personName);
-  newTableRow.append(personGender);
-  newTableRow.append(personBorn);
-  newTableRow.append(personDied);
-  newTableRow.append(personAge);
-  newTableRow.append(personCentury);
+  newTableRow.innerHTML = `
+    <td>${person.name}</td>
+    <td>${personGender}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${personAge}</td>
+    <td>${personCentury}</td>
+  `;
 
   table.append(newTableRow);
 }
