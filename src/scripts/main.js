c7869aad9f6f5d651af60221c166da9becdc56b7
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-10-15 10:26:46 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,15 @@
 const people = require('./lib/people');
 const table = document.querySelector('tbody');
 
-for (const person of people) {
+for (const { name, sex, born, died } of people) {
   table.insertAdjacentHTML('beforeend', `
     <tr>
-      <td>${person.name}</td>
-      <td>${person.sex === 'f' ? 'Female' : 'Male'}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
-      <td>${person.died - person.born}</td>
-      <td>${Math.ceil(person.died / 100)}</td>
+      <td>${name}</td>
+      <td>${sex === 'f' ? 'Female' : 'Male'}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${died - born}</td>
+      <td>${Math.ceil(died / 100)}</td>
     </tr>
   `);
 };
