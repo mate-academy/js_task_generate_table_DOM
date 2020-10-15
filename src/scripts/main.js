604e482c116c10f0fb03fc8fcb76feb78d98d7e0
KirillLutsenko <mnrovski68@gmail.com>
2020-10-15 18:23:26 +0300

    Generate Table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
+const information = ['name', 'sex', 'born', 'died', 'age', 'century'];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const man of people) {
+  const row = document.createElement('tr');
 
-// write your code here
+  for (const info of information) {
+    const manInfo = document.createElement('td');
+
+    manInfo.textContent = man[info];
+
+    if (info === 'sex') {
+      manInfo.textContent = man.sex === 'm'
+        ? 'Male'
+        : 'Female';
+    } else if (info === 'age') {
+      manInfo.textContent = man.died - man.born;
+    } else if (info === 'century') {
+      manInfo.textContent = Math.ceil(man.died / 100);
+    }
+
+    row.append(manInfo);
+  }
+
+  table.append(row);
+}
