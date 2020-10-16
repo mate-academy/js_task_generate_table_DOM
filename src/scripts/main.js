9d0ce22b170c48c25c2d2dc7b2bc6ee80b7b79f3
Liza <elizabethhoncharenko@gmail.com>
2020-10-16 14:41:32 +0300

    fixed solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,6 @@
 
 const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
-const length = document.getElementsByTagName('th').length;
 
 for (let i = 0; i < people.length; i++) {
   const row = document.createElement('tr');
@@ -10,47 +9,14 @@ for (let i = 0; i < people.length; i++) {
   const age = died - born;
   const century = Math.ceil(died / 100);
 
-  for (let j = 0; j < length; j++) {
-    const column = document.createElement('td');
-    let columnText = '';
+  row.innerHTML = `
+    <td>${name}</td>
+    <td>${sex}</td>
+    <td>${born}</td>
+    <td>${died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  `;
 
-    switch (j) {
-      case 0: {
-        columnText = document.createTextNode(name);
-        break;
-      }
-
-      case 1: {
-        columnText = document.createTextNode(sex);
-        break;
-      }
-
-      case 2: {
-        columnText = document.createTextNode(born);
-        break;
-      }
-
-      case 3: {
-        columnText = document.createTextNode(died);
-        break;
-      }
-
-      case 4: {
-        columnText = document.createTextNode(age);
-        break;
-      }
-
-      case 5: {
-        columnText = document.createTextNode(century);
-        break;
-      }
-
-      default: {
-        return 'Error';
-      }
-    }
-    column.append(columnText);
-    row.append(column);
-  }
   table.append(row);
 }
