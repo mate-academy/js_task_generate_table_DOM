612d43636fbd9fb21902dd5f13c2e7d62b224e79
Lancaelot <Lancaelot@gmail.com>
2020-10-16 16:26:37 +0300

    add variables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,17 @@ const dashBoard = document.querySelector('.dashboard');
 
 people.forEach(person => {
   const dashBoardRow = document.createElement('tr');
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
 
   dashBoardRow.innerHTML = `
     <td>${person.name}</td>
-    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${sex}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
-    <td>${person.died - person.born}</td>
-    <td>${Math.ceil(person.died / 100)}</t>
+    <td>${age}</td>
+    <td>${century}</t>
   `;
 
   dashBoard.append(dashBoardRow);
