dc0c3a79dd2229e8d9d532e957f424e095c596db
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-16 15:54:09 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,36 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+function getpeopleFullData() {
+  for (const person of people) {
+    person.age = person.died - person.born;
+    person.century = Math.ceil(person.died / 100);
+  }
+}
+getpeopleFullData();
 
-// write your code here
+function createTableforPeople() {
+  const table = document.querySelector('.dashboard');
+
+  for (const person of people) {
+    const tableRow = document.createElement('tr');
+
+    delete person.motherName;
+    delete person.fatherName;
+    delete person.slug;
+
+    person.sex === 'm' ? person.sex = 'Male' : person.sex = 'Female';
+
+    const personProperties = Object.values(person);
+
+    table.append(tableRow);
+
+    for (let i = 0; i <= personProperties.length - 1; i++) {
+      const tableData = document.createElement('td');
+
+      tableData.innerText = personProperties[i];
+      tableRow.append(tableData);
+    }
+  }
+}
+createTableforPeople();
