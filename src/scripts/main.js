c8523d741306a1db7670a9e6a78495c6436e16b2
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-17 19:55:18 +0300

    generate table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,26 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
+const names = people.map(person => person.name);
+const gender = people.map(person => person.sex === 'm' ? 'Male' : 'Female');
+const bornYear = people.map(person => person.born);
+const dieYear = people.map(person => person.died);
 
-// write your code here
+for (let i = 0; i < people.length; i++) {
+  const row = document.createElement('tr');
+
+  for (let j = 0; j < 6; j++) {
+    const cell = document.createElement('td');
+
+    row.appendChild(cell);
+  }
+
+  table.appendChild(row);
+  row.children[0].innerText = names[i];
+  row.children[1].innerText = gender[i];
+  row.children[2].innerText = bornYear[i];
+  row.children[3].innerText = dieYear[i];
+  row.children[4].innerText = dieYear[i] - bornYear[i];
+  row.children[5].innerText = Math.ceil(dieYear[i] / 100);
+}
