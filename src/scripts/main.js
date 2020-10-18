51c2be49123f1011d8c3b325af4b7a67c5714eeb
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-18 13:26:16 +0300

    code refactored

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,18 @@
 const people = require('./lib/people');
 
 const table = document.querySelector('.dashboard');
-const names = people.map(person => person.name);
-const gender = people.map(person => person.sex === 'm' ? 'Male' : 'Female');
-const bornYear = people.map(person => person.born);
-const dieYear = people.map(person => person.died);
 
-for (let i = 0; i < people.length; i++) {
+people.forEach(({ name, sex, born, died }) => {
   const row = document.createElement('tr');
 
-  for (let j = 0; j < 6; j++) {
-    const cell = document.createElement('td');
+  row.innerHTML = `
+  <td>${name}</td>
+  <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${born}</td>
+  <td>${died}</td>
+  <td>${died - born}</td>
+  <td>${Math.ceil(died / 100)}</td>
+  `;
 
-    row.appendChild(cell);
-  }
-
-  table.appendChild(row);
-  row.children[0].innerText = names[i];
-  row.children[1].innerText = gender[i];
-  row.children[2].innerText = bornYear[i];
-  row.children[3].innerText = dieYear[i];
-  row.children[4].innerText = dieYear[i] - bornYear[i];
-  row.children[5].innerText = Math.ceil(dieYear[i] / 100);
-}
+  table.append(row);
+});
