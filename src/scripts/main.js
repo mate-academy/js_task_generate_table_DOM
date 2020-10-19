ddc52a3ff863a184e137b6c79b40c8d2136a765a
Yurii <yurastt1@gmail.com>
2020-10-19 10:32:59 +0300

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,27 +2,22 @@
 
 const people = require('./lib/people');
 
-const dashboard = document.querySelector('.dashboard');
+const dashboard = document.querySelector('.dashboard').children[0];
 
 people.forEach(person => {
   person.age = person.died - person.born;
   person.century = Math.ceil(person.died / 100);
 
   person.sex = (person.sex === 'm') ? 'Male' : 'Female';
-  delete person.motherName;
-  delete person.fatherName;
-  delete person.slug;
+
+  dashboard.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${person.name}</td>
+    <td>${person.sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.age}</td>
+    <td>${person.century}</td>
+  </tr>
+  `);
 });
-
-for (let i = 0; i < people.length; i++) {
-  const tr = document.createElement('tr');
-
-  for (const key in people[i]) {
-    const td = document.createElement('td');
-
-    tr.append(td);
-
-    td.textContent = people[i][key];
-    dashboard.lastChild.append(tr);
-  };
-}
