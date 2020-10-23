8c7f8ba9d8da3ce98217bfb0461c0fcb065966e7
AntonChetveryk <chetverykanton92@gmail.com>
2020-10-23 20:22:45 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,31 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
-const people = require('./lib/people');
+const people = require("./lib/people");
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector(".dashboard");
+const tbody = table.children[0];
+
+tbody.insertAdjacentHTML(
+  "beforeend",
+  `${people
+    .map((person) => {
+      const age = person.died - person.born;
+      const century = Math.ceil(person.died / 100);
+
+      return `
+      <tr>
+        <td>${person.name}</td>
+        <td>${person.sex}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${age}</td>
+        <td>${century}</td>
+      </tr>
+      `;
+    })
+    .join("")}`
+);
