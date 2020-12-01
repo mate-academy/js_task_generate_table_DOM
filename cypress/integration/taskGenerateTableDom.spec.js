e68b0d7be3f13b7ac8afcd21eab676d707c2bc38
Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>
2020-12-01 10:42:25 +0200

    js_task_generate_table_DOM: Cypress tests added (#112)
    * tests added
    
    * tests added
    
    * tests refactored
    
    * fix grammar
    
    * renamed some variables, methods changed to jquery
    
    * tests refactored
    
    Co-authored-by: y.kostenko <y.kostenko@mate.academy>
    Co-authored-by: Roman Apostol <roman@mate.academy>

---
+++ cypress/integration/taskGenerateTableDom.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const people = require('../../src/scripts/lib/people.json');
+
+describe('Generate table app', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should create a table based on people.json file', () => {
+    for (let i = 0; i < people.length; i++) {
+
+      cy.contains('tr', people[i].name).contains('td', people[i].born);
+      cy.contains('tr', people[i].name).contains('td', people[i].died);
+
+      cy.contains('tr', people[i].name)
+        .contains(('td', people[i].died) - ('td', people[i].born));
+
+      cy.contains('tr', people[i].name)
+        .contains('td', Math.ceil(people[i].died / 100));
+
+      cy.contains('tr', people[i].name)
+        .contains('td', people[i].sex === 'm' ? 'Male' : 'Female');
+    }
+  });
+
+  it(`shouldn't contain fatherName, motherName and slug in table`, () => {
+    cy.get('tbody').contains('Petrus de Milliano').should('not.exist');
+    cy.get('tbody').contains('Laurentia van Vlaenderen').should('not.exist');
+    cy.get('tbody').contains('jan-van-brussel-1714').should('not.exist');
+  });
+});
