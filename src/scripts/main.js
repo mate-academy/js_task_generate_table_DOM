84c543713e7aa0e6ec03731610b32c5b8cd691c7
Your Name <mogolgogol.90@gmail.com>
2020-12-28 18:53:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,27 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const advancedDB = [ ...people ];
 
-// write your code here
+for (let i = 0; i < advancedDB.length; i++) {
+  advancedDB[i] = {
+    ...advancedDB[i],
+    age: advancedDB[i].died - advancedDB[i].born,
+    century: Math.ceil(advancedDB[i].died / 100),
+  };
+}
+
+const table = document.querySelector('.dashboard>tbody');
+
+for (const row of advancedDB) {
+  table.insertAdjacentHTML('beforeend',
+    `<tr>
+      <td>${row.name}</td>
+      <td>${row.sex}</td>
+      <td>${row.born}</td>
+      <td>${row.died}</td>
+      <td>${row.age}</td>
+      <td>${row.century <= 20 ? row.century + 'th' : row.century + 'st'}</td>
+    </tr>`
+  );
+}
