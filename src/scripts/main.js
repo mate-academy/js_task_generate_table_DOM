7c48f1ad92c7686d9c482c464db3c7e36180ad29
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-31 18:58:37 +0200

    Completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboard = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const tableRow = document.createElement('tr');
+  const gender = (person.sex === 'm' ? 'Male' : 'Female');
+  const century = Math.ceil(person.died / 100);
 
-// write your code here
+  tableRow.insertAdjacentHTML('beforeend',
+    `<td>${person.name}</td>
+    <td>${gender}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${century}</td>`
+  );
+
+  dashboard.append(tableRow);
+}
