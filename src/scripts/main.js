0fcbfa8e4a52ab0345f1cbef0828ea6c2df9f1ed
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-02 15:08:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,18 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const table = document.getElementsByTagName('tbody')[0];
+
+for (const person of people) {
+  table.innerHTML += `
+    <tr>
+      <td>${person.name}</td>
+      <td>${(person.sex === 'm') ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.born / 100)}</td>
+    </tr>
+  `;
+}
