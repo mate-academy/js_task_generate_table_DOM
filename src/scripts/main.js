a3ca82a4f46db4da53d7553eb26d0b5d509d1e36
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-04 19:28:21 +0200

    Dashboard

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,31 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('table.dashboard');
+
+people.forEach(person => {
+  const row = document.createElement('tr');
+
+  const tdName = document.createElement('td');
+  tdName.textContent = person.name;
+  const tdGender = document.createElement('td');
+  tdGender.textContent = person.sex;
+  const tdBorn = document.createElement('td');
+  tdBorn.textContent = person.born;
+  const tdDied = document.createElement('td');
+  tdDied.textContent = person.died;
+  const tdAge = document.createElement('td');
+  tdAge.textContent = person.died - person.born;
+  const tdCentury = document.createElement('td');
+  tdCentury.textContent = Math.ceil(person.born/100);
+
+  row.append(tdName);
+  row.append(tdGender);
+  row.append(tdBorn);
+  row.append(tdDied);
+  row.append(tdAge);
+  row.append(tdCentury);
+
+  table.append(row);
+})
+
