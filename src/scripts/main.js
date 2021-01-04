f0070e92b7e710b691c5612e20feec182df23577
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-04 17:19:33 +0200

    refreshed version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,28 +8,14 @@ console.log(people); // you can remove it
 const table = document.querySelector('.dashboard');
 
 for (const person of people) {
-  const tableRow = document.createElement('tr');
-
-  table.append(tableRow);
-
-  const cellOne = document.createElement('th');
-  const cellTwo = document.createElement('th');
-  const cellThree = document.createElement('th');
-  const cellFour = document.createElement('th');
-  const cellFive = document.createElement('th');
-  const cellSix = document.createElement('th');
-
-  cellOne.textContent = person.name;
-  cellTwo.textContent = person.sex;
-  cellThree.textContent = person.born;
-  cellFour.textContent = person.died;
-  cellFive.textContent = person.died - person.born;
-  cellSix.textContent = Math.ceil(person.died / 100);
-
-  tableRow.append(cellOne);
-  tableRow.append(cellTwo);
-  tableRow.append(cellThree);
-  tableRow.append(cellFour);
-  tableRow.append(cellFive);
-  tableRow.append(cellSix);
+  table.insertAdjacentHTML('beforeend', `
+  <tr>
+    <th>${person.name}</th>
+    <th>${person.sex}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
+  </tr>
+  `);
 };
