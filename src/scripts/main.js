0f7ba44bee58f777737b9b79c9634bc8c9e64477
Dima Bond <neverpooh@gmail.com>
2021-01-05 17:30:09 +0200

    optimized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,14 @@ console.log(people); // you can remove it
 const table = document.querySelector('.dashboard').firstElementChild;
 
 people.forEach(person => {
-  const gender = person.sex === 'm' ? 'Male' : 'Female';
-  const century = Math.ceil(person.died / 100);
-
   return table.insertAdjacentHTML('beforeend', `
     <tr>
       <td>${person.name}</td>
-      <td>${gender}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
       <td>${person.died - person.born}</td>
-      <td>${century}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
     </tr>
   `);
 });
