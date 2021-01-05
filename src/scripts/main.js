e2bbf449d797289e46904a844af2bd3d00764368
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-05 16:40:26 +0200

    chenges after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,14 @@ const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
 for (const person of people) {
-  const age = (person.died - person.born);
-  const century = Math.ceil(person.died / 100);
-  const sex = person.sex === 'm' ? 'Male' : 'Female';
-
   table.children[0].insertAdjacentHTML('beforeend', `
     <tr>
       <td>${person.name}</td>
-      <td>${sex}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
-      <td>${age}</td>
-      <td>${century}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
     </tr>
   `);
 }
