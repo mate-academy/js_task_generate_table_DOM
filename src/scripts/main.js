879914731820053d954fced394963fc0c4fe38f9
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-01-05 12:26:29 +0200

    table was created and fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,14 +8,14 @@ console.log(people); // you can remove it
 const table = document.querySelector('.dashboard');
 
 for (const person of people) {
-  table.insertAdjacentHTML('beforeend', `
+  table.insertAdjacentdTML('beforeend', `
   <tr>
-    <th>${person.name}</th>
-    <th>${person.sex}</th>
-    <th>${person.born}</th>
-    <th>${person.died}</th>
-    <th>${person.died - person.born}</th>
-    <th>${Math.ceil(person.died / 100)}</th>
+    <td>${person.name}</td>
+    <td>${person.sex}</td
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Matd.ceil(person.died / 100)}</td>
   </tr>
   `);
 };
