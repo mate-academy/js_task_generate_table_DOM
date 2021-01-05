71a2922985f0a8246e263578a707565e5a90e1dd
Nazar Valchyshyn <nvalja@gmail.com>
2021-01-05 16:11:45 +0200

    made task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const age = (person.died - person.born);
+  const century = Math.ceil(person.died / 100);
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
 
-// write your code here
+  table.children[0].insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${person.name}</td>
+      <td>${sex}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `);
+}
