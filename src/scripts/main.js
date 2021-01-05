bd1e5e18b14f7057d57c436d31655f21abae354e
RioDron <riopositivniy@gmail.com>
2021-01-06 00:15:56 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,27 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboard = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const fullPeopleData = people.map(person => {
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+  person.sex = person.sex === 'm' ? 'Male' : 'Female';
 
-// write your code here
+  return person;
+});
+
+for (const person of fullPeopleData) {
+  dashboard.firstElementChild.insertAdjacentHTML(
+    'beforeend',
+    `
+    <tr>
+        <td>${person.name}</td>
+        <td>${person.sex}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.age}</td>
+        <td>${person.century}</td>
+    </tr>
+  `);
+}
