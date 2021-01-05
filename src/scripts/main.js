c7142bca1f92e309f65a64764dab590171440d29
Maryna Zinchenko <pandoz16@gmail.com>
2021-01-05 18:10:50 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,24 +3,12 @@
 const people = require('./lib/people');
 const table = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people);
-// write your code here
-
 for (const person of people) {
-  let sex = '';
-
-  if (person.sex === 'm') {
-    sex = 'Male';
-  } else {
-    sex = 'Female';
-  }
-
   table.insertAdjacentHTML('beforeend',
     `
   <tr>
       <td>${person.name}</td>
-      <td>${sex}</td>
+      <td>${person.sex === 'f' ? 'Female' : 'Male'}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
       <td>${person.died - person.born}</td>
