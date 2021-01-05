101589e81d392b2441090d7b1dc834e9dd871d4a
Your Name <mogolgogol.90@gmail.com>
2021-01-05 18:30:21 +0200

    reduced code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,27 +2,20 @@
 
 const people = require('./lib/people');
 
-const advancedDB = [ ...people ];
-
-for (let i = 0; i < advancedDB.length; i++) {
-  advancedDB[i] = {
-    ...advancedDB[i],
-    age: advancedDB[i].died - advancedDB[i].born,
-    century: Math.ceil(advancedDB[i].died / 100),
-  };
-}
-
 const table = document.querySelector('.dashboard>tbody');
 
-for (const row of advancedDB) {
+for (const row of people) {
   table.insertAdjacentHTML('beforeend',
     `<tr>
       <td>${row.name}</td>
       <td>${row.sex === 'f' ? 'Female' : 'Male'}</td>
       <td>${row.born}</td>
       <td>${row.died}</td>
-      <td>${row.age}</td>
-      <td>${row.century <= 20 ? row.century + 'th' : row.century + 'st'}</td>
+      <td>${row.died - row.born}</td>
+      <td>${Math.ceil(row.died / 100) <= 20
+    ? Math.ceil(row.died / 100) + 'th'
+    : Math.ceil(row.died / 100) + 'st'}
+      </td>
     </tr>`
   );
 }
