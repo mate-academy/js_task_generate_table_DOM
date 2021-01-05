a267e30ab90d64651510a15afbd0dd597c395572
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-01-05 17:33:15 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,14 @@ const people = require('./lib/people');
 const table = document.getElementsByTagName('tbody')[0];
 
 for (const person of people) {
-  table.innerHTML += `
-    <tr>
+  table.insertAdjacentHTML('beforeend',
+    `<tr>
       <td>${person.name}</td>
       <td>${(person.sex === 'm') ? 'Male' : 'Female'}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
       <td>${person.died - person.born}</td>
       <td>${Math.ceil(person.died / 100)}</td>
-    </tr>
-  `;
+    </tr>`
+  );
 }
