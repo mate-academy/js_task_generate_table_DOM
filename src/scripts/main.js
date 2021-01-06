9033b3c789ad1e2b8818521209dbcf4d31f4dfe0
PaulT <ptimofejev@gmail.com>
2021-01-06 16:15:58 +0200

    Unneccessary variables fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,16 @@
 
 const people = require('./lib/people');
 
-const table = document.querySelector('.dashboard');
-
 people.forEach(person => {
-  const gender = (person.sex === 'm') ? 'Male' : 'Female';
-
-  table.children[0].insertAdjacentHTML('beforeend', `
-    <tr>
-      <td>${person.name}</td>
-      <td>${gender}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
-      <td>${person.died - person.born}</td>
-      <td>${Math.ceil(person.died / 100)}</td>
-    </tr>
-  `);
+  document.querySelector('.dashboard').children[0]
+    .insertAdjacentHTML('beforeend', `
+      <tr>
+        <td>${person.name}</td>
+        <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+      </tr>
+    `);
 });
