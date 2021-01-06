155972b689582da533c4bcd67a59c0fe26a33163
Dima Semenov <semenovdima998@gmail.com>
2021-01-06 15:49:15 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,10 @@ const people = require('./lib/people');
 const row = document.querySelector('tr');
 
 for (const person of people) {
-  const sex = person.sex === 'm' ? 'Male' : 'Female';
-
   row.insertAdjacentHTML('afterend', `
     <tr>
       <td>${person.name}</td>
-      <td>${sex}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
       <td>${person.died - person.born}</td>
