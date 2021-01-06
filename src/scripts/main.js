8e92b5778d2a32e33c181ba03278abe44ebcab8b
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-06 10:28:37 +0200

    Optimized solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,26 +10,13 @@ const table = document.querySelector('table.dashboard');
 people.forEach(person => {
   const row = document.createElement('tr');
 
-  const tdName = document.createElement('td');
-  const tdGender = document.createElement('td');
-  const tdBorn = document.createElement('td');
-  const tdDied = document.createElement('td');
-  const tdAge = document.createElement('td');
-  const tdCentury = document.createElement('td');
-
-  tdName.textContent = person.name;
-  tdGender.textContent = person.sex === 'm' ? 'Male' : 'Female';
-  tdBorn.textContent = person.born;
-  tdDied.textContent = person.died;
-  tdAge.textContent = person.died - person.born;
-  tdCentury.textContent = Math.ceil(person.died / 100);
-
-  row.append(tdName);
-  row.append(tdGender);
-  row.append(tdBorn);
-  row.append(tdDied);
-  row.append(tdAge);
-  row.append(tdCentury);
+  row.insertAdjacentHTML('afterbegin',
+  `<td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.died / 100)}</td>`)
 
   table.append(row);
 });
