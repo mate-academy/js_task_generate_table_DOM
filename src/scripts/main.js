e18f346b058382646c5bcf4c6ceb90c7b15248c8
Maxim Zhuravlyov <maksym.zhuravlov1995@gmail.com>
2021-01-08 17:10:04 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 const people = require('./lib/people');
+const tableBody = document.querySelector('.dashboard tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const html = `
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `;
 
-// write your code here
+  tableBody.insertAdjacentHTML('beforeend', html);
+}
