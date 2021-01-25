428d72afac0602dcc301c818f4637a38db208c70
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-01-25 21:12:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,26 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const tr = document.createElement('tr');
+  const dashboard = document.querySelector('.dashboard');
 
-// write your code here
+  let sex = '';
+
+  if (person.sex === 'm') {
+    sex = 'Male';
+  } else {
+    sex = 'Female';
+  }
+
+  tr.insertAdjacentHTML('afterbegin', `
+    <td>${person.name}</td>
+    <td>${sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `);
+
+  dashboard.append(tr);
+}
