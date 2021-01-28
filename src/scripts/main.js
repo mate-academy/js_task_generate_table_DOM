c7dfd3cffdf9e655654783d13b2f1bd9e43a37db
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2021-01-28 21:22:51 +0200

    Fixed after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,41 +11,21 @@ const el = document.querySelector('.dashboard');
 
 for (const person of people) {
   const tr = document.createElement('tr');
-  const tdName = document.createElement('td');
 
-  tdName.textContent = person.name;
-  tr.append(tdName);
-
-  const tdSex = document.createElement('td');
-
-  tdSex.textContent = person.sex;
-
-  if (tdSex.textContent === 'm') {
-    tdSex.textContent = 'Male';
+  if (person.sex === 'm') {
+    person.sex = 'Male';
   } else {
-    tdSex.textContent = 'Female';
+    person.sex = 'Female';
   }
-  tr.append(tdSex);
 
-  const tdBorn = document.createElement('td');
-
-  tdBorn.textContent = person.born;
-  tr.append(tdBorn);
-
-  const tdDied = document.createElement('td');
-
-  tdDied.textContent = person.died;
-  tr.append(tdDied);
-
-  const tdAge = document.createElement('td');
-
-  tdAge.textContent = person.died - person.born;
-  tr.append(tdAge);
-
-  const tdCentury = document.createElement('td');
-
-  tdCentury.textContent = Math.ceil(person.died / 100);
-  tr.append(tdCentury);
+  tr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `;
 
   el.append(tr);
 }
