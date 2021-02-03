d3ceb47263dd9450e945429c03aba8972ae3d7b9
DominoSl <artemenko.mik@gmail.com>
2021-02-03 11:04:55 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,45 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+const innerText = (target, index) => {
+  switch (index) {
+    case 0 : {
+      return target.name;
+    }
+
+    case 1 : {
+      return (target.sex === 'm') ? 'Male' : 'Female';
+    }
+
+    case 2 : {
+      return target.born;
+    }
+
+    case 3 : {
+      return target.died;
+    }
+
+    case 4 : {
+      return target.died - target.born;
+    }
+
+    case 5 : {
+      return Math.ceil(target.died / 100);
+    }
+  }
+};
+
+for (const human of people) {
+  const row = document.createElement('tr');
+
+  for (let j = 0; j < 6; ++j) {
+    const cell = document.createElement('td');
+
+    cell.innerText = innerText(human, j);
+    row.appendChild(cell);
+  }
+
+  table.appendChild(row);
+}
