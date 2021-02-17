2b17bb0be788dcd9139cb16c0d4500d9a5e38005
Andriy <andriy@Air-Andriy.lan>
2021-02-17 00:59:02 +0000

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,22 @@
 'use strict';
 
 const people = require('./lib/people');
-const table  = document.querySelector('.dashboard');
+const table = document.querySelector('.dashboard');
 
 people.forEach(person => {
-    const tr = document.createElement('tr');
-  
-    const age = person.died - person.born;
-    const century = Math.ceil(person.died / 100);
-    const sex = person.sex === 'm' ? 'Male' : 'Female'
-  
-    tr.innerHTML = `
-        <td>${person.name}</td>
-        <td>${sex}</td>
-        <td>${person.born}</td>
-        <td>${person.died}</td>
-        <td>${age}</td>
-        <td>${century}</td>
-    `;
-  
-    table.appendChild(tr);
-})
+  const tr = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+
+  tr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  `;
+
+  table.appendChild(tr);
+});
