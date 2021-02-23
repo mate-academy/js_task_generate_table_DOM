0c7980076e84eae431cdfbadfac4cbbd7efe8293
Bohdan Dobak <chudolipton@gmail.com>
2021-02-23 23:33:31 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,30 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('table');
 
-// write your code here
+for (let i = 0; i < people.length; i++) {
+  const row = document.createElement('tr');
+  const personName = people[i].name;
+  const personGender = people[i].sex;
+  const personBorn = people[i].born;
+  const personDied = people[i].died;
+  const personAge = (people[i].died - people[i].born);
+  const personCentury = Math.ceil(people[i].died / 100);
+  const nameCell = document.createElement('td');
+  const genderCell = document.createElement('td');
+  const bornCell = document.createElement('td');
+  const diedCell = document.createElement('td');
+  const ageCell = document.createElement('td');
+  const centuryCell = document.createElement('td');
+
+  nameCell.innerText = personName;
+  genderCell.innerText = personGender === 'm' ? 'Male' : 'Female';
+  bornCell.innerText = personBorn;
+  diedCell.innerText = personDied;
+  ageCell.innerText = personAge;
+  centuryCell.innerText = personCentury;
+  row.append(nameCell, genderCell, bornCell, diedCell, ageCell, centuryCell);
+
+  table.appendChild(row);
+}
