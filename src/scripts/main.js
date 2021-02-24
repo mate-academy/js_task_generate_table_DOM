7ddb7a5dd9e86dd6b95fd306826ab9f9ad6d6314
morozoleh <olegmoroz93@gmail.com>
2021-02-24 20:30:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,35 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+function updateTable(element, parameters) {
+  parameters.forEach(({ name: firstName, sex, born, died }) => {
+    const tableRow = document.createElement('tr');
+    const tableName = document.createElement('td');
+    const tableGender = document.createElement('td');
+    const tableBorn = document.createElement('td');
+    const tableDied = document.createElement('td');
+    const tableAge = document.createElement('td');
+    const tableCentury = document.createElement('td');
+
+    element.append(tableRow);
+    tableRow.append(tableName);
+    tableRow.append(tableGender);
+    tableRow.append(tableBorn);
+    tableRow.append(tableDied);
+    tableRow.append(tableAge);
+    tableRow.append(tableCentury);
+    tableName.textContent = firstName;
+    tableBorn.textContent = born;
+    tableDied.textContent = died;
+    tableAge.textContent = died - born;
+    tableCentury.textContent = Math.ceil(died / 100);
+
+    tableGender.textContent = sex === 'm'
+      ? 'Male'
+      : 'Female';
+  });
+}
+
+updateTable(table, people);
