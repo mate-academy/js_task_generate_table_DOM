63724759095a3eb5e970cecb691aff5ff7b6a223
morozoleh <olegmoroz93@gmail.com>
2021-02-25 13:00:52 +0200

    fixed-after-mentor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,30 +6,16 @@ const table = document.querySelector('.dashboard');
 
 function updateTable(element, parameters) {
   parameters.forEach(({ name: firstName, sex, born, died }) => {
-    const tableRow = document.createElement('tr');
-    const tableName = document.createElement('td');
-    const tableGender = document.createElement('td');
-    const tableBorn = document.createElement('td');
-    const tableDied = document.createElement('td');
-    const tableAge = document.createElement('td');
-    const tableCentury = document.createElement('td');
-
-    element.append(tableRow);
-    tableRow.append(tableName);
-    tableRow.append(tableGender);
-    tableRow.append(tableBorn);
-    tableRow.append(tableDied);
-    tableRow.append(tableAge);
-    tableRow.append(tableCentury);
-    tableName.textContent = firstName;
-    tableBorn.textContent = born;
-    tableDied.textContent = died;
-    tableAge.textContent = died - born;
-    tableCentury.textContent = Math.ceil(died / 100);
-
-    tableGender.textContent = sex === 'm'
-      ? 'Male'
-      : 'Female';
+    element.children[0].insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${firstName}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${died - born}</td>
+      <td>${Math.ceil(died / 100)}</td>
+      <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+    </tr>`
+    );
   });
 }
 
