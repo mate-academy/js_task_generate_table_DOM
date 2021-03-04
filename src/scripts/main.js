3a00c2da2cf3b6da4e1b25c7db60737ba481e655
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-03-04 16:26:47 +0200

    created the function convertingAPeopleObjectToATable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,24 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+convertingAPeopleObjectToATable(people);
 
-// write your code here
+function convertingAPeopleObjectToATable(men) {
+  const tbody = document.querySelector('tbody');
+
+  for (const person of men) {
+    const { sex, born, died } = person;
+    const namePerson = person.name;
+
+    tbody.insertAdjacentHTML('beforeend', `
+      <tr>
+        <td>${namePerson}</td>
+        <td>${sex === 'f' ? 'Female' : 'Male'}</td>
+        <td>${born}</td>
+        <td>${died}</td>
+        <td>${died - born}</td>
+        <td>${Math.ceil(died / 100)}</td>
+      </tr>
+    `);
+  }
+}
