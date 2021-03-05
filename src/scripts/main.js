1729850367718e36e5677e07114ba131560cbad9
ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-05 15:03:13 +0200

    firstCommit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,20 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+console.log(people);
 
-// write your code here
+const headerOFDashboard = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const newRow = document.createElement('tr');
+
+  newRow.insertAdjacentHTML('beforeend', `
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+  `);
+  headerOFDashboard.append(newRow);
+}
