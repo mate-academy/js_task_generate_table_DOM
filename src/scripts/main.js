38cc20796619a63a396626553adf79d145aa442a
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-03-05 18:53:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,30 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+people.forEach(human => {
+  const table = document.querySelector('.dashboard');
+  const tableBody = table.querySelector('tbody');
+
+  const age = human.died - human.born;
+  const century = Math.ceil(human.died / 100);
+
+  let gender = '';
+
+  if (human.sex === 'm') {
+    gender = 'Male';
+  } else {
+    gender = 'Female';
+  }
+
+  tableBody.insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${human.name}</td>
+      <td>${gender}</td>
+      <td>${human.born}</td>
+      <td>${human.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `);
+});
