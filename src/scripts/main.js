39c467cb0b14f25de35f4748e3f12a4d6bdb27c7
Artur Shpontak <ashpontak97@gmail.com>
2021-03-08 23:05:57 +0200

    Solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,25 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+function createRowElement(value) {
+  const createdRowElement = document.createElement('td');
+
+  createdRowElement.innerHTML = value;
+
+  return createdRowElement;
+}
+
+for (const person of people) {
+  const createdRow = document.createElement('tr');
+
+  createdRow.append(createRowElement(person.name));
+  createdRow.append(createRowElement(person.sex === 'm' ? 'Male' : 'Female'));
+  createdRow.append(createRowElement(person.born));
+  createdRow.append(createRowElement(person.died));
+  createdRow.append(createRowElement(person.died - person.born));
+  createdRow.append(createRowElement(Math.ceil(person.born / 100)));
+
+  table.lastChild.after(createdRow);
+}
