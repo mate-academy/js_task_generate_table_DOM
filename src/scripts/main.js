a6646353ca23378ad48a46f49e9dc9a493dfeb20
Olha Briukhanova <oaleksandrovva@gmail.com>
2021-03-08 20:10:54 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,19 @@
 const people = require('./lib/people');
 const table = document.querySelector('.dashboard tbody');
 
-people.forEach(person => {
-  table.insertAdjacentHTML('beforeend', `
-    <tr>
-      <td>${person.name}</td>
-      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
-      <td>${person.died - person.born}</td>
-      <td>${Math.ceil(person.died / 100)}</td>
-    </tr>
-  `);
-});
+function addToTable(map, data) {
+  data.forEach(person => {
+    map.children[0].insertAdjacentHTML('beforeend', `
+      <tr>
+        <td>${person.name}</td>
+        <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+      </tr>
+    `);
+  });
+}
+
+addToTable(table, people);
