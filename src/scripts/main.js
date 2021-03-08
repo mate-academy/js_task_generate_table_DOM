585619a8aca074e62de3f0604fd2062e803ce9d9
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-08 09:50:17 +0200

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,34 +2,18 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
-
 const table = document.querySelector('.dashboard');
 
-for (const person in people) {
+for (const person of people) {
   const row = document.createElement('tr');
-  const personName = document.createElement('td');
-  const personGender = document.createElement('td');
-  const personBorn = document.createElement('td');
-  const personDied = document.createElement('td');
-  const personAge = document.createElement('td');
-  const personCentury = document.createElement('td');
 
+  row.insertAdjacentHTML('beforeend', `
+    <td>${person.name}</td>
+    <td>${person.sex === 'f' ? 'Female' : 'Male'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `);
   table.append(row);
-  row.append(personName);
-  row.append(personGender);
-  row.append(personBorn);
-  row.append(personDied);
-  row.append(personAge);
-  row.append(personCentury);
-
-  personName.textContent = people[person].name;
-  personGender.textContent = people[person].sex === 'f' ? 'Female' : 'Male';
-  personBorn.textContent = people[person].born;
-  personDied.textContent = people[person].died;
-  personAge.textContent = people[person].died - people[person].born;
-  personCentury.textContent = Math.ceil(people[person].died / 100);
 }
