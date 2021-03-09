1b9f72185b56f1bd1a61142149a6bcc18803fda9
Ping <4ivrik@ukr.net>
2021-03-09 16:05:36 +0200

    return from forEach removed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,7 @@ const table = document.querySelector('.dashboard tbody');
 
 function addingRows(list, data) {
   return list.forEach(person => {
-    return data.insertAdjacentHTML('beforeend',
+    data.insertAdjacentHTML('beforeend',
       `<tr>
         <td>${person.name}</td>
         <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
