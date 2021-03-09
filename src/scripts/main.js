78ab060b8c75454677f420dbfa719e362adc60b2
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-09 16:21:57 +0200

    edited with comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,15 +4,15 @@ const people = require('./lib/people');
 
 const element = document.querySelector('.dashboard');
 
-[...people].forEach((person) => {
-  const newLine = document.createElement('tr');
+people.forEach((person) => {
+  const rowTable = document.createElement('tr');
 
-  newLine.innerHTML = `<td> ${person.name} </td>`
+  rowTable.innerHTML = `<td> ${person.name} </td>`
     + `<td> ${person.sex === 'm' ? 'Male' : 'Female'} </td>`
     + `<td> ${person.born} </td>`
     + `<td> ${person.died} </td>`
     + `<td> ${person.died - person.born} </td>`
     + `<td> ${Math.ceil(person.died / 100)} </td>`;
 
-  element.append(newLine);
+  element.append(rowTable);
 });
