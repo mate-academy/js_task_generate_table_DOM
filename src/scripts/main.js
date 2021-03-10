0b01df3b5f388c2e400404bd0dd4ce5f4f807806
Nikita Puhovskii <cheliojul@gmail.com>
2021-03-10 10:32:50 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,47 +8,36 @@ for (const person of people) {
   const row = document.createElement('tr');
 
   createCell(person, 'name', row);
-  createCellGender(person, row);
+  createCell(person, 'gender', row);
   createCell(person, 'born', row);
   createCell(person, 'died', row);
-  createCellAge(person, row);
-  createCellCentury(person, row);
+  createCell(person, 'age', row);
+  createCell(person, 'century', row);
   table.append(row);
 }
 
 function createCell(object, key, row) {
   const cell = document.createElement('td');
-  const cellText = document.createTextNode(object[key]);
+  let cellText = document.createTextNode(object[key]);
 
-  cell.appendChild(cellText);
-  row.appendChild(cell);
-}
-
-function createCellGender(object, row) {
-  const cell = document.createElement('td');
-  let cellText = '';
-
-  if (object.sex === 'f') {
-    cellText = document.createTextNode('Female');
-  } else {
-    cellText = document.createTextNode('Male');
+  if (key === 'age') {
+    cellText = document.createTextNode(object.died - object.born);
   }
+
+  if (key === 'gender') {
+    object.sex === 'f'
+      ? cellText = document.createTextNode('Female')
+      : cellText = document.createTextNode('Male');
+  }
+
+  if (key === 'age') {
+    cellText = document.createTextNode(object.died - object.born);
+  }
+
+  if (key === 'century') {
+    cellText = document.createTextNode(Math.ceil(object.died / 100));
+  }
+
   cell.appendChild(cellText);
   row.appendChild(cell);
-}
-
-function createCellCentury(object, row) {
-  const cell = document.createElement('td');
-  const cellText = document.createTextNode(Math.ceil(object.died / 100));
-
-  cell.appendChild(cellText);
-  row.appendChild(cell);
-}
-
-function createCellAge(object, row) {
-  const cell = document.createElement('td');
-  const cellText = document.createTextNode(object.died - object.born);
-
-  cell.appendChild(cellText);
-  row.appendChild(cell);
-}
+};
