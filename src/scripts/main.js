af1f5a2cc62da393424c424a59fcfd1acd2dbcab
Inna Sorochynska <innasoro@gmail.com>
2021-03-23 16:07:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const cells = [person.name, person.sex, person.born, person.died,
+    person.died - person.born, Math.ceil(person.died / 100)];
+  const row = document.createElement('tr');
 
-// write your code here
+  for (let cell of cells) {
+    if (cell === 'f') {
+      cell = 'Female';
+    } else if (cell === 'm') {
+      cell = 'Male';
+    }
+
+    const item = document.createElement('td');
+
+    item.textContent = cell;
+    row.append(item);
+  }
+  table.append(row);
+});
