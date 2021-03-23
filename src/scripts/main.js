4c466b2a75a172cd943ae4244309a2b277033dd6
Ihor Drul <druleger@gmail.com>
2021-03-23 11:57:38 +0200

    Fixed comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,16 +6,14 @@ const table = document.querySelector('.dashboard');
 for (let i = 0; i < people.length; i++) {
   const row = document.createElement('tr');
   const { name: personName, sex, born, died } = people[i];
-  const age = died - born;
-  const century = Math.ceil(died / 100);
 
   row.innerHTML = `
     <td>${personName}</td>
     <td>${sex === 'm' ? 'Male' : 'Female'}</td>
     <td>${born}</td>
     <td>${died}</td>
-    <td>${age}</td>
-    <td>${century}</td>
+    <td>${died - born}</td>
+    <td>${Math.ceil(died / 100)}</td>
   `;
 
   table.append(row);
