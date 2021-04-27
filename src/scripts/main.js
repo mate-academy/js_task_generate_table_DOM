fdf91dd76df59d18f4663dc9b62c7a54888fe273
Ivan Major <tarstarcas@gmail.com>
2021-04-27 21:38:35 +0300

    Generate table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,10 @@ for (let i = 0; i < people.length; i++) {
   people[i].age = people[i].died - people[i].born;
   people[i].century = Math.ceil(people[i].died / 100);
 
-  if (people[i].sex === 'm') {
-    people[i].gender = 'Male';
-  } else {
-    people[i].gender = 'Female';
-  };
-}
+  (people[i].sex === 'm')
+    ? people[i].gender = 'Male'
+    : people[i].gender = 'Female';
+};
 
 const table = document.querySelector('.dashboard');
 
@@ -19,17 +17,17 @@ const heading = document.querySelectorAll('th');
 
 const items = [];
 
-for (let j = 0; j < heading.length; j++) {
-  items.push(heading[j].textContent.toLowerCase());
+for (let i = 0; i < heading.length; i++) {
+  items.push(heading[i].textContent.toLowerCase());
 };
 
-for (let x = 0; x < people.length; x++) {
+for (let i = 0; i < people.length; i++) {
   const row = document.createElement('tr');
 
   for (const item of items) {
     const cell = document.createElement('td');
 
-    cell.textContent = people[x][item];
+    cell.textContent = people[i][item];
     row.append(cell);
   };
 
