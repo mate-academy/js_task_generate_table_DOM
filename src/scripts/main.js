141a154d1fc74d10424c0b3864af8a44d98a41d7
Vlad Trundenko <vlad.trundenko@gmail.com>
2021-05-04 12:09:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,18 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const dashboard = document.querySelector('.dashboard tbody');
+
+for (const person of people) {
+  dashboard.insertAdjacentHTML('beforeend', `
+    <tr>
+      <th>${person.name}</th>
+      <th>${person.sex === 'f' ? 'Female' : 'Male'}</th>
+      <th>${person.born}</th>
+      <th>${person.died}</th>
+      <th>${person.died - person.born}</th>
+      <th>${person.died ? Math.ceil(person.died / 100) : 1}</th>
+    </tr>
+  `);
+}
