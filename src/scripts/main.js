aeff057d19a4b23363d017b44156062c5f4103d7
romandubovskyi <rdubowskij@gmail.com>
2021-05-10 17:22:22 +0300

    converted json to table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,26 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableBody = document.querySelector('tBody');
 
-// write your code here
+for (let i = 0; i < people.length; i++) {
+  const tableRow = formRow(people[i]);
+
+  tableBody.insertAdjacentHTML('beforeend', tableRow);
+}
+
+function formRow(person) {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const gender = person.sex === 'f' ? 'Female' : 'Male';
+  const row = `<tr>
+    <td>${person.name}</td>
+    <td>${gender}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  </tr>`;
+
+  return row;
+}
