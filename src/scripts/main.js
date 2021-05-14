5e2d90991b75c9a3e95bbc383bf50ea7abe6b9b6
Eugene Khablenko <eugene.khablenko@gmail.com>
2021-05-14 13:16:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,18 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (let i = 0; i < people.length; i++) {
+  const tableRow = document.createElement('tr');
+
+  tableRow.innerHTML = `
+  <td>${people[i].name}</td>
+  <td>${(people[i].sex).toUpperCase()}</td>
+  <td>${people[i].born}</td>
+  <td>${people[i].died}</td>
+  <td>${people[i].died - people[i].born}</td>
+  <td>${Math.ceil(people[i].died / 100)}</td>
+  `;
+  table.append(tableRow);
+}
