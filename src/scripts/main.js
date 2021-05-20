7efd4a95ecb85b7409fb39af8361580ecbce0b3e
Inna <inna_temis@ukr.net>
2021-05-20 15:15:34 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,31 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const tr = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const gender = () => {
+    if (person.sex === 'm') {
+      return 'Male';
+    }
+
+    return 'Female';
+  };
+
+  tr.insertAdjacentHTML('beforeend', `
+
+    <td>${person.name}</td>
+    <td>${gender(person)}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td> 
+
+  `);
+
+  table.append(tr);
+};
