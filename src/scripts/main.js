b895c501934d9fb508e7ec365220a6dc0a6d52fa
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-01 18:18:32 +0300

    Filled table using js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,47 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboard = document
+  .querySelector('.dashboard')
+  .getElementsByTagName('tbody')[0];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
 
-// write your code here
+  const row = document.createElement('tr');
+  const nameCell = row.insertCell();
+  const sexCell = row.insertCell();
+  const bornCell = row.insertCell();
+  const diedCell = row.insertCell();
+  const ageCell = row.insertCell();
+  const centuryCell = row.insertCell();
+
+  const personName = document.createTextNode(person.name);
+
+  nameCell.appendChild(personName);
+
+  const personSex = person.sex === 'm'
+    ? document.createTextNode('Male')
+    : document.createTextNode('Female');
+
+  sexCell.appendChild(personSex);
+
+  const personBorn = document.createTextNode(person.born);
+
+  bornCell.appendChild(personBorn);
+
+  const personDied = document.createTextNode(person.died);
+
+  diedCell.appendChild(personDied);
+
+  const personAge = document.createTextNode(age);
+
+  ageCell.appendChild(personAge);
+
+  const personCentury = document.createTextNode(century);
+
+  centuryCell.appendChild(personCentury);
+
+  dashboard.appendChild(row);
+});
