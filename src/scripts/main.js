63c1e5e918ea69d22aa877cabe488e0bd07c8456
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-02 17:05:36 +0300

    Made code smaller

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,42 +6,14 @@ const dashboard = document
   .getElementsByTagName('tbody')[0];
 
 people.forEach(person => {
-  const age = person.died - person.born;
-  const century = Math.ceil(person.died / 100);
-
   const row = document.createElement('tr');
-  const nameCell = row.insertCell();
-  const sexCell = row.insertCell();
-  const bornCell = row.insertCell();
-  const diedCell = row.insertCell();
-  const ageCell = row.insertCell();
-  const centuryCell = row.insertCell();
 
-  const personName = document.createTextNode(person.name);
-
-  nameCell.appendChild(personName);
-
-  const personSex = person.sex === 'm'
-    ? document.createTextNode('Male')
-    : document.createTextNode('Female');
-
-  sexCell.appendChild(personSex);
-
-  const personBorn = document.createTextNode(person.born);
-
-  bornCell.appendChild(personBorn);
-
-  const personDied = document.createTextNode(person.died);
-
-  diedCell.appendChild(personDied);
-
-  const personAge = document.createTextNode(age);
-
-  ageCell.appendChild(personAge);
-
-  const personCentury = document.createTextNode(century);
-
-  centuryCell.appendChild(personCentury);
+  row.innerHTML = `<td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.died / 100)}</td>`;
 
   dashboard.appendChild(row);
 });
