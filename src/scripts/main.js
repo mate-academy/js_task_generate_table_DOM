30c25b0d880c4ef3a5e11bc44ec1e247f69dc695
korzh <korzh.alexey@yahoo.com>
2021-07-05 23:12:22 +0300

    solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// console.log(people); // you can remove it
 
-// write your code here
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  const row = document.createElement('tr');
+
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+
+  row.innerHTML = `<td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'male' : 'female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.age}</td>
+  <td>${person.century}</td>`;
+
+  table.append(row);
+};
