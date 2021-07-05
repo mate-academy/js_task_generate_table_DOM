34d207193e22002c0aa4da5fd7fa567d7c9136db
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-05 17:26:15 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,35 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const row = document.createElement('tr');
 
-// write your code here
+  const nameCell = document.createElement('td');
+  const genderCell = document.createElement('td');
+  const bornCell = document.createElement('td');
+  const diedCell = document.createElement('td');
+  const ageCell = document.createElement('td');
+  const centuryCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+
+  genderCell.textContent = person.sex === 'm'
+    ? 'Male'
+    : 'Female';
+
+  bornCell.textContent = person.born;
+  diedCell.textContent = person.died;
+  ageCell.textContent = person.died - person.born;
+  centuryCell.textContent = Math.ceil(person.died / 100);
+
+  row.append(nameCell);
+  row.append(genderCell);
+  row.append(bornCell);
+  row.append(diedCell);
+  row.append(ageCell);
+  row.append(centuryCell);
+
+  table.append(row);
+}
