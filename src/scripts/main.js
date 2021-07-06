66b20e72de73570bb38941390428aa236ca950c2
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-06 22:35:02 +0300

    Check me

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,41 @@
 'use strict';
 
 const people = require('./lib/people');
+const getTable = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const newTr = document.createElement('tr');
 
-// write your code here
+  for (let i = 0; i < 6; i++) {
+    const createTh = document.createElement('th');
+
+    if (i === 0) {
+      createTh.innerText = person.name;
+    };
+
+    if (i === 1) {
+      createTh.innerText = (person.sex === 'm')
+        ? 'male'
+        : 'famale';
+    };
+
+    if (i === 2) {
+      createTh.innerText = person.born;
+    };
+
+    if (i === 3) {
+      createTh.innerText = person.died;
+    };
+
+    if (i === 4) {
+      createTh.innerText = person.died - person.born;
+    };
+
+    if (i === 5) {
+      createTh.innerText = Math.ceil(person.died / 100);
+    };
+
+    newTr.append(createTh);
+  };
+  getTable.append(newTr);
+});
