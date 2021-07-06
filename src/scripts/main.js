6aaf4fc912cb9ae830ccbe07cc217aeb92b4a411
korzh <korzh.alexey@yahoo.com>
2021-07-06 15:05:45 +0300

    optimized code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,15 +7,12 @@ for (let i = 0; i < people.length; i++) {
   const person = people[i];
   const row = document.createElement('tr');
 
-  person.age = person.died - person.born;
-  person.century = Math.ceil(person.died / 100);
-
   row.innerHTML = `<td>${person.name}</td>
   <td>${person.sex === 'm' ? 'male' : 'female'}</td>
   <td>${person.born}</td>
   <td>${person.died}</td>
-  <td>${person.age}</td>
-  <td>${person.century}</td>`;
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.died / 100)}</td>`;
 
   table.append(row);
 };
