bef67834941dea52b3a9f754826865efc6910846
Dmytro Prontenko <dprontenko@gmail.com>
2021-07-07 17:19:45 +0300

    Fixes after CR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,36 +3,19 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-// console.log(people); // you can remove it
 const table = document.querySelector('tbody');
-// console.log(table);
 
 // write your code here
-
 for (let i = 0; i <= people.length - 1; i++) {
-  // console.log(people[i]);
-
   const tableRow = document.createElement('tr');
-  const tableDataName = document.createElement('td');
-  const tableDataGender = document.createElement('td');
-  const tableDataBorn = document.createElement('td');
-  const tableDataDied = document.createElement('td');
-  const tableDataAge = document.createElement('td');
-  const tableCentury = document.createElement('td');
 
-  tableDataName.textContent = people[i].name;
-
-  tableDataGender.textContent = (people[i].sex === 'm') ? 'Male' : 'Female';
-  tableDataBorn.textContent = people[i].born;
-  tableDataDied.textContent = people[i].died;
-  tableDataAge.textContent = people[i].died - people[i].born;
-  tableCentury.textContent = Math.floor(people[i].died / 100) + 1;
-
+  tableRow.innerHTML = `
+    <td>${people[i].name}</td>
+    <td>${(people[i].sex === 'm') ? 'Male' : 'Female'}</td>
+    <td>${people[i].born}</td>
+    <td>${people[i].died}</td>
+    <td>${people[i].died - people[i].born}</td>
+    <td>${Math.floor(people[i].died / 100) + 1}</td>
+  `;
   table.append(tableRow);
-  tableRow.append(tableDataName);
-  tableRow.append(tableDataGender);
-  tableRow.append(tableDataBorn);
-  tableRow.append(tableDataDied);
-  tableRow.append(tableDataAge);
-  tableRow.append(tableCentury);
 }
