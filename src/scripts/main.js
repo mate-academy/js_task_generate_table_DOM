4d56c120e96e6377dc64496ecd7bc017797f812f
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-07-07 17:25:34 +0300

    Check me again

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,20 @@
 'use strict';
 
 const people = require('./lib/people');
-const getTable = document.querySelector('.dashboard');
+const table = document.querySelector('.dashboard');
 
 people.forEach(person => {
   const newTr = document.createElement('tr');
 
-  for (let i = 0; i < 6; i++) {
-    const createTh = document.createElement('th');
-
-    if (i === 0) {
-      createTh.innerText = person.name;
-    };
-
-    if (i === 1) {
-      createTh.innerText = (person.sex === 'm')
-        ? 'male'
-        : 'famale';
-    };
-
-    if (i === 2) {
-      createTh.innerText = person.born;
-    };
-
-    if (i === 3) {
-      createTh.innerText = person.died;
-    };
-
-    if (i === 4) {
-      createTh.innerText = person.died - person.born;
-    };
-
-    if (i === 5) {
-      createTh.innerText = Math.ceil(person.died / 100);
-    };
-
-    newTr.append(createTh);
-  };
-  getTable.append(newTr);
+  newTr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.innerText = (person.sex === 'm')
+    ? 'male'
+    : 'famale'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.floor(person.died / 100)}</td>
+  `;
+  table.append(newTr);
 });
