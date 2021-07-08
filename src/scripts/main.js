09eeaff632ea417eeecfb8be06b94d1127881a81
David Dohru <tronsmartmy@gmail.com>
2021-07-08 21:13:13 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const people = require('./lib/people');
+const accessToTable = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+accessToTable.innerHTML = `
+  <tr>
+    <th>Name</th>
+    <th>Gender</th>
+    <th>Born</th>
+    <th>Died</th>
+    <th>Age</th>
+    <th>Century</th>
+  </tr>`
 
-// write your code here
+    + people.map(x => `
+    <tr>
+      <th>${x.name}</th>
+      <th>${x.sex}</th>
+      <th>${x.born}</th>
+      <th>${x.died}</th>
+      <th>${x.died - x.born}</th>
+      <th>${Math.ceil(x.died / 100)}</th>
+  </tr>`).join('');
