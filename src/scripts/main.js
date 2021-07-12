b7940f6027e39014e1a2bb3460feb6d6b936335c
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-12 15:05:47 +0300

    task is completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,30 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const row = document.createElement('tr');
+  const personName = document.createElement('td');
+  const personGender = document.createElement('td');
+  const personBorn = document.createElement('td');
+  const personDied = document.createElement('td');
+  const personAge = document.createElement('td');
+  const personCentury = document.createElement('td');
+
+  personName.textContent = person.name;
+  personGender.textContent = person.sex === 'm' ? 'male' : 'female';
+  personBorn.textContent = `${person.born}`;
+  personDied.textContent = `${person.died}`;
+  personAge.textContent = `${person.died - person.born}`;
+  personCentury.textContent = `${Math.ceil(person.died / 100)}`;
+
+  row.append(personName);
+  row.append(personGender);
+  row.append(personBorn);
+  row.append(personDied);
+  row.append(personAge);
+  row.append(personCentury);
+
+  table.append(row);
+});
