f4fae58a4d81bfade27073a14cabcb5d775e1c8f
herkoss <herkodmytro@gmail.com>
2021-07-20 12:05:46 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,23 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+
+'use strict';
+
+// write code here
+
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+
+const a = 10;
+
+let b = 'checkruns';
+
+b = false;
