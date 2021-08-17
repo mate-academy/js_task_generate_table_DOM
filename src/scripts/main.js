4522ac19b2ee62c63982800f99ff3382acbcdd6b
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-17 22:07:44 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,19 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => {
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `;
+
+  table.append(row);
+});
