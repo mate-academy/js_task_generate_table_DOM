acd9c25bfb802d5851aa06883760e246997fb4ca
Olesya Parfylo <olesya.parfylo@gmail.com>
2021-08-18 18:32:07 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,28 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+function createCell(item, itemRow) {
+  const cell = document.createElement('th');
+
+  cell.textContent = item;
+
+  itemRow.append(cell);
+}
+
+function createTable(person) {
+  const row = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const table = document.querySelector('.dashboard');
+
+  createCell(person.name, row);
+  createCell(person.sex, row);
+  createCell(person.born, row);
+  createCell(person.died, row);
+  createCell(age, row);
+  createCell(century, row);
+
+  table.append(row);
+}
+
+people.map(createTable);
