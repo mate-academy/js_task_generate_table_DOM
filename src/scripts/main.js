57118555c8b48e647cdf2ff87e307090e45aa1c6
Roman Minchenko <minchenko.poma@gmail.com>
2021-08-25 10:45:30 +0300

    generate-table-DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,33 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+const dashboard = document.querySelector('.dashboard');
+
+people.forEach(element => {
+  const age = element.died - element.born;
+  const century = Math.ceil(element.died / 100);
+  const person = document.createElement('tr');
+  let gender;
+
+  dashboard.append(person);
+
+  if (element.sex === 'm') {
+    gender = 'male';
+  } else if (element.sex === 'f') {
+    gender = 'female';
+  };
+
+  person.innerHTML = `
+    <tr>
+      <th>${element.name}</th>
+      <th>${gender}</th>
+      <th>${element.born}</th>
+      <th>${element.died}</th>
+      <th>${age}</th>
+      <th>${century}</th>
+    </tr>
+  `;
+
+  return element;
+});
