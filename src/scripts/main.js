bed203c8cbc571b17fd069034ff83aafc8c873a2
westone <west0ne@MacBook-Pro-Oleg.local>
2021-09-17 13:49:11 +0300

    some changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,23 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-// console.log(people); // you can remove it
+console.log(people); // you can remove it
 
-const str = document.querySelector('.dashboard');
+const table = document.querySelector('.dashboard');
 
 people.forEach((person) => {
   const age = person.died - person.born;
   const century = Math.ceil(person.died / 100);
 
-  str.insertAdjacentHTML('beforeend', `
+  if (person.sex === 'f') {
+    person.sex = 'Female';
+  }
+
+  if (person.sex === 'm') {
+    person.sex = 'Male';
+  }
+
+  table.insertAdjacentHTML('beforeend', `
     <td>${person.name}</td>
     <td>${person.sex}</td>
     <td>${person.born}</td>
