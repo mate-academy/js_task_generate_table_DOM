f1eac45ee9438476aa8f039a87fd3e3d51f618c9
bohdan_kochetov <bodja1208@gmail.com>
2021-09-30 14:24:17 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,35 +3,24 @@
 const people = require('./lib/people');
 
 const newPeople = people.map(person => ({ ...person }));
+const table = document.querySelector('table');
 
-newPeople.map(person => {
-  delete person.fatherName;
-  delete person.motherName;
-  delete person.slug;
-  person.age = person.died - person.born;
-  person.century = Math.ceil(person.died / 100);
-
-  if (person.sex === 'm') {
-    person.sex = 'Male';
-  }
-
-  if (person.sex === 'f') {
-    person.sex = 'Female';
-  }
-});
+newPeople
+  .map(person => {
+    delete person.fatherName;
+    delete person.motherName;
+    delete person.slug;
+    person.age = person.died - person.born;
+    person.century = Math.ceil(person.died / 100);
+    person.sex = (person.sex === 'm') ? 'Male' : 'Female';
+  });
 
 for (const person of newPeople) {
-  const tbody = document.querySelector('tbody');
-  const tr = document.querySelector('tr');
-  const trClone = tr.cloneNode(true);
-  const th = trClone.querySelectorAll('th');
+  const row = table.insertRow();
 
-  th[0].innerText = person.name;
-  th[1].innerText = person.sex;
-  th[2].innerText = person.born;
-  th[3].innerText = person.died;
-  th[4].innerText = person.age;
-  th[5].innerText = person.century;
+  for (const data in person) {
+    const call = row.insertCell();
 
-  tbody.append(trClone);
+    call.innerText = person[data];
+  }
 }
