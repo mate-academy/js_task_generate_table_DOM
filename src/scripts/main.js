94e0c58b58ddcde75d1be3e7c800a7e488d8b174
Dina Samoilova <s13.dina@gmail.com>
2021-11-03 21:56:17 +0200

    solution after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,26 +10,14 @@ const table = document.querySelector('.dashboard').firstElementChild;
 for (const person of people) {
   const row = document.createElement('tr');
 
-  const cellFirst = document.createElement('td');
-  const cellSecond = document.createElement('td');
-  const cellThird = document.createElement('td');
-  const cellFourth = document.createElement('td');
-  const cellFifth = document.createElement('td');
-  const cellSixth = document.createElement('td');
-
-  cellFirst.textContent = person.name;
-  cellSecond.textContent = person.sex === 'm' ? 'Male' : 'Female';
-  cellThird.textContent = person.born;
-  cellFourth.textContent = person.died;
-  cellFifth.textContent = person.died - person.born;
-  cellSixth.textContent = Math.ceil(person.died / 100);
-
-  row.append(cellFirst);
-  row.append(cellSecond);
-  row.append(cellThird);
-  row.append(cellFourth);
-  row.append(cellFifth);
-  row.append(cellSixth);
+  row.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `;
 
   table.append(row);
 }
