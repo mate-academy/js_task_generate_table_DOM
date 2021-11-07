be98c9360232dfe3cd3fa159628322d4bcaf90c8
Yuriy Titov <yuranus@gmail.com>
2021-11-07 08:51:10 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,36 @@
 'use strict';
 
 const people = require('./lib/people');
+const rows = people
+  .map(person => {
+    const tr = document.createElement('tr');
+    const td = `
+      <td>
+        ${person.name}
+      </td>
+      <td>
+        ${person.sex}
+      </td>
+      <td>
+        ${person.born}
+      </td>
+      <td>
+        ${person.died}
+      </td>
+      <td>
+        ${person.died - person.born}
+      </td>
+      <td>
+        ${Math.ceil(person.died / 100)}
+      </td>`;
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+    tr.innerHTML = td;
 
-// write your code here
+    return tr;
+  });
+const table = document
+  .querySelector('.dashboard');
+
+rows
+  .map(row =>
+    table.append(row));
