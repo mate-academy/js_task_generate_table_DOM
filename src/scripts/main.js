e36af3ac066c478da38d8ec9b403b122875dad46
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-11-30 12:30:19 +0200

    Solution js_task_generate_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,24 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const allTables = document.querySelector('.dashboard');
+
+for (const person of people) {
+  if (person.sex === 'm') {
+    person.sex = 'Male';
+  } else {
+    person.sex = 'Female';
+  }
+
+  allTables.insertAdjacentHTML('beforeend', `
+  <tr>
+  <td>${person.name}</td>
+  <td>${person.sex}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.died / 100)}</td>
+</tr>
+  `);
+}
