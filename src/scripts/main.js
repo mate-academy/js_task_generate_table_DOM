3893106f73a67cf9cd6a6d294b198c80a6e6bc02
Vasiliy <voronvasia@gmail.com>
2021-12-08 00:35:50 +0200

    task complite

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,18 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.querySelector('.dashboard');
+const tbody = dashboard.querySelector('tbody');
 
-// write your code here
+const arr = people.map(item =>
+  `<tr>
+    <td>${item.name}</td>
+    <td>${item.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${item.born}</td>
+    <td>${item.died}</td>
+    <td>${item.died - item.born}</td>
+    <td>${Math.ceil(item.died / 100)}</td>
+  </tr>`
+).join('');
+
+tbody.insertAdjacentHTML('beforeend', arr);
