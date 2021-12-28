0129dcd3ff3ef91ab481327e070e0096f27ef2a5
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-12-28 15:32:49 +0200

    Mentor solution point of view is applied

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,21 +5,30 @@ const people = require('./lib/people');
 for (const person of people) {
   const tableRow = document.createElement('tr');
 
-  const fullName = document.createElement('td');
-  const sex = document.createElement('td');
-  const born = document.createElement('td');
-  const died = document.createElement('td');
-  const age = document.createElement('td');
-  const century = document.createElement('td');
+  tableRow.insertAdjacentHTML('afterbegin', `
+  <td>${person.name}</td>
+  <td>${person.sex}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.died / 100)}</td>
+  `);
 
-  fullName.innerHTML = person.name;
-  sex.innerText = person.sex;
-  born.innerText = person.born;
-  died.innerHTML = person.died;
-  age.innerText = person.died - person.born;
-  century.innerText = Math.ceil(person.died / 100);
+  // const fullName = document.createElement('td');
+  // const sex = document.createElement('td');
+  // const born = document.createElement('td');
+  // const died = document.createElement('td');
+  // const age = document.createElement('td');
+  // const century = document.createElement('td');
 
-  tableRow.append(fullName, sex, born, died, age, century);
+  // fullName.innerHTML = person.name;
+  // sex.innerText = person.sex;
+  // born.innerText = person.born;
+  // died.innerHTML = person.died;
+  // age.innerText = person.died - person.born;
+  // century.innerText = Math.ceil(person.died / 100);
+
+  // tableRow.append(fullName, sex, born, died, age, century);
 
   const table = document.querySelector('.dashboard');
 
