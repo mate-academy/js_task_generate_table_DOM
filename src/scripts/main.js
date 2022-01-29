b66c0e4a18a337e142b26340c6ec8584f88404f1
Oleh Rak <rakole86@gmail.com>
2022-01-29 15:36:22 +0200

    generate_table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,28 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const nevarr = people.map(el => {
+  return `<tr>
+ <td>${el.name}</td>
+ <td>${el.sex === 'f' ? 'Female' : 'Male'}</td>
+ <td>${el.born}</td>
+ <td>${el.died}</td>
+ <td>${el.died - el.born}</td>
+ <td>${Math.ceil(el.died / 100)}</td>
+ </tr>
+ `;
+});
+
+const table = document.querySelector('.dashboard');
+
+table.innerHTML = `
+ <tr>
+ <th>Name</th>
+ <th>Gender</th>
+ <th>Born</th>
+ <th>Died</th>
+ <th>Age</th>
+ <th>Century</th>
+</tr>
+${nevarr.join('')}
+`;
