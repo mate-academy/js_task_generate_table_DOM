e3f0f3fb3b0f675a8db913ce6c806998be02422f
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-30 18:19:34 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,18 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+// write your code her
+const table = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const row = `<tr>
+                <td>${person.name}</td>
+                <td>${person.sex}</td>
+                <td>${person.born}</td>
+                <td>${person.died}</td>
+                <td>${person.died - person.born}</td>
+                <td>${Math.ceil(person.died / 100)}</td>
+              </tr>`;
+
+  table.children[0].insertAdjacentHTML('beforeend', row);
+}
