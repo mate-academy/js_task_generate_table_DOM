dc13f959d60669f70b65ac3535ae2b475894279e
Oleh Rak <rakole86@gmail.com>
2022-02-01 14:47:08 +0200

    generate_table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,19 +5,18 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-const nevarr = people.map(el => {
-  return `
-  <tr>
- <td>${el.name}</td>
- <td>${el.sex === 'f' ? 'Female' : 'Male'}</td>
- <td>${el.born}</td>
- <td>${el.died}</td>
- <td>${el.died - el.born}</td>
- <td>${Math.ceil(el.died / 100)}</td>
- </tr>
- `;
-});
-
 const table = document.querySelector('tbody');
 
-table.insertAdjacentHTML('beforeend', nevarr.join(' '));
+people.forEach(el => {
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
+  <td>${el.name}</td> 
+  <td>${el.sex === 'f' ? 'Female' : 'Male'}</td>
+  <td>${el.born}</td> 
+  <td>${el.died}</td> 
+  <td>${el.died - el.born}</td> 
+  <td>${Math.ceil(el.died / 100)}</td> 
+  `;
+  table.append(row);
+});
