db0b7d8fa23c61677e446cfc43f918dc28d1a779
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-03 20:59:50 +0200

    add person in table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,24 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const personName = person.name;
+  const gender = (person.sex === 'm') ? 'Male' : 'Female';
+  const born = person.born;
+  const died = person.died;
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const arr = [personName, gender, born, died, age, century];
+  const tr = document.createElement('tr');
+
+  for (const key of arr) {
+    const th = document.createElement('th');
+
+    th.textContent = key;
+    tr.append(th);
+    table.append(tr);
+  }
+});
