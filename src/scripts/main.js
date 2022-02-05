f9f4f82d91edd7cf04f730de2c288605462fad20
Sasha Pavlenko <380971520240@gmail.com>
2022-02-05 22:43:17 +0200

    Gender cell was changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,10 +10,17 @@ const table = document.querySelector('.dashboard tbody');
 for (const person of people) {
   const age = person.died - person.born;
   const century = Math.ceil(person.died / 100);
+  let sex = '';
+
+  if (person.sex === 'm') {
+    sex = 'Male';
+  } else {
+    sex = 'Female';
+  }
 
   table.insertAdjacentHTML('beforeend', `
     <td>${person.name}</td>
-    <td>${person.sex}</td>
+    <td>${sex}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
     <td>${age}</td>
