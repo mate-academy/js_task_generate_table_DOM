7c9b28155896e1ba6bc36f68006b64bf69fcf1ee
KristinaTar <kristina.tarashchuk@gmail.com>
2022-02-13 18:01:30 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,16 @@
 'use strict';
 
 const people = require('./lib/people');
-const tableRef = document.getElementsByClassName('dashboard')[0];
+const tableRow = document.querySelector('tr');
 
 for (const person of people) {
-  const newRow = tableRef.insertRow(-1);
-
-  newRow.insertAdjacentHTML('beforeend', '<td>' + `${person.name}` + '</td>');
-
-  newRow.insertAdjacentHTML('beforeend', '<td>'
-  + `${person.sex === 'm' ? 'Male' : 'Female'}` + '</td>');
-  newRow.insertAdjacentHTML('beforeend', '<td>' + `${person.born}` + '</td>');
-  newRow.insertAdjacentHTML('beforeend', '<td>' + `${person.died}` + '</td>');
-
-  newRow.insertAdjacentHTML('beforeend', '<td>'
-  + `${person.died - person.born}` + '</td>');
-
-  newRow.insertAdjacentHTML('beforeend', '<td>'
-  + `${Math.ceil(person.died / 100)}` + '</td>');
+  tableRow.insertAdjacentHTML('afterend', `
+  <tr>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+</tr>`);
 }
