2d03c2b8c58312b745d71118c9b97d754b766161
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-19 17:11:52 +0200

    A mistake with row creating was fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,69 +2,12 @@
 
 const people = require('./lib/people');
 
-/* const people = [
-  {
-    name: 'Carolus Haverbeke',
-    sex: 'm',
-    born: 1832,
-    died: 1905,
-    fatherName: 'Carel Haverbeke',
-    motherName: 'Maria van Brussel',
-    slug: 'carolus-haverbeke-1832',
-  },
-  {
-    name: 'Carolus Haverbeke',
-    sex: 'm',
-    born: 1832,
-    died: 1905,
-    fatherName: 'Carel Haverbeke',
-    motherName: 'Maria van Brussel',
-    slug: 'carolus-haverbeke-1832',
-  },
-  {
-    name: 'Carolus Haverbeke',
-    sex: 'm',
-    born: 1832,
-    died: 1905,
-    fatherName: 'Carel Haverbeke',
-    motherName: 'Maria van Brussel',
-    slug: 'carolus-haverbeke-1832',
-  },
-  {
-    name: 'Carolus Haverbeke',
-    sex: 'm',
-    born: 1832,
-    died: 1905,
-    fatherName: 'Carel Haverbeke',
-    motherName: 'Maria van Brussel',
-    slug: 'carolus-haverbeke-1832',
-  },
-  {
-    name: 'Carolus Haverbeke',
-    sex: 'm',
-    born: 1832,
-    died: 1905,
-    fatherName: 'Carel Haverbeke',
-    motherName: 'Maria van Brussel',
-    slug: 'carolus-haverbeke-1832',
-  },
-  {
-    name: 'Carolus Haverbeke',
-    sex: 'm',
-    born: 1832,
-    died: 1905,
-    fatherName: 'Carel Haverbeke',
-    motherName: 'Maria van Brussel',
-    slug: 'carolus-haverbeke-1832',
-  },
-]; */
-
 const myTable = document.querySelector('.dashboard');
 
-const tableRow = document.createElement('tr');
-
 for (const person of people) {
-  tableRow.insertAdjacentHTML('beforeend', `
+  const tableRow = document.createElement('tr');
+
+  tableRow.insertAdjacentHTML('afterbegin', `
     <td> ${person.name} </td>
     <td> ${person.sex} </td>
     <td> ${person.born} </td>
