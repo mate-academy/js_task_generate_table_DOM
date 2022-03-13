30e59f5dfa1140549e880eec289224248c12744d
Yuri Taraday <KaIIRu3@gmail.com>
2022-03-13 22:33:49 +0200

    solv

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,28 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const result = [...people];
 
-// write your code here
+const finalaze = result.map((el) => {
+  const tableRows = document.createElement('tr');
+
+  const age = el.died - el.born;
+  const century = Math.ceil(el.died / 100);
+
+  tableRows.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <th>${el.name}</th>
+    <th>${el.sex === 'f' ? 'Female' : 'Male'}</th>
+    <th>${el.born}</th>
+    <th>${el.died}</th>
+    <th>${age}</th>
+    <th>${century}</th>
+    `
+  );
+
+  table.append(tableRows);
+});
