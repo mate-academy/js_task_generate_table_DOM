a3961fca3634ebf855eab1b9dea98a947d4652b2
Mykhailo Ianush <yanushmisha@gmail.com>
2022-03-17 18:56:02 +0200

    use insertAdjacentHTML

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,15 @@
 'use strict';
 
 const people = require('./lib/people');
-const table = document.querySelector('.dashboard');
-let index = 1;
+const table = document.querySelector('.dashboard > tbody');
 
 people.forEach(person => {
-  const row = table.insertRow(index++);
-  const cell1 = row.insertCell(0);
-  const cell2 = row.insertCell(1);
-  const cell3 = row.insertCell(2);
-  const cell4 = row.insertCell(3);
-  const cell5 = row.insertCell(4);
-  const cell6 = row.insertCell(5);
-
-  cell1.innerHTML = person.name;
-  cell2.innerHTML = person.sex === 'm' ? 'Male' : 'Female';
-  cell3.innerHTML = person.born;
-  cell4.innerHTML = person.died;
-  cell5.innerHTML = person.died - person.born;
-  cell6.innerHTML = Math.ceil(person.died / 100);
+  table.insertAdjacentHTML('beforeend', `<tr>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  </tr>`);
 });
