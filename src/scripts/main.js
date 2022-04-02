d1ce770b3335e17a6d58454397691fa3e8a9134c
Alexander Skidan <xpromt@gmail.com>
2022-04-02 18:12:20 +0300

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,24 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// console.log(people); // you can remove it
 
-// write your code here
+const tab = document.querySelector('.dashboard');
+
+for (const person of people) {
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+
+  const personTabRow = document.createElement('tr');
+
+  personTabRow.insertAdjacentHTML('afterbegin', `
+    <th>${person.name}</th>
+    <th>${person.sex === 'm' ? 'Male' : 'Female'}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.age}</th>
+    <th>${person.century}</th>
+  `);
+
+  tab.append(personTabRow);
+}
