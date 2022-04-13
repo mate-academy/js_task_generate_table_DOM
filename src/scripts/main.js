268dcbefc3eb7877a9a014c47728993f8e420046
Max Shylenko <maxshylenko@gmail.com>
2022-04-13 15:42:16 +0200

    generate table DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,41 @@
+/* eslint-disable semi */
 'use strict';
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  for (let i = 1; i <= 6; i++) {
+    const cell = document.createElement('th');
+
+    switch (i) {
+      case 1:
+        cell.innerText = person.name;
+        break;
+      case 2:
+        cell.innerText = person.sex === 'm'
+          ? 'Male'
+          : 'Female';
+        break;
+      case 3:
+        cell.innerText = person.born;
+        break;
+      case 4:
+        cell.innerText = person.died;
+        break;
+      case 5:
+        cell.innerText = person.died - person.born;
+        break;
+      case 6:
+        cell.innerText = Math.ceil(person.died / 100);
+        break;
+    }
+
+    row.append(cell);
+  }
+
+  table.append(row);
+}
