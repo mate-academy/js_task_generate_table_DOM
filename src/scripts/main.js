ddec6ffc8956bf6f9db25e59bcc0be487afabb60
Maksym Yaremenko <restartermord123@ukr.net>
2022-04-13 19:58:14 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,16 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+const tablet = document.querySelector('.dashboard').children[0];
+
+people.forEach(person => tablet.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${person.name}</th>
+    <td>${person.sex}</th>
+    <td>${person.born}</th>
+    <td>${person.died}</th>
+    <td>${person.died - person.born}</th>
+    <td>${Math.ceil(person.died / 100)}</th>
+  </tr>
+`));
