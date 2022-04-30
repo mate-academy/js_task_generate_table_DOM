6e4a260e5e17c0bed1b391f167ab67c30b07ec75
Igor Stasiv <igorstsv@gmail.com>
2022-04-30 18:14:17 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,31 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
+const headers = [...document.querySelectorAll('th')];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const setHeaderData = (person, headerName) => {
+  switch (headerName) {
+    case 'gender':
+      return person.sex === 'm' ? 'Male' : 'Female';
+    case 'age':
+      return person.died - person.born;
+    case 'century':
+      return Math.ceil(person.died / 100);
+    default:
+      return person[headerName];
+  }
+};
 
-// write your code here
+people.forEach(person => {
+  const tr = document.createElement('tr');
+
+  headers.forEach(cell => {
+    const td = document.createElement('td');
+
+    td.innerText = setHeaderData(person, cell.innerText.toLowerCase());
+    tr.append(td);
+  });
+
+  table.append(tr);
+});
