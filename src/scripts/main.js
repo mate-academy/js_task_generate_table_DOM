413ba32102da2098283c1fa8978ddcd00f17ef30
Nazar Muliar <nazarmuliar26@gmail.com>
2022-05-13 11:34:27 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,28 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.map(person => (person.sex = person.sex === 'm' ? 'Male' : 'Female'));
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+table.append(document.createElement('tr'));
+
+for (const person of people) {
+  table.append(document.createElement('tr'));
+
+  const personData = Object.values(person).slice(0, 4);
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  personData.push(age, century);
+
+  for (let i = 0; i <= 5; i++) {
+    const currentRow = table.lastChild;
+
+    currentRow.append(document.createElement('td'));
+
+    const ceil = currentRow.lastChild;
+
+    ceil.innerText = personData[i];
+  }
+}
