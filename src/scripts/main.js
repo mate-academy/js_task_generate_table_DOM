406bc86061176e6c933d3f4e8daf52e87b15d3c9
Katya Suvorova <katerinasuvorova97@gmail.com>
2022-05-25 20:08:40 +0300

    Fixes after code rewiew

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,41 +9,25 @@ console.log(people); // you can remove it
 const table = document.querySelector('.dashboard').children[0];
 
 for (const person of people) {
-  const namePerson = person.name;
-  const gender = person.sex;
-  const born = person.born;
-  const died = person.died;
-  const age = person.died - person.born;
-  const century = Math.ceil(person.died / 100);
-
   const trItem = document.createElement('tr');
 
   table.append(trItem);
 
-  const tdName = document.createElement('td');
-  const tdGender = document.createElement('td');
-  const tdBorn = document.createElement('td');
-  const tdDied = document.createElement('td');
-  const tdAge = document.createElement('td');
-  const tdCentury = document.createElement('td');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const data
+   = [person.name, person.sex, person.born, person.died, age, century];
 
-  tdName.textContent = namePerson;
+  for (const field of data) {
+    const td = document.createElement('td');
 
-  if (gender === 'm') {
-    tdGender.textContent = 'Male';
-  } else {
-    tdGender.textContent = 'Female';
+    if (field === 'm') {
+      td.textContent = 'Male';
+    } else if (field === 'f') {
+      td.textContent = 'Female';
+    } else {
+      td.textContent = field;
+    }
+    trItem.append(td);
   }
-
-  tdBorn.textContent = born;
-  tdDied.textContent = died;
-  tdAge.textContent = age;
-  tdCentury.textContent = century;
-
-  trItem.append(tdName);
-  trItem.append(tdGender);
-  trItem.append(tdBorn);
-  trItem.append(tdDied);
-  trItem.append(tdAge);
-  trItem.append(tdCentury);
 }
