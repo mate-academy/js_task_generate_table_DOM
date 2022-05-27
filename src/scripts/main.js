819c5575aa91ff49645f0f4fdefbd15257368e22
Alina Yermolenko <alina.yermolenko27@gmail.com>
2022-05-27 19:48:22 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const people = require('./lib/people');
+const tbody = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const tr = document.createElement('tr');
+  const firstName = document.createElement('th');
+  const gender = document.createElement('th');
+  const born = document.createElement('th');
+  const died = document.createElement('th');
+  const age = document.createElement('th');
+  const century = document.createElement('th');
 
-// write your code here
+  tbody.appendChild(tr);
+  tr.appendChild(firstName);
+  tr.appendChild(gender);
+  tr.appendChild(born);
+  tr.appendChild(died);
+  tr.appendChild(age);
+  tr.appendChild(century);
+
+  firstName.append(person.name);
+  gender.append(person.sex);
+  born.append(person.born);
+  died.append(person.died);
+  age.append(person.died - person.born);
+  century.append(Math.ceil(person.died / 100));
+});
