7872ef71ef688d2e1d7f802fc43b5e53bb04afc6
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-06-16 23:46:07 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,40 +2,17 @@
 
 const people = require('./lib/people');
 
-const table = document.querySelector('.dashboard');
+const table = document.querySelector('.dashboard').children[0].children[0];
 
 for (let i = 0; i < people.length; i++) {
-  const row = document.createElement('tr');
-
-  const fullName = document.createElement('td');
-
-  fullName.textContent = people[i].name;
-  row.appendChild(fullName);
-
-  const sex = document.createElement('td');
-
-  sex.textContent = (people[i].sex === 'm') ? 'Male' : 'Female';
-  row.appendChild(sex);
-
-  const born = document.createElement('td');
-
-  born.textContent = people[i].born;
-  row.appendChild(born);
-
-  const died = document.createElement('td');
-
-  died.textContent = people[i].died;
-  row.appendChild(died);
-
-  const age = document.createElement('td');
-
-  age.textContent = people[i].died - people[i].born;
-  row.appendChild(age);
-
-  const century = document.createElement('td');
-
-  century.textContent = Math.ceil(people[i].died / 100);
-  row.appendChild(century);
-
-  table.appendChild(row);
+  table.insertAdjacentHTML(`afterend`, `
+    <tr>
+      <td>${people[i].name}</td>
+      <td>${people[i].sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${people[i].born}</td>
+      <td>${people[i].died}</td>
+      <td>${people[i].died - people[i].born}</td>
+      <td>${Math.ceil(people[i].died / 100)}</td>
+    </tr>
+  `);
 }
