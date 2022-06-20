774adddcb2badd0eea52f3bfb4a5be539095084d
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-20 14:05:44 +0200

    result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,27 @@
 'use strict';
 
-const people = require('./lib/people');
+const people = require('./lib/people').sort((a, b) => (a.born - b.died));
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('tr');
+
+for (let i = 0; i < people.length; i++) {
+  const us = people[i].name;
+  const sex = people[i].sex;
+  const born = people[i].born;
+  const died = people[i].died;
+
+  table.insertAdjacentHTML(
+    'afterend',
+    `<tr>
+      <th>${us}</th>
+      <th>${sex.toUpperCase()}</th>
+      <th>${born}</th>
+      <th>${died}</th>
+      <th>${died - born}</th>
+      <th>${Math.ceil(died / 100)}</th>
+    </tr>`
+  );
+}
