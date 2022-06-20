b2cbd3c79872ef3174298ca78b555aa822d849b4
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-06-20 15:56:21 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,25 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  const personName = document.createElement('td');
+  const gender = document.createElement('td');
+  const born = document.createElement('td');
+  const died = document.createElement('td');
+  const age = document.createElement('td');
+  const centry = document.createElement('td');
+
+  personName.textContent = person.name;
+  gender.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  born.textContent = person.born;
+  died.textContent = person.died;
+  age.textContent = person.died - person.born;
+  centry.textContent = Math.ceil(person.died / 100);
+
+  row.append(personName, gender, born, died, age, centry);
+  table.append(row);
+}
