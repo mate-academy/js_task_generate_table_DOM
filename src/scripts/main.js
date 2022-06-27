7998ab1aaf67a8c44c8668c27f6cd1452c173b27
Pavel Maistrenko <maystrenko.p@gmail.com>
2022-06-27 16:22:17 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,43 +3,21 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
 // write your code here
 const table = document.querySelector('tbody');
 
-for (let i = 0; i < people.length; i++) {
-  const tr = document.createElement('tr');
+for (const person of people) {
+  const userTable = document.createElement('tr');
 
-  table.append(tr);
+  table.append(userTable);
 
-  for (let k = 0; k < 6; k++) {
-    const td = document.createElement('td');
-
-    tr.append(td);
-
-    if (k === 0) {
-      td.innerText = people[i].name;
-    }
-
-    if (k === 1) {
-      (people[i].sex === 'm') ? td.innerText = 'male' : td.innerText = 'female';
-    }
-
-    if (k === 2) {
-      td.innerText = people[i].born;
-    }
-
-    if (k === 3) {
-      td.innerText = people[i].died;
-    }
-
-    if (k === 4) {
-      td.innerText = people[i].died - people[i].born;
-    }
-
-    if (k === 5) {
-      td.innerText = Math.ceil(people[i].died / 100);
-    }
-  }
+  userTable.insertAdjacentHTML('beforeend', `
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.age}</td>
+    <td>${person.century}</td>
+  `);
 }
