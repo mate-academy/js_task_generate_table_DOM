a26ad43a0db8fd1f3c4cd7a118acc4eef3c83408
Konstantin <ivanchenkomypost@gmail.com>
2022-06-28 15:10:38 +0300

    Task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,21 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const human of people) {
+  const row = document.createElement('tr');
+
+  for (let i = 0; i < 6; i++) {
+    row.append(document.createElement('td'));
+  }
+
+  row.children[0].innerText = human.name;
+  row.children[1].innerText = human.sex === 'm' ? 'Male' : 'Female';
+  row.children[2].innerText = human.born;
+  row.children[3].innerText = human.died;
+  row.children[4].innerText = human.died - human.born;
+  row.children[5].innerText = Math.ceil(human.died / 100);
+
+  table.append(row);
+}
