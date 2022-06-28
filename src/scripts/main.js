6549127e94a2cdbca3cc9665c3bcbe5f279af7a7
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-28 14:15:31 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,22 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
 // write your code here
+const dashboard = document.querySelector(`.dashboard`);
+
+people.forEach(el => {
+  const table = document.createElement(`tr`);
+  const century = Math.ceil(el.died / 100);
+
+  table.innerHTML
+     = `
+      <td>${el.name}</td>
+      <td>${(el.sex === 'm') ? 'Male' : 'Female'}</td>
+      <td>${el.born}</td>
+      <td>${el.died}</td>
+      <td>${el.died - el.born}</td>
+      <td>${century}</td>
+    `;
+  dashboard.appendChild(table);
+});
