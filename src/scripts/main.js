30c808c61530b0bf27902475b61a4e781fd84ca9
Stanislav Bzhezinskyi <stasiva28@gmail.com>
2022-07-06 12:55:58 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,44 @@
 'use strict';
 
 const people = require('./lib/people');
+const mainTab = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const newRow = document.createElement('tr');
 
-// write your code here
+  mainTab.children[0].append(newRow);
+
+  const nameRow = document.createElement('td');
+
+  nameRow.innerText = person.name;
+  newRow.append(nameRow);
+
+  const genderRow = document.createElement('td');
+
+  if (person.sex === 'm') {
+    genderRow.innerText = 'Male';
+  } else {
+    genderRow.innerText = 'Female';
+  }
+  newRow.append(genderRow);
+
+  const bornRow = document.createElement('td');
+
+  bornRow.innerText = person.born;
+  newRow.append(bornRow);
+
+  const diedRow = document.createElement('td');
+
+  diedRow.innerText = person.died;
+  newRow.append(diedRow);
+
+  const ageRow = document.createElement('td');
+
+  ageRow.innerText = person.died - person.born;
+  newRow.append(ageRow);
+
+  const centuryRow = document.createElement('td');
+
+  centuryRow.innerText = Math.ceil(person.died / 100);
+  newRow.append(centuryRow);
+}
