d2905f8a2af54d262102ca9abdcf8b4bd2ab986c
Max Shylenko <maxshylenko@gmail.com>
2022-07-07 18:54:11 +0200

    generate table DOM done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,19 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const tableRow = document.createElement('tr');
+
+  tableRow.innerHTML = `
+     <th>${person.name}</th>
+     <th>${person.sex === 'm' ? 'Male' : 'Female'}</th>
+     <th>${person.born}</th>
+     <th>${person.died}</th>
+     <th>${person.died - person.born}</th>
+     <th>${Math.ceil(person.died / 100)}</th>
+  `;
+
+  table.append(tableRow);
+}
