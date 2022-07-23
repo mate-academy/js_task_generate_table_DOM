bb4b675b4e610802baa8397b04ea19d1e22ecb97
Olha Stakhova <olha.stakhova@gmail.com>
2022-07-23 17:19:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,25 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const tableRef = document.querySelector('tbody');
+
+for (const person of people) {
+  const template = `<tr>
+    <td>${person.name}</td>
+    <td>${getsexPeople(person.sex)}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  </tr>`;
+
+  tableRef.insertAdjacentHTML('beforeend', template);
+}
+
+function getsexPeople(str) {
+  if (str === 'm') {
+    return 'Male';
+  } else {
+    return 'Female';
+  }
+}
