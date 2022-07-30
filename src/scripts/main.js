5380045653c4cc26f23d30f552af21553a402af9
Vitaliy Bushovskiy <Bushovskiy@gmail.com>
2022-07-30 17:50:35 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,25 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+let i = 1;
+
+people.forEach(elem => {
+  const tr = document.createElement('tr');
+
+  table.appendChild(tr);
+  tr.appendChild(document.createElement('td'));
+  tr.appendChild(document.createElement('td'));
+  tr.appendChild(document.createElement('td'));
+  tr.appendChild(document.createElement('td'));
+  tr.appendChild(document.createElement('td'));
+  tr.appendChild(document.createElement('td'));
+  table.rows[i].cells[0].textContent = elem.name;
+  table.rows[i].cells[1].innerHTML = elem.sex;
+  table.rows[i].cells[2].textContent = elem.born;
+  table.rows[i].cells[3].innerHTML = elem.died;
+  table.rows[i].cells[4].textContent = elem.died - elem.born;
+  table.rows[i].cells[5].innerHTML = Math.ceil(elem.died / 100);
+  i++;
+});
