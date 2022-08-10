1f2d1c136567d69a40d70cad9c8861430e4e85ab
Andrian Melnyk <andrian_m@ukr.net>
2022-08-11 00:11:37 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,35 @@
 'use strict';
 
-const people = require('./lib/people');
+const people = require('./lib/people.json');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(obj => {
+  const row = document.createElement('tr');
 
-// write your code here
+  const firstColumn = document.createElement('td');
+  const secondColumn = document.createElement('td');
+  const thirdColumn = document.createElement('td');
+  const fourthColumn = document.createElement('td');
+  const fifthColumn = document.createElement('td');
+  const sixthColumn = document.createElement('td');
+
+  firstColumn.innerHTML = `${Object.values(obj)[0]}`;
+
+  if (Object.values(obj)[1] === 'm') {
+    secondColumn.innerHTML = 'male';
+  } else {
+    secondColumn.innerHTML = 'female';
+  }
+  thirdColumn.innerHTML = `${Object.values(obj)[2]}`;
+  fourthColumn.innerHTML = `${Object.values(obj)[3]}`;
+  fifthColumn.innerHTML = `${Object.values(obj)[3] - Object.values(obj)[2]}`;
+
+  sixthColumn.innerHTML = `${Math.ceil(
+    Object.values(obj)[3] / 100)}`;
+
+  row.append(firstColumn, secondColumn,
+    thirdColumn, fourthColumn,
+    fifthColumn, sixthColumn);
+
+  table.append(row);
+});
