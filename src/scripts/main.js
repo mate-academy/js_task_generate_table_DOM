f2339e7398ab3c3a3b6e741565813443112fb2a9
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-10 12:40:20 +0200

    Solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,36 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+console.log(Array.isArray(people)); // you can remove it
 
 // write your code here
+
+const table = document.querySelector('.dashboard');
+
+valTableName(people);
+
+function valTableName(arr) {
+  arr.forEach(el => {
+    const tr = document.createElement('tr');
+
+    table.append(tr);
+    tr.appendChild(document.createElement('td')).append(el.name);
+
+    tr.appendChild(document.createElement('td')).append(male(el.sex));
+    tr.appendChild(document.createElement('td')).append(el.born);
+    tr.appendChild(document.createElement('td')).append(el.died);
+    tr.appendChild(document.createElement('td')).append(el.died - el.born);
+
+    tr.appendChild(document.createElement('td')).append(
+      Math.ceil(el.died / 100)
+    );
+  });
+}
+
+function male(person) {
+  if (person === 'm') {
+    return 'Male';
+  } else {
+    return 'Female';
+  }
+}
