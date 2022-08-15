71fcf232a237cebadaab170ebdcb6a061ceb71f8
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-15 10:45:16 +0300

    First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,22 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const personAge = person.died - person.born;
+  const personCentury = Math.ceil(person.died / 100);
+
+  const tableRow = document.createElement('tr');
+
+  tableRow.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${personAge}</td>
+    <td>${personCentury}</td>
+  `;
+
+  table.append(tableRow);
+}
