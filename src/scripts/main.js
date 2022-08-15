d2be2b1e9dc9c223bf0d5e5e9cfb1254c76c1f82
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-15 11:54:25 +0200

    Solution3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,28 +11,38 @@ const table = document.querySelector('.dashboard');
 
 valTableName(people);
 
-function valTableName(arr) {
-  arr.forEach(el => {
-    const tr = document.createElement('tr');
-
-    table.append(tr);
-    tr.appendChild(document.createElement('td')).append(el.name);
-
-    tr.appendChild(document.createElement('td')).append(male(el.sex));
-    tr.appendChild(document.createElement('td')).append(el.born);
-    tr.appendChild(document.createElement('td')).append(el.died);
-    tr.appendChild(document.createElement('td')).append(el.died - el.born);
-
-    tr.appendChild(document.createElement('td')).append(
-      Math.ceil(el.died / 100)
-    );
-  });
-}
-
-function male(person) {
-  if (person === 'm') {
+function male(p) {
+  if (p === 'm') {
     return 'Male';
   } else {
     return 'Female';
   }
+}
+
+function valTableName(arr) {
+  arr.forEach(person => {
+    table.insertAdjacentHTML('beforeend', `
+   <tr>
+     <td>${person.name}</td>
+     <td>${male(person.sex)}</td>
+     <td>${person.born}</td>
+     <td>${person.died}</td>
+     <td>${person.died - person.born}</td>
+     <td>${Math.ceil(person.died / 100)}</td>
+   </tr>
+  `);
+    // const tr = document.createElement('tr');
+
+    // table.append(tr);
+    // tr.appendChild(document.createElement('td')).append(el.name);
+
+    // tr.appendChild(document.createElement('td')).append(male(el.sex));
+    // tr.appendChild(document.createElement('td')).append(el.born);
+    // tr.appendChild(document.createElement('td')).append(el.died);
+    // tr.appendChild(document.createElement('td')).append(el.died - el.born);
+
+    // tr.appendChild(document.createElement('td')).append(
+    //   Math.ceil(el.died / 100)
+    // );
+  });
 }
