674b8f871f32976bf99e93083326bbbbfe624c5f
Yuliia Parashchenko <yuliia.parashchenko@gmail.com>
2022-08-16 16:15:58 +0300

    Generate table DOM task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,33 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+const tableData = people.map(person => ({
+  name: person.name,
+  gender: person.sex === 'm' ? 'Male' : 'Female',
+  born: person.born,
+  died: person.died,
+  age: person.died - person.born,
+  century: Math.ceil(person.died / 100),
+}));
+
+tableData.forEach(person => {
+  const row = document.createElement('tr');
+
+  for (const data in person) {
+    const item = tableCell(person[data]);
+
+    row.append(item);
+  }
+
+  table.append(row);
+});
+
+function tableCell(value) {
+  const cell = document.createElement('td');
+
+  cell.textContent = value;
+
+  return cell;
+}
