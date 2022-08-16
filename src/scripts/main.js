46520ca48d9e9a2c018760bd2c4576c28d97b7eb
Oksana Shvets <ksenyasvatko1993@gmail.com>
2022-08-16 12:23:47 +0200

    Solution5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,7 @@ console.log(Array.isArray(people)); // you can remove it
 
 // write your code here
 
-const table = document.querySelector('.dashboard');
+const table = document.querySelector('.dashboard tbody');
 
 valTableName(people);
 
@@ -20,29 +20,29 @@ function male(p) {
 }
 
 function valTableName(arr) {
-  arr.forEach(el => {
-//     table.insertAdjacentHTML('beforeend', `
-//    <tr>
-//      <td>${person.name}</td>
-//      <td>${male(person.sex)}</td>
-//      <td>${person.born}</td>
-//      <td>${person.died}</td>
-//      <td>${person.died - person.born}</td>
-//      <td>${Math.ceil(person.died / 100)}</td>
-//    </tr>
-//   `);
-    const tr = document.createElement('tr');
+  arr.forEach(person => {
+    table.insertAdjacentHTML('beforeend', `
+       <tr>
+         <td>${person.name}</td>
+         <td>${male(person.sex)}</td>
+         <td>${person.born}</td>
+         <td>${person.died}</td>
+         <td>${person.died - person.born}</td>
+         <td>${Math.ceil(person.died / 100)}</td>
+       </tr>
+      `);
+    // const tr = document.createElement('tr');
 
-    table.append(tr);
-    tr.appendChild(document.createElement('td')).append(el.name);
+    // table.append(tr);
+    // tr.appendChild(document.createElement('td')).append(el.name);
 
-    tr.appendChild(document.createElement('td')).append(male(el.sex));
-    tr.appendChild(document.createElement('td')).append(el.born);
-    tr.appendChild(document.createElement('td')).append(el.died);
-    tr.appendChild(document.createElement('td')).append(el.died - el.born);
+    // tr.appendChild(document.createElement('td')).append(male(el.sex));
+    // tr.appendChild(document.createElement('td')).append(el.born);
+    // tr.appendChild(document.createElement('td')).append(el.died);
+    // tr.appendChild(document.createElement('td')).append(el.died - el.born);
 
-    tr.appendChild(document.createElement('td')).append(
-      Math.ceil(el.died / 100)
-    );
+    // tr.appendChild(document.createElement('td')).append(
+    //   Math.ceil(el.died / 100)
+    // );
   });
 }
