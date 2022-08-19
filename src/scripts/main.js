5790d62f74ee42c2a5bd4b176602056f42987d2d
Denys Topchyi <miliarder97@gmail.com>
2022-08-19 15:09:16 +0300

    tableCreated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+for (const char of people) {
+  table.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${char.name}</td>
+    <td>${char.sex === 'm' ? `Male` : 'Female'}</td>
+    <td>${char.born}</td>
+    <td>${char.died}</td>
+    <td>${char.died - char.born}</td>
+    <td>${Math.ceil(char.died / 100)}</td>
+</tr>
+`);
+}
