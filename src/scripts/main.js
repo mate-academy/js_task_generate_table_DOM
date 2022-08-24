91ea6e7528ce624e3b879e22b0de55172360f534
Elina Bahirova <eli4ka.bagirova@gmail.com>
2022-08-24 15:55:59 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,28 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+const table = document.querySelector('table.dashboard');
+
+for (const person of people) {
+  const tableRow = document.createElement('tr');
+
+  const personData = {};
+
+  personData.name = person.name;
+  personData.genger = person.sex === 'f' ? 'Female' : 'Male';
+  personData.born = person.born;
+  personData.died = person.died;
+  personData.age = person.died - person.born;
+  personData.century = Math.ceil(person.died / 100);
+
+  for (const prop in personData) {
+    const tableData = document.createElement('td');
+
+    tableData.innerText = '' + personData[prop];
+
+    tableRow.append(tableData);
+  }
+
+  table.append(tableRow);
+}
