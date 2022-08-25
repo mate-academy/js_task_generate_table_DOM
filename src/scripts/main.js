6847ba53f64f088e7e00a249d7a617fc94a6ce3d
Dmytro Matsehora <ninjawithoutface@gmail.com>
2022-08-25 13:41:18 +0300

    Solution #2 using insertAdjacentHTML

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,34 +5,22 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
+const dashboard = document.getElementsByClassName('dashboard');
+
 people.forEach(person => {
-  const dashboard = document.getElementsByClassName('dashboard');
   const row = document.createElement('tr');
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
 
-  const cellName = document.createElement('td');
-  const cellSex = document.createElement('td');
-  const cellBorn = document.createElement('td');
-  const cellDied = document.createElement('td');
-  const cellAge = document.createElement('td');
-  const cellCentury = document.createElement('td');
-
-  cellName.textContent = person.name;
-  row.append(cellName);
-
-  cellSex.textContent = person.sex === 'f' ? 'Female' : 'Male';
-  row.append(cellSex);
-
-  cellBorn.textContent = person.born;
-  row.append(cellBorn);
-
-  cellDied.textContent = person.died;
-  row.append(cellDied);
-
-  cellAge.textContent = person.died - person.born;
-  row.append(cellAge);
-
-  cellCentury.textContent = Math.ceil(person.died / 100);
-  row.append(cellCentury);
+  row.insertAdjacentHTML('afterbegin', `
+    <td>${person.name}</td>
+    <td>${gender}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  `);
 
   dashboard[0].append(row);
 });
