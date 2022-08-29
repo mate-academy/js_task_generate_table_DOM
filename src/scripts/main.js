fff91327c39d8214b5e79aa350d0f3d2f3e26f51
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-08-29 10:59:05 +0300

    all done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,29 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard tbody');
+
+valTableName(people);
+
+function male(p) {
+  if (p === 'm') {
+    return 'Male';
+  } else {
+    return 'Female';
+  }
+}
+
+function valTableName(arr) {
+  arr.forEach(person => {
+    table.insertAdjacentHTML('beforeend', `
+       <tr>
+         <td>${person.name}</td>
+         <td>${male(person.sex)}</td>
+         <td>${person.born}</td>
+         <td>${person.died}</td>
+         <td>${person.died - person.born}</td>
+         <td>${Math.ceil(person.died / 100)}</td>
+       </tr>
+      `);
+  });
+}
