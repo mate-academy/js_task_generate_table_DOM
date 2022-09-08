a056c37619a700c45acdafa399ead27ca82b4d97
Oleksii <oleksii@it.com>
2022-09-08 19:24:45 +0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,25 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const tabl = document.querySelector('tbody');
+// console.log(table);
+
+for (const person of people) {
+  tabl.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${person.name}</td>
+    <td>${gender(person)}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  </tr>`);
+}
+
+function gender(pers) {
+  if (pers.sex === 'm') {
+    return 'Male';
+  }
+
+  return 'Female';
+}
