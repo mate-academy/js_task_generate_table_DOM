8b7fea3233d8b899cfab1621c307d064f62a7785
fenderukr <priststoreua@gmail.com>
2022-09-08 18:09:06 +0300

    generateDashboardFromJSON0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// console.log(people); // you can remove it
 
-// write your code here
+people.map(item => {
+  delete item.motherName;
+  delete item.fatherName;
+  delete item.slug;
+
+  item.age = item.died - item.born;
+  item.century = Math.ceil(item.died / 100);
+});
+
+for (const personObj of people) {
+  const row = document.createElement('tr');
+
+  table.append(row);
+
+  for (const person in personObj) {
+    const cell = document.createElement('td');
+
+    row.append(cell);
+    cell.textContent = personObj[person];
+  }
+}
