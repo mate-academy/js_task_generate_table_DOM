8a25c909d898b9ed93269f5fcf329debb10297ed
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-09-12 15:39:33 +0200

    HTML Table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,39 @@
 'use strict';
 
 const people = require('./lib/people');
+const myTable = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
 
-// write your code here
+  cellName.textContent = `${person.name}`;
+
+  const cellGender = document.createElement('td');
+
+  cellGender.textContent = `${person.sex}`;
+
+  const cellBorn = document.createElement('td');
+
+  cellBorn.textContent = `${person.born}`;
+
+  const cellDied = document.createElement('td');
+
+  cellDied.textContent = `${person.died}`;
+
+  const cellAge = document.createElement('td');
+
+  cellAge.textContent = `${person.died - person.born}`;
+
+  const cellCentury = document.createElement('td');
+
+  cellCentury.textContent = `${Math.ceil(person.died / 100)}`;
+
+  row.append(cellName);
+  row.append(cellGender);
+  row.append(cellBorn);
+  row.append(cellDied);
+  row.append(cellAge);
+  row.append(cellCentury);
+  myTable.append(row);
+}
