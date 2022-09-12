3b889f02e06fea32233f4f2cf605a80cbbcf9a52
Inna <dragotaiv@gmail.com>
2022-09-13 00:03:24 +0300

    table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,23 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${gender}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+`;
+
+  table.append(tr);
+}
