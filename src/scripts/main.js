7f648237019efd8274dc0bde9f2ea72cbad11006
Anastasiia Mahalias <anastasiabatychenko@gmail.com>
2022-09-13 17:27:15 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
+const defineGender = gender => (gender === 'f') ? 'Female' : 'Male';
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+for (const person of people) {
+  table.firstElementChild.insertAdjacentHTML('beforeend', `
+    <tr>
+      <th>${person.name}</th>
+      <th>${defineGender(person.sex)}</th>
+      <th>${person.born}</th>
+      <th>${person.died}</th>
+      <th>${person.died - person.born}</th>
+      <th>${Math.ceil(person.died / 100)}</th>
+    </tr>
+  `);
+}
