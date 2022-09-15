486a7d2ad56377ab1ee411ec7dbd7de4f023329a
Kostya Berehoviy <kostyaber3@gmail.com>
2022-09-15 16:59:23 +0300

    Table was Generated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,36 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const tr = document.createElement('tr');
+  const personName = document.createElement('td');
+  const personGender = document.createElement('td');
+  const personBorn = document.createElement('td');
+  const personDied = document.createElement('td');
+  const personAge = document.createElement('td');
+  const personCentury = document.createElement('td');
+
+  personName.innerHTML = person.name;
+
+  if (person.sex === 'm') {
+    personGender.innerHTML = 'Male';
+  }
+
+  if (person.sex === 'f') {
+    personGender.innerHTML = 'Female';
+  }
+
+  personBorn.innerHTML = person.born;
+  personDied.innerHTML = person.died;
+  personAge.innerHTML = person.died - person.born;
+  personCentury.innerHTML = Math.ceil(person.died / 100);
+
+  tr.append(
+    personName, personGender, 
+    personBorn, personDied, 
+    personAge, personCentury
+  );
+  dashboard.append(tr);
+}
