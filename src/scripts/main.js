c7716f96e002c29b9e51a3dec9db4cd7d88d2ab1
Paul Voronin <voron.paul@gmail.com>
2022-09-17 16:08:16 +0300

    ver.1.1 (destructured person object)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,16 +4,17 @@ const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
 for (const person of people) {
-  const age = person.died - person.born;
-  const century = Math.ceil(person.died / 100);
-  const gender = (person.sex === 'm') ? 'Male' : 'Female';
+  const { sex, born, died } = person;
+  const age = died - born;
+  const century = Math.ceil(died / 100);
+  const gender = (sex === 'm') ? 'Male' : 'Female';
 
   table.children[0].insertAdjacentHTML('beforeend', `
     <tr>
       <td>${person.name}</td>
       <td>${gender}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
+      <td>${born}</td>
+      <td>${died}</td>
       <td>${age}</td>
       <td>${century}</td>
     </tr>
