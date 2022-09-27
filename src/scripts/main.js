fbc63a73e980237dcab7704a5d7d49c499231516
Serhii Yunnikov <yunnikov@icloud.com>
2022-09-27 12:30:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,51 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// find a table with class dashboard in the document.
 
-// write your code here
+const tableTag = document.querySelector('.dashboard');
+
+// function returns full gender word
+
+function genderItem(sex) {
+  if (sex === 'm') {
+    return 'Male';
+  }
+
+  return 'Female';
+}
+
+// add age and century properties to each object
+
+for (const item of people) {
+  item.age = item.died - item.born;
+  item.century = Math.ceil(item.died / 100);
+  item.sex = genderItem(item.sex);
+}
+
+// function that returns the row with 6 cells person info
+
+function genRow(person) {
+  const tableRow = document.createElement('tr');
+
+  tableRow.innerHTML += `
+    <td>${person.name}</td>
+    <td>${person.sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.age}</td>
+    <td>${person.century}</td>
+  `;
+
+  return tableRow;
+}
+
+// get arrow of rows
+
+const rowsArray = people.map(genRow);
+
+// append created row to table
+
+for (const personRow of rowsArray) {
+  tableTag.append(personRow);
+}
