cc305c7022f059dc250a9325914189d971b96134
Andriy Hladkevych <andriyglad@gmail.com>
2022-10-05 18:22:30 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,33 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+function appendTable(group) {
+  const table = document.querySelector('.dashboard');
 
-// write your code here
+  for (const person of group) {
+    const newElemLine = document.createElement('tr');
+
+    const age = person.died - person.born;
+    const centuary = Math.ceil(person.died / 100);
+
+    const updatePerson = {
+      ...person,
+      age,
+      centuary,
+    };
+
+    const types = ['name', 'sex', 'born', 'died', 'age', 'centuary'];
+
+    for (const prop of types) {
+      const cell = document.createElement('td');
+
+      cell.innerHTML = updatePerson[prop];
+
+      newElemLine.appendChild(cell);
+    }
+
+    table.appendChild(newElemLine);
+  }
+}
+
+appendTable(people);
