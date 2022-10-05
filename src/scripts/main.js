058655d005b228d4316fd1490a9ed76fd4fb98af
Danil <dasha8854567@gmail.com>
2022-10-05 21:07:58 +0300

    commit1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,36 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard tbody');
 
-// write your code here
+function personSex(person) {
+  let personSexStr;
+
+  if (person.sex === 'f') {
+    personSexStr = `<th>${person.sex.toUpperCase()}emale</th>`;
+  } else {
+    personSexStr = `<th>${person.sex.toUpperCase()}ale</th>`;
+  };
+
+  return ` 
+  <tr>
+    <th>${person.name}</th>
+  ` + personSexStr + `
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
+  </tr>
+  `;
+}
+
+people.map(person => table.insertAdjacentHTML('beforeend', ` 
+  <tr>
+    <th>${person.name}</th>
+    <th>${personSex(person.sex)}ale</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
+  </tr>
+  `));
