2baec4df9a2a2a42499546fed97d9c34bfcd8181
Hlazman <glazman.b@gmail.com>
2022-10-09 12:53:10 +0300

    changed gender string to show full word

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,7 @@ people.forEach(person => {
   const row = document.querySelectorAll('tr');
 
   row[count].insertCell(0).innerText = person.name;
-  row[count].insertCell(1).innerText = person.sex;
+  row[count].insertCell(1).innerText = person.sex === 'm' ? 'Male' : 'Female';
   row[count].insertCell(2).innerText = person.born;
   row[count].insertCell(3).innerText = person.died;
   row[count].insertCell(4).innerText = person.died - person.born;
