076359804884e76918f282d18664bab1df067b0d
Oleksandr <Khabailiuk@gmail.com>
2022-10-12 22:44:37 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,29 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
 // write your code here
+const table = people.map(person => {
+  let sex;
+  const age = person.died - person.born;
+  const centery = Math.ceil(person.died / 100);
+
+  if (person.sex === 'm') {
+    sex = 'Male';
+  } else {
+    sex = 'Female';
+  }
+
+  return (`<tr>
+    <td>${person.name}</td>
+    <td>${sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${centery}</td>
+  </tr>`);
+});
+
+const dashboard = document.querySelector('.dashboard tbody');
+
+dashboard.insertAdjacentHTML('beforeend', table);
