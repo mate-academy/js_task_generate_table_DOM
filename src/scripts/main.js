0e8c6bd388cc63b553f867801570b398cf962fe9
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-10-14 13:26:50 +0300

    task generate table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,6 @@
 
 const people = require('./lib/people');
 
-// // eslint-disable-next-line no-console
-// console.log(people); // you can remove it
-
 for (const person of people) {
   const table = document.querySelector('tbody');
 
@@ -13,42 +10,17 @@ for (const person of people) {
   table.append(row);
 
   row.insertAdjacentHTML('afterbegin', `
-  <th>${person.name}</th>
-  <th>${sex(person.sex)}</th>
-  <th>${person.born}</th>
-  <th>${person.died}</th>
-  <th>${person.died - person.born}</th>
-  <th>${Math.ceil(person.died / 100)}</th>
+    <th>${person.name}</th>
+    <th>${getSex(person.sex)}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
   `
   );
-
-  // const namePeople = document.createElement('th');
-
-  // const gender = document.createElement('th');
-
-  // const born = document.createElement('th');
-
-  // const died = document.createElement('th');
-
-  // const age = document.createElement('th');
-
-  // const century = document.createElement('th');
-
-  // namePeople.innerText += (person.name);
-
-  // gender.innerText += (sex(person.sex));
-
-  // born.innerText += (person.born);
-
-  // died.innerText += (person.died);
-
-  // age.innerText += (person.died - person.born);
-
-  // century.innerText += (Math.ceil(person.died / 100));
-  // row.append(namePeople, gender, born, died, age, century);
 }
 
-function sex(x) {
+function getSex(x) {
   if (x === 'm') {
     return 'Male';
   }
