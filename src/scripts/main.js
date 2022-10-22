22156ff4e8c1b19c56c096ca35f1b79eb844705d
miamarichka <ilchyshyn.marichka@gmail.com>
2022-10-22 12:46:57 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,36 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => {
+  person.sex = sexData(person.sex);
+  person.age = person.died - person.born;
+  person.century = centuryData(person.died);
+});
+
+function sexData(data) {
+  if (data === 'm') {
+    return 'Male';
+  } else {
+    return 'Female';
+  };
+}
+
+function centuryData(deathYear) {
+  return Math.ceil(deathYear / 100);
+}
+
+people.map(person => {
+  const tableRow = `
+  <tr>
+    <td>${person.name}</td>
+    <td>${person.sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.age}</td>
+    <td>${person.century}</td>
+  </tr>`;
+
+  table.lastChild.innerHTML += tableRow;
+});
