ba0c333c28ef8e559a0e8bcc4da37015615d6172
Anastasia Babko <hr.anastasia.t@gmail.com>
2022-10-22 13:16:44 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,19 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboardOnPage = document.querySelector('.dashboard tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  person.gender = person.sex === 'm' ? 'Male' : 'Female';
 
-// write your code here
+  dashboardOnPage.insertAdjacentHTML('beforeend', `
+    <tr>
+        <td>${person.name}</td>
+        <td>${person.gender}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `);
+});
