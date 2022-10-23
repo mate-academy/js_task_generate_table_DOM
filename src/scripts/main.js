d899b0486f28a9290e8aa0742647da7604e3792f
Margarita Chorniak <chorniak.rita@gmail.com>
2022-10-23 15:18:40 +0300

    generate_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,36 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const tr = document.createElement('tr');
+  const personName = person.name;
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+  const born = person.born;
+  const died = person.died;
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  tr.insertAdjacentHTML('beforeend', `
+    <td>
+      ${personName}
+    </td>  
+    <td>
+      ${gender}
+    </td>  
+    <td>
+      ${born}  
+    </td>
+    <td>
+      ${died}
+    </td>
+    <td>
+      ${age}
+    <td>
+    ${century}
+    </td>
+  `);
+
+  table.append(tr);
+});
