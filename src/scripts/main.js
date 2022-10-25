007b86a5288bd10185f71b5cde3d3b14ec71e0bf
Valentyn Ivanenko <Ivanenko1402@gmail.com>
2022-10-26 02:20:32 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,25 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashBoard = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const row = document.createElement('tr');
+  const sex = person.sex === 'm'
+    ? 'Male'
+    : 'Female';
+
+  row.innerHTML = `
+  <td>${person.name}</td>
+  <td>${sex}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.born / 100)}</td>
+  `;
+
+  dashBoard.append(row);
+}
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
-
-// write your code here
