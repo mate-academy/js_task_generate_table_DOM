7e09c36c95c40caac3ef3b819245cbe7f342cf9c
Anastasiya145 <asiva@ukr.net>
2022-10-26 20:24:44 +0200

    the task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+for (const person of people) {
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+
+  table.children[0].innerHTML += `
+    <tr>
+      <td>${person.name}</td>
+      <td>${gender}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `;
+}
