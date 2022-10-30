a6dc94a856fa387fcb85725f6fc4cd3172570b6a
Dima Romanenko <demigod177712@gmail.com>
2022-10-30 11:59:15 +0200

    table was generated by dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,25 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard tbody');
 
-// write your code here
+function checkSex(sex) {
+  return sex === 'm' ? 'Male' : 'Female';
+}
+
+function fillTable(arr) {
+  arr.forEach(person => {
+    table.insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${person.name}</td>
+      <td>${checkSex(person.sex)}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+    `);
+  });
+}
+
+fillTable(people);
