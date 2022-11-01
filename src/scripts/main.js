f4411e3a31d492235364b2a2d194d566b1be0aa6
Max Fedak <max.fedak@gmail.com>
2022-11-01 22:57:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,33 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
+
+let tableKeys = [...document.querySelectorAll('th')];
+
+tableKeys = tableKeys.map(item => item.innerText.toLowerCase());
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const tableRow = document.createElement('tr');
 
-// write your code here
+  for (const criteria of tableKeys) {
+    const cell = document.createElement('td');
+
+    if (criteria === 'age') {
+      cell.innerText = person.died - person.born;
+    } else if (criteria === 'century') {
+      cell.innerText = Math.ceil(person.died / 100);
+    } else if (criteria === 'gender') {
+      cell.innerHTML = person.sex === 'm'
+        ? 'Male'
+        : 'Female';
+    } else {
+      cell.innerText = person[criteria];
+    }
+
+    tableRow.appendChild(cell);
+  }
+
+  table.appendChild(tableRow);
+});
