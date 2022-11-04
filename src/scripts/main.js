b84c212dd29de41206c84eee0eaa5fd734ed4b68
Ruslan Miroshnychenko <rusikm87@gmail.com>
2022-11-04 11:47:25 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,17 @@ const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
 people.forEach(person => {
-  const gender = (person.sex === 'm') ? 'Male' : 'Fermale';
+  const gender = (person.sex === 'm') ? 'Male' : 'Female';
   const age = person.died - person.born;
   const century = Math.ceil(person.died / 100);
 
   table.firstElementChild.insertAdjacentHTML('beforeend',
     `<tr>
-      <th>${person.name}</th>
-      <th>${gender}</th>
-      <th>${person.born}</th>
-      <th>${person.died}</th>
-      <th>${age}</th>
-      <th>${century}</th>
+      <td>${person.name}</td>
+      <td>${gender}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
     </tr>`);
 });
