cca97a30abe3a397eecd4118a88a1a9d45ef0000
Antonenko-AA <alexandr.antonenko.fr@gmail.com>
2022-11-05 02:35:13 +0200

    People table has been created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+people.forEach(({ name: personName, sex, born, died }) => {
+  table.tBodies[0].insertAdjacentHTML('beforeend', `
+    <tr>
+      <th>${personName}</th>
+      <th>${sex}</th>
+      <th>${born}</th>
+      <th>${died}</th>
+      <th>${died - born}</th>
+      <th>${Math.ceil(died / 100)}</th>
+    </tr>
+  `);
+});
