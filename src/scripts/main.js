3a504815d21f326bcc4e8cc385cb2884676d8135
Elena Ver <hellena84@i.ua>
2022-11-12 22:02:13 +0100

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,20 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+const newPeople = [...people];
+
+newPeople.forEach(person => {
+  const cells = document.createElement('tr');
+
+  cells.insertAdjacentHTML('afterbegin', `
+  <td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.age = person.died - person.born}</td>
+  <td>${person.century = Math.round(person.died / 100)}</td>`);
+
+  table.append(cells);
+});
