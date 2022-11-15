2ef9e2282ded90a1a4077cb48f34b4d2861ae159
Elena Ver <hellena84@i.ua>
2022-11-15 20:43:53 +0100

    commit number two

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,18 @@ const people = require('./lib/people');
 
 const table = document.querySelector('.dashboard');
 
-const newPeople = [...people];
+people.forEach(person => {
+  const tableRow = document.createElement('tr');
 
-newPeople.forEach(person => {
-  const cells = document.createElement('tr');
-
-  cells.insertAdjacentHTML('afterbegin', `
+  tableRow.insertAdjacentHTML('afterbegin', `
+  <tr>
   <td>${person.name}</td>
   <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
   <td>${person.born}</td>
   <td>${person.died}</td>
   <td>${person.age = person.died - person.born}</td>
-  <td>${person.century = Math.round(person.died / 100)}</td>`);
+  <td>${person.century = Math.round(person.died / 100)}</td>
+  </tr>`);
 
-  table.append(cells);
+  table.append(tableRow);
 });
