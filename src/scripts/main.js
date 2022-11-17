9d2bdbe55ca2ffd07b34fb8a1b46851d4de652c5
Eugene Farafonov <eu.farafonov@gmail.com>
2022-11-17 16:41:25 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,38 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const tr = document.createElement('tr');
 
-// write your code here
+  const tdName = document.createElement('td');
+
+  tdName.textContent = person.name;
+  tr.append(tdName);
+
+  const tdSex = document.createElement('td');
+
+  tdSex.textContent = person.sex;
+  tr.append(tdSex);
+
+  const tdBorn = document.createElement('td');
+
+  tdBorn.textContent = person.born;
+  tr.append(tdBorn);
+
+  const tdDied = document.createElement('td');
+
+  tdDied.textContent = person.died;
+  tr.append(tdDied);
+
+  const tdAge = document.createElement('td');
+
+  tdAge.textContent = person.died - person.born;
+  tr.append(tdAge);
+
+  const tdCentury = document.createElement('td');
+
+  tdCentury.textContent = Math.ceil(person.died / 100);
+  tr.append(tdCentury);
+
+  document.querySelector('.dashboard').append(tr);
+});
