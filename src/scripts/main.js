98400b41532b319f0dd2fcf0999c7b969e8a6c1d
Sergey Vorobey <sergey88nik@gmail.com>
2022-11-20 00:06:50 +0200

    Generate table solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,19 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('tbody');
 
-// write your code here
+for (const person of people) {
+  const age = person.died - person.born;
+
+  table.insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `);
+}
