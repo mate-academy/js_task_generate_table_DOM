4541cbc65e67653a6557e2ae0f8e5ffebf267ffd
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-11-21 15:48:47 +0100

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
+
 function gender(sex) {
   if (sex === 'm') {
     return 'Male';
   }
 
   return 'Female';
-}
+};
 
 const people = require('./lib/people');
-let table = document.querySelector('.dashboard tbody');
+
+const table = document.querySelector('.dashboard tbody');
 let row = table.insertRow();
 
 for (let i = 0; i < people.length; i++) {
@@ -17,17 +19,19 @@ for (let i = 0; i < people.length; i++) {
 
   row.insertCell(0)
     .innerHTML = people[i].name;
+
   row.insertCell(1)
     .innerHTML = gender(people[i].sex);
+
   row.insertCell(2)
     .innerHTML = people[i].born;
+
   row.insertCell(3)
     .innerHTML = people[i].died;
+
   row.insertCell(4)
     .innerHTML = people[i].died - people[i].born;
+
   row.insertCell(5)
     .innerHTML = Math.ceil(people[i].died / 100);
-
-  //row = table.insertRow(i + 2);
-
 }
