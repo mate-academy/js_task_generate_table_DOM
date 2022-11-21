7f2243d30219c011997aafc9252299b5d3b2ad82
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-11-21 15:41:30 +0100

    Appended created row to table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,33 @@
 'use strict';
+function gender(sex) {
+  if (sex === 'm') {
+    return 'Male';
+  }
+
+  return 'Female';
+}
 
 const people = require('./lib/people');
+let table = document.querySelector('.dashboard tbody');
+let row = table.insertRow();
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (let i = 0; i < people.length; i++) {
+  row = table.insertRow(i + 1);
+  table.appendChild(row);
 
-// write your code here
+  row.insertCell(0)
+    .innerHTML = people[i].name;
+  row.insertCell(1)
+    .innerHTML = gender(people[i].sex);
+  row.insertCell(2)
+    .innerHTML = people[i].born;
+  row.insertCell(3)
+    .innerHTML = people[i].died;
+  row.insertCell(4)
+    .innerHTML = people[i].died - people[i].born;
+  row.insertCell(5)
+    .innerHTML = Math.ceil(people[i].died / 100);
+
+  //row = table.insertRow(i + 2);
+
+}
