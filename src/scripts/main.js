08aab8a9f740f60af56d757e74ec798445b7ad6f
Maks <comanderius@gmail.com>
2022-11-22 21:34:16 +0100

    Generate table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,43 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const list = document.querySelector('.dashboard');
+
+for (let i = 0; i < people.length; i++) {
+  const line = document.createElement('tr');
+  const nameLine = document.createElement('td');
+
+  nameLine.textContent = people[i].name;
+  line.append(nameLine);
+
+  const genderLine = document.createElement('td');
+
+  if (people[i].sex === 'm') {
+    genderLine.textContent = 'Male';
+  } else {
+    genderLine.textContent = 'Female';
+  }
+  line.append(genderLine);
+
+  const bornLine = document.createElement('td');
+
+  bornLine.textContent = people[i].born;
+  line.append(bornLine);
+
+  const diedLine = document.createElement('td');
+
+  diedLine.textContent = people[i].died;
+  line.append(diedLine);
+
+  const ageLine = document.createElement('td');
+
+  ageLine.textContent = people[i].died - people[i].born;
+  line.append(ageLine);
+
+  const centuryLine = document.createElement('td');
+
+  centuryLine.textContent = Math.ceil(people[i].died / 100);
+  line.append(centuryLine);
+
+  list.append(line);
+}
