4a037412d2c839c485f8679bebddcae07f72c793
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-11-22 22:48:04 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,26 @@
 'use strict';
 
 const people = require('./lib/people');
+const mainTable = document.body.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  delete person.motherName;
+  delete person.fatherName;
+  delete person.slug;
 
-// write your code here
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+  person.sex = person.sex === 'm' ? 'Male' : 'Female';
+
+  const columnInTable = document.createElement('tr');
+
+  mainTable.append(columnInTable);
+
+  for (const info in person) {
+    const infoInColumn = document.createElement('td');
+
+    infoInColumn.textContent = person[info];
+
+    columnInTable.append(infoInColumn);
+  }
+};
