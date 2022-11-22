b33a8ef8ab15ba33e62f4f3c2d54c35f097c6057
Maria Bramnik <mariabramnik@gmail.com>
2022-11-22 18:13:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,45 @@
 'use strict';
 
 const people = require('./lib/people');
+const arrPeople = JSON.parse(people);
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableElement = document.querySelector('.dashboard');
+for(let i = 1 ; i < arrPeople.length; i++){
+    const newRow = tableElement.insertRow(i);
+    for(let j = 0; j < 6; j++ ){
+        const person = arrPeople[i-1];
+        const newCell = newRow.insertCell(j);
+        let strTextCell = '';
 
-// write your code here
+        switch (j) {
+            case 0:
+                strTextCell = person.name;
+                break;
+            case 1:
+                const sex = person.sex;
+                if(sex === 'm') {
+                    strTextCell = 'Male';
+                } else strTextCell = 'Female';
+
+                break;
+            case 2:
+                strTextCell =person.born;
+                break;
+            case 3:
+                strTextCell = person.died;
+                break;
+            case 4:
+                const age = Number(person.died)- Number(person.born);
+                strTextCell = age;
+                break;
+            case 5:
+               const century =  Math.ceil(Number(person.died) / 100);
+               strTextCell = century;
+               break;
+        }
+        let newText = document.createTextNode(strTextCell);
+        newCell.appendChild(newText);
+    }
+}
+
