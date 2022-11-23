4f440a0bbceca8581f2b2ddf826e74a65809e43d
Maria Bramnik <mariabramnik@gmail.com>
2022-11-23 22:44:31 +0200

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,49 +5,24 @@ const arrPeople = JSON.parse(people);
 
 // eslint-disable-next-line no-console
 const tableElement = document.querySelector('.dashboard');
-
 for (let i = 1; i < arrPeople.length; i++) {
-  const newRow = tableElement.insertRow(i);
-
-  for (let j = 0; j < 6; j++) {
     const person = arrPeople[i - 1];
-    const newCell = newRow.insertCell(j);
-    let strTextCell = '';
+    const trElement = document.createElement('tr');
 
-    switch (j) {
-      case 0:
-        strTextCell = person.name;
-        break;
-      case 1:
-        const sex = person.sex;
+    let strSex = '';
+    if (person.sex === 'm') {
+        strSex = 'Male';
+    } else strSex = 'Female';
+    const age = Number(person.died) - Number(person.born);
+    const century = Math.ceil(Number(person.died) / 100);
 
-        if (sex === 'm') {
-          strTextCell = 'Male';
-        } else {
-          strTextCell = 'Female';
-        }
+    let strTextCell = '<td>' + person.name + '</><td>'
+        + strSex + '</><td>'
+        + person.born + '</><td>'
+        + person.died + '</><td>'
+        + age.toString() + '</><td>'
+        + century + '</>';
+    trElement.insertAdjacentHTML('beforeend', strTextCell);
 
-        break;
-      case 2:
-        strTextCell = person.born;
-        break;
-      case 3:
-        strTextCell = person.died;
-        break;
-      case 4:
-        const age = Number(person.died) - Number(person.born);
-
-        strTextCell = age;
-        break;
-      case 5:
-        const century = Math.ceil(Number(person.died) / 100);
-
-        strTextCell = century;
-        break;
-    }
-
-    const newText = document.createTextNode(strTextCell);
-
-    newCell.appendChild(newText);
-  }
+    tableElement.insertAdjacentElement('beforeend', trElement);
 }
