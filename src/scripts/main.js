c2ef1c7958bb4d8a6c0e40d30f21ab21ed385805
Vlad Fedorishchev <fedorischev23@gmail.com>
2022-11-24 23:11:12 +0300

    Tried to avoid deleting values

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,17 @@ const people = require('./lib/people');
 const mainTable = document.body.querySelector('.dashboard');
 
 for (const person of people) {
-  delete person.motherName;
-  delete person.fatherName;
-  delete person.slug;
-
   person.age = person.died - person.born;
   person.century = Math.ceil(person.died / 100);
   person.sex = person.sex === 'm' ? 'Male' : 'Female';
+
+  const { motherName, fatherName, slug, ...mainInfo } = person;
 
   const columnInTable = document.createElement('tr');
 
   mainTable.append(columnInTable);
 
-  for (const info in person) {
+  for (const info in mainInfo) {
     const infoInColumn = document.createElement('td');
 
     infoInColumn.textContent = person[info];
