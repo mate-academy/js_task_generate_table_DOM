71b01c865365feb3268cbe7e38322530d802d329
Maria Bramnik <mariabramnik@gmail.com>
2022-11-24 18:08:05 +0200

    Fixes after code review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,24 +5,30 @@ const arrPeople = JSON.parse(people);
 
 // eslint-disable-next-line no-console
 const tableElement = document.querySelector('.dashboard');
+
 for (let i = 1; i < arrPeople.length; i++) {
-    const person = arrPeople[i - 1];
-    const trElement = document.createElement('tr');
+  const person = arrPeople[i - 1];
+  const trElement = document.createElement('tr');
 
-    let strSex = '';
-    if (person.sex === 'm') {
-        strSex = 'Male';
-    } else strSex = 'Female';
-    const age = Number(person.died) - Number(person.born);
-    const century = Math.ceil(Number(person.died) / 100);
+  let strSex = '';
 
-    let strTextCell = '<td>' + person.name + '</><td>'
-        + strSex + '</><td>'
-        + person.born + '</><td>'
-        + person.died + '</><td>'
-        + age.toString() + '</><td>'
-        + century + '</>';
-    trElement.insertAdjacentHTML('beforeend', strTextCell);
+  if (person.sex === 'm') {
+    strSex = 'Male';
+  } else {
+    strSex = 'Female';
+  }
 
-    tableElement.insertAdjacentElement('beforeend', trElement);
+  const age = Number(person.died) - Number(person.born);
+  const century = Math.ceil(Number(person.died) / 100);
+
+  const strTextCell = `<td> ${person.name} </><td>
+    ${strSex}  </><td>
+    ${person.born}  </><td>
+    ${person.died}  </><td>
+    ${age.toString()} </><td>
+    ${century}</>`;
+
+  trElement.insertAdjacentHTML('beforeend', strTextCell);
+
+  tableElement.insertAdjacentElement('beforeend', trElement);
 }
