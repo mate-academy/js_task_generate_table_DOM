42ee33188c87f3af8a16b4c37719f4ac1d1a0c66
Olena Dats <datsolena@gmail.com>
2022-11-28 13:01:40 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,27 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const ppl = people.map(pl => ({
+  Name: pl.name,
+  Gender: pl.sex,
+  Born: pl.born,
+  Died: pl.died,
+  Age: pl.died - pl.born,
+  Century: Math.ceil(pl.died / 100),
+}));
+
+const table = document.querySelector('.dashboard');
+
+ppl.forEach(person => {
+  const row = document.createElement('tr');
+
+  Object.values(person).forEach(text => {
+    const cell = document.createElement('td');
+    const textNode = document.createTextNode(text);
+
+    cell.appendChild(textNode);
+    row.appendChild(cell);
+  });
+
+  table.appendChild(row);
+});
