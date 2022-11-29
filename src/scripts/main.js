f539cb95c08ccbf903c9c4d95342a1afdeff959d
Panianchuk M <panianchuk@gmail.com>
2022-11-29 20:13:18 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,21 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const tableWithPerson = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  person.sex = person.sex === 'm' ? 'Male' : 'Female';
+
+  tableWithPerson.lastElementChild.insertAdjacentHTML('beforeend',
+    `
+      <tr>
+        <td>${person.name}</td>
+        <td>${person.sex}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+      </tr>
+    `
+  );
+});
