c924253f0a508c57f3f90e1aa29d9e3036f657a8
anton-sosonskyi <anton.sosonskyi@gmail.com>
2022-12-02 21:18:50 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,22 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const { name: fullName, sex, born, died } = person;
+  const gender = sex === 'm' ? 'Male' : 'Female';
+
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <td>${fullName}</td>
+    <td>${gender}</td>
+    <td>${born}</td>
+    <td>${died}</td>
+    <td>${died - born}</td>
+    <td>${Math.ceil(died / 100)}</td>
+  `;
+
+  table.append(tr);
+}
