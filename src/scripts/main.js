1ea7f5a72160010abf704dc8811b93359e3ecc76
Vadym Kimpaiev <kivabi.uk@gmail.com>
2022-12-10 17:16:33 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,27 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+console.log(Array.isArray(people)); // you can remove it
 
-// write your code here
+const tableRowsArray = people.map(person => {
+  const century = Math.ceil(person.died / 100);
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+  const age = person.died - person.born;
+  const tableRow = `
+    <tr>
+      <td>${person.name}</td>
+      <td>${gender}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `;
+
+  return tableRow;
+});
+
+const dashboardElem = document.querySelector('.dashboard');
+
+dashboardElem.children[0]
+  .insertAdjacentHTML('beforeend', tableRowsArray.join(''));
