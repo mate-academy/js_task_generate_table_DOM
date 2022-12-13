44fcd1c64093b1dd7c0980a835f0570750db793f
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2022-12-13 20:24:57 +0200

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const arrayPeople = people.map(person => {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+  const tablePerson = `
+  <tr>
+      <td>${person.name}</td>
+      <td>${sex}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+   </tr>
+  `;
 
-// write your code here
+  return tablePerson;
+});
+
+table.children[0].insertAdjacentHTML('beforeend', arrayPeople.join(''));
