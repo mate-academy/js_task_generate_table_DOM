cd67f81840317ea521905cf95fbc755f4fdb7743
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-12-14 05:40:04 +0200

    Generate table solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,34 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// console.log(people); // you can remove it
 
-// write your code here
+const peopleForTable = people.map(function(person) {
+  const obj = {
+    name: person.name,
+    gender: person.sex,
+    born: person.born,
+    died: person.died,
+    age: person.died - person.born,
+    century: Math.ceil(person.died / 100),
+  };
+
+  obj.gender === 'm' ? obj.gender = 'Male' : obj.gender = 'Female';
+
+  return obj;
+});
+
+const table = document.body.querySelector('.dashboard');
+
+peopleForTable.map(person => {
+  const tr = document.createElement('tr');
+
+  table.append(tr);
+
+  for (const key in person) {
+    const td = document.createElement('td');
+
+    td.innerHTML = person[key];
+    tr.append(td);
+  }
+});
