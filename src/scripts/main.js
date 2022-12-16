80a25cc7ce81a73d01d0852c661e727cc99e6a59
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-12-16 22:40:38 +0200

    fix remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,26 @@
 'use strict';
 
 const people = require('./lib/people');
-
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
 const table = document.querySelector('.dashboard');
 
-const peopleForTable = people.map(person => {
-  const century = Math.ceil(person.died / 100);
-  const age = person.died - person.born;
-  const sex = person.sex === 'm' ? 'Male' : 'Female';
+people.forEach(person => {
+  const { name: personName, sex, born, died } = person;
 
-  return ({
-    name: person.name,
-    sex: sex,
-    born: person.born,
-    died: person.died,
-    age: age,
-    century: century,
-  });
-});
+  const century = Math.ceil(died / 100);
+  const gender = sex === 'm' ? 'Male' : 'Female';
+  const age = died - born;
 
-peopleForTable.forEach(person => {
-  table.insertAdjacentHTML('beforeend', `
-    <tr>
-      <td>${person.name}</td>
-      <td>${person.sex}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
-      <td>${person.age}</td>
-      <td>${person.century}</td>
-    </tr>
-  `);
+  const tableRow = document.createElement('tr');
+
+  tableRow.innerHTML = `
+      <td>${personName}</td>
+      <td>${gender}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+  `;
+
+  table.append(tableRow);
 });
