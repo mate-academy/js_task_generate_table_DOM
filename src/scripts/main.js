e6df50fa9164bdf798954fc648d41ab36b48705b
divineretribution1 <danya00776@gmail.com>
2022-12-16 03:47:30 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,37 +5,35 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-let table = document.querySelector(".dashboard");
+const table = document.querySelector('.dashboard');
 
+const rows = people.map(function(person) {
+  const row = document.createElement('tr');
 
-let rows = people.map(function(person) {
+  const nameCell = document.createElement('td');
 
-  let row = document.createElement("tr");
-  
-
-  let nameCell = document.createElement("td");
   nameCell.textContent = person.name;
-  
-  let genderCell = document.createElement("td");
-  if (person.sex === "m") {
-    genderCell.textContent = "Male";
-  } else {
-    genderCell.textContent = "Female";
-  }
-  
-  let bornCell = document.createElement("td");
+
+  const genderCell = document.createElement('td');
+
+  genderCell.textContent = person.sex === 'm' ? 'Male' : 'Female';
+
+  const bornCell = document.createElement('td');
+
   bornCell.textContent = person.born;
-  
-  let diedCell = document.createElement("td");
+
+  const diedCell = document.createElement('td');
+
   diedCell.textContent = person.died;
-  
-  let ageCell = document.createElement("td");
+
+  const ageCell = document.createElement('td');
+
   ageCell.textContent = person.died - person.born;
-  
-  let centuryCell = document.createElement("td");
+
+  const centuryCell = document.createElement('td');
+
   centuryCell.textContent = Math.ceil(person.died / 100);
-  
- 
+
   row.appendChild(nameCell);
   row.appendChild(genderCell);
   row.appendChild(bornCell);
