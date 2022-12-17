a2878058c6907fb9fd896327b068830afb37b6f3
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-12-17 06:00:58 +0200

    fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,15 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-// console.log(people); // you can remove it
 
-const peopleForTable = people.map(function(person) {
-  const obj = {
-    name: person.name,
-    gender: person.sex,
-    born: person.born,
-    died: person.died,
-    age: person.died - person.born,
-    century: Math.ceil(person.died / 100),
-  };
-
-  obj.gender === 'm' ? obj.gender = 'Male' : obj.gender = 'Female';
-
-  return obj;
-});
+const peopleForTable = people.map((person) => ({
+  name: person.name,
+  gender: person.sex === 'm' ? 'Male' : 'Female',
+  born: person.born,
+  died: person.died,
+  age: person.died - person.born,
+  century: Math.ceil(person.died / 100),
+}));
 
 const table = document.body.querySelector('.dashboard');
 
