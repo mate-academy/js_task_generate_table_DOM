76a29a468cece3235c187cd0cb0bef799e6215b2
Dmitriy Sporysh <dopmoneyd@gmail.com>
2022-12-19 21:26:27 +0200

    info about people in the table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,27 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableBody = document.querySelector('tbody');
 
-// write your code here
+tableBody.insertAdjacentHTML('beforeend', `
+  ${people.map((
+    {
+      // eslint-disable-next-line no-shadow
+      name,
+      sex,
+      born,
+      died,
+      age = (died - born),
+      century = (Math.ceil(died / 100)),
+    }
+  ) => `
+    <tr>
+      <td>${name}</td>
+      <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `).join('')};
+`);
