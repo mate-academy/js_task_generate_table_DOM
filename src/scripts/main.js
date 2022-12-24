842ef456bc5358afc8df6bef46610fad9b0bd9d4
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-24 12:20:26 +0200

    generateTableDK

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
+const properties = ['name', 'sex', 'born', 'died'];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const tr = document.createElement('tr');
 
-// write your code here
+  for (let i = 0; i < 6; i++) {
+    const td = document.createElement('td');
+
+    if (i === 4) {
+      td.innerText = person[properties[3]] - person[properties[2]];
+    } else if (i === 5) {
+      td.innerText = Math.ceil(person[properties[3]] / 100);
+    } else {
+      td.innerText = person[properties[i]];
+    }
+    tr.append(td);
+  }
+  table.append(tr);
+}
