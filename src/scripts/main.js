e57cb11d4d4cb28a1693577a0118ad99dcd49774
Vasiliy22 <hitretsukrnet@gmail.com>
2022-12-26 23:54:26 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,21 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const element = document.querySelector('.dashboard');
 
-// write your code here
+people.map(item => {
+  const row = document.createElement('tr');
+
+  element.append(row);
+
+  row.innerHTML = `
+ <tr>
+  <th>${item.name}</th>
+  <th>${item.sex}</th>
+  <th>${item.born}</th>
+  <th>${item.died}</th>
+  <th>${item.died - item.born}</th>
+  <th>${Math.ceil(item.died / 100)}</th>
+  </tr>
+`;
+});
