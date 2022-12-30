a96bd11496e8d21f0351f6bceb65f1ad8be30a91
Nick Busarhin <busarginkolia@gmail.com>
2022-12-30 23:48:09 +0200

    table added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,21 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+  const gender
+  = person.sex === 'f' ? 'Female' : 'Male';
+
+  row.innerHTML = `
+     <th> ${person.name} </th>
+     <th> ${gender} </th>
+     <th> ${person.born} </th>
+     <th> ${person.died} </th>
+     <th> ${person.died - person.born} </th>
+     <th> ${Math.ceil(person.died / 100)} </th>
+    `;
+
+  const table = document.querySelector('.dashboard');
+
+  table.append(row);
+}
