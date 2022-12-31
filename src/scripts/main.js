23a93fa7557dc5f4c63d587d61821f99f01cc227
Nazarii Kyselevych <nazarkysel@gmail.com>
2022-12-31 16:04:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,23 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('table');
 
-// write your code here
+// eslint-disable-next-line no-shadow
+people.forEach(({ name, sex, born, died }) => {
+  const row = document.createElement('tr');
+  const ceils = Array(6).fill(null).map(e => document.createElement('td'));
+
+  const [nameCeil, genderCeil, bornCeil, diedCeil,
+    ageCeil, centuryCeil] = ceils;
+
+  nameCeil.innerHTML = name;
+  genderCeil.innerHTML = sex === 'm' ? 'Male' : 'Female';
+  bornCeil.innerHTML = born;
+  diedCeil.innerHTML = died;
+  ageCeil.innerHTML = died - born;
+  centuryCeil.innerHTML = Math.ceil(died / 100);
+
+  row.append(...ceils);
+  table.append(row);
+});
