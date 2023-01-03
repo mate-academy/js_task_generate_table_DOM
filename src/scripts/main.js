ca169b791f471990047c0b94cba6a9d6e9386b99
EhorSaprykin <astralik1993@gmail.com>
2023-01-03 12:16:13 +0200

    Table done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,30 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+function createTable(peoplesArr) {
+  const table = document.querySelector('tbody');
 
-// write your code here
+  for (const person of people) {
+    const tableRow = document.createElement('tr');
+
+    const personData = {
+      name: person.name,
+      gender: person.sex === 'm' ? 'Male' : 'Female',
+      born: person.born,
+      died: person.died,
+      Age: (person.died - person.born),
+      Century: (Math.ceil(person.died / 100)),
+    };
+
+    table.append(tableRow);
+
+    for (const data in personData) {
+      const tableData = document.createElement('td');
+
+      tableRow.append(tableData);
+      tableData.textContent = personData[data];
+    };
+  };
+};
+
+createTable(people);
