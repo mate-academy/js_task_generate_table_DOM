ecbe40f697ccafb2da02a5a6c3eea30c455394bb
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-01-03 13:48:13 +0200

    much cleaner  version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,19 +6,16 @@ const tableBody = document.querySelector('tbody');
 
 // eslint-disable-next-line no-shadow
 people.forEach(({ name, sex, born, died }) => {
-  const row = document.createElement('tr');
-  const ceils = Array(6).fill(null).map(e => document.createElement('td'));
-
-  const [nameCeil, genderCeil, bornCeil, diedCeil,
-    ageCeil, centuryCeil] = ceils;
-
-  nameCeil.innerHTML = name;
-  genderCeil.innerHTML = sex === 'm' ? 'Male' : 'Female';
-  bornCeil.innerHTML = born;
-  diedCeil.innerHTML = died;
-  ageCeil.innerHTML = died - born;
-  centuryCeil.innerHTML = Math.ceil(died / 100);
-
-  row.append(...ceils);
-  tableBody.append(row);
+  tableBody.insertAdjacentHTML('beforeend',
+    `
+    <tr>
+      <td>${name}</td>
+      <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${died - born}</td>
+      <td>${Math.ceil(died / 100)}</td>
+    </tr>
+    `
+  );
 });
