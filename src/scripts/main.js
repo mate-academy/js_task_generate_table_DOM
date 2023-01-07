7f50b515b68745f541c1530bc1b6bb3d95529e09
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-01-07 19:09:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,13 @@ const table = document.querySelector('table');
 for (const person of people) {
   const row = document.createElement('tr');
 
-  row.innerHTML += `
-  <tr>
+  row.innerHTML = `
     <td>${person.name}</td>
     <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
     <td>${person.died - person.born}</td>
     <td>${Math.ceil(person.born / 100)}</td>
-</tr>
   `;
   table.append(row);
 }
