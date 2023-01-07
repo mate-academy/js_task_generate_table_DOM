768a56f1078944b85c2a1a4ede173977ad4a171d
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-01-07 18:45:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('table');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+for (const person of people) {
+  table.innerHTML += `
+  <tr>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.born / 100)}</td>
+</tr>
+  `;
+}
