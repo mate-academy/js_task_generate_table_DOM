8ed5fa784f9b1516af7ec204364ed77ad4f41b1e
Karadzha Mykola <nicholaskaradzha@gmail.com>
2023-01-09 21:04:33 +0000

    generate table DOM task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,27 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
+const table = document.getElementsByClassName('dashboard')[0];
+const getCentury = (deathAge) => {
+  return Math.ceil(deathAge / 100);
+};
+
+const getFullGender = (sex) => {
+  return sex === 'f' ? 'Female' : 'Male';
+};
+
+const getRow = (person) => {
+  return `<tr>
+    <td> ${person.name} </td>
+    <td>${getFullGender(person.sex)}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${getCentury(person.died)}</td>
+  </tr>`;
+};
+
+const tbody = table.children[0];
+
+tbody.innerHTML += people.map(person => getRow(person)).join('');
