a7a8f322c70e1ff87a24efe3058e61c51a645555
Anna Inozemets <inoznetta@gmail.com>
2023-01-10 22:45:36 +0200

    Table was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const gender = person.sex === 'm'
+    ? 'Male'
+    : 'Female';
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
 
-// write your code here
+  table.insertAdjacentHTML('beforeend', `
+  <tr>
+    <td>${person.name}</td>
+    <td>${gender}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  </tr>
+  `);
+});
