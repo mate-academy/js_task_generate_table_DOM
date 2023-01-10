026836f413712ec1d4d16477b2dcb82bdea61df5
Nick_Radchuk <mukola.radchuk@gmail.com>
2023-01-10 15:54:58 +0200

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,24 +4,17 @@ const people = require('./lib/people');
 
 const table = document.querySelector('.dashboard');
 
-people.forEach(person => {
+// eslint-disable-next-line no-shadow
+people.forEach(({ name, sex, born, died }) => {
   const row = document.createElement('tr');
 
-  if (person.sex === 'm') {
-    person.sex = 'Male';
-  }
-
-  if (person.sex === 'f') {
-    person.sex = 'Female';
-  }
-
   row.innerHTML = `
-  <th>${person.name}</th>
-  <th>${person.sex}</th>
-  <th>${person.born}</th>
-  <th>${person.died}</th>
-  <th>${person.died - person.born}</th>
-  <th>${Math.ceil(person.died / 100)}</th>`;
+  <th>${name}</th>
+  <th>${sex === 'm' ? 'Male' : 'Female'}</th>
+  <th>${born}</th>
+  <th>${died}</th>
+  <th>${died - born}</th>
+  <th>${Math.ceil(died / 100)}</th>`;
 
   table.append(row);
 });
