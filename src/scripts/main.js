2e9e64eca728bc0357bd31de20977cd8fad4ba59
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-01-11 09:14:12 +0200

    solution simplified

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,35 +5,22 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-const table = document.querySelector('.dashboard');
+const firstRow = document.querySelector('tr');
 
-for (let j = 0; j < people.length; j++) {
-  const newRow = document.createElement('tr');
-  const data = Object.values(people[j]);
-
-  for (let i = 0; i < 6; i++) {
-    const newCell = document.createElement('td');
-
-    newCell.textContent = data[i];
-
-    if (i === 1) {
-      if (newCell.textContent === 'm') {
-        newCell.textContent = 'Male';
-      } else {
-        newCell.textContent = 'Female';
-      }
-    }
-
-    if (i === 4) {
-      newCell.textContent = data[3] - data[2];
-    }
-
-    if (i === 5) {
-      newCell.textContent = Math.ceil(data[3] / 100);
-    }
-
-    newRow.append(newCell);
+for (const person of people) {
+  if (person.sex === 'm') {
+    person.sex = 'Male';
+  } else {
+    person.sex = 'Female';
   }
 
-  table.append(newRow);
+  firstRow.insertAdjacentHTML('afterend',
+    `<tr>
+      <td>${person.name}</td>
+      <td>${person.sex}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>`);
 }
