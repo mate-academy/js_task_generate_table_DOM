4d4b39b1ba8e382fe9cd00892514b081184f7fcb
abobyr <artem.bobyr@dataart.com>
2023-01-12 19:58:41 +0200

    task is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,19 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.getElementsByClassName('dashboard')[0];
 
-// write your code here
+for (const person of people) {
+  const row = `
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `;
+
+  dashboard.insertAdjacentHTML('beforeend', row);
+}
