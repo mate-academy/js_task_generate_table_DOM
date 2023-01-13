5dfdf2af34b4dc4f04a4476d81120eb00a41b239
Kate <ekaterina2002aleks@gmail.com>
2023-01-13 16:55:50 +0200

    Solution for table

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,34 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const dashboard = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const row = document.createElement('tr');
+  const name_ = document.createElement('td');
+  const sex = document.createElement('td');
+  const born = document.createElement('td');
+  const died = document.createElement('td');
+  const age = document.createElement('td');
+  const century = document.createElement('td');
+
+  name_.textContent = person.name_;
+  born.textContent = person.born;
+  died.textContent = person.died;
+  age.textContent = person.died - person.born;
+  century.textContent = Math.ceil(person.died / 100);
+
+  if (person.sex === 'f') {
+    sex.textContent = 'Female';
+  } else {
+    sex.textContent = 'Male';
+  }
+
+  row.append(name_);
+  row.append(sex);
+  row.append(born);
+  row.append(died);
+  row.append(age);
+  row.append(century);
+  dashboard.append(row);
+}
