2ce8f0858981e3f27e94294d35f025b8d504de41
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-01-16 15:32:15 +0100

    new sol 1.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,18 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
 const dashboard = document.querySelector('.dashboard tr');
 
 people.forEach(person => {
   dashboard.insertAdjacentHTML('afterend', `
-  <tr>
-    <td>${person.name}</td>
-    <td>${person.sex}</td>
-    <td>${person.born}</td>
-    <td>${person.died}</td>
-    <td>${person.died - person.born}</td>
-    <td>${Math.ceil(person.died / 100)}</td>
-  </tr>
-    `);
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `);
 });
