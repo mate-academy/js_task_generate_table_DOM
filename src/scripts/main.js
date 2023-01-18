f8c063b86a91597ad384b3507ebf82c9b411f567
stepband <vladshvik@gmail.com>
2023-01-18 23:41:48 +0200

    version with append

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,45 +3,8 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
 const table = document.querySelector('.dashboard');
-
-/* for (const person of people) {
-  table.firstElementChild.insertAdjacentHTML("beforeend",
-    `<tr>
-    <td>${person.name}</td>
-    </tr>`
-  );
-}
-
-/* for (const person of people) {
-  const tableRow = document.createElement('tr');
-  const tableData = document.createElement('td');
-
-  tableData.textContent = person.born;
-
-  tableRow.appendChild(tableData);
-  table.appendChild(tableRow);
-}
-document.createElement('td').textContent = '14';
-table.appendChild()
-*/
-// eslint-disable-next-line no-debugger
-
-/* for (const person of people) {
-  const tableRow = document.createElement('tr');
-
-  for (const data in person) {
-    const tableData = document.createElement('td');
-
-    tableData.innerText = person[data];
-
-    tableRow.appendChild(tableData);
-    table.appendChild(tableRow);
-  }
-}
-*/
 const dataArray = [];
 
 for (const person of people) {
