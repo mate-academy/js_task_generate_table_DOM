9b7bd078fe3b7a85c886168d1809eb7e482824a2
haduigon <haduigon@gmail.com>
2023-01-24 22:21:18 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,34 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dash = document.querySelector('.dashboard');
 
-// write your code here
+people.map(a => createRow(a, dash));
+
+function createRow(human, nodeHook) {
+  const table = document.createElement('tr');
+
+  const humanName = document.createElement('th');
+  humanName.textContent = human.name;
+
+  const humanSex = document.createElement('th');
+  humanSex.textContent = human.sex === 'm' ? 'Male' : 'Female';
+
+  const humanBorn = document.createElement('th');
+  humanBorn.textContent = human.born;
+
+  const humanDied = document.createElement('th');
+  humanDied.textContent = human.died;
+
+  const humanAge = document.createElement('th');
+  humanAge.textContent = human.died - human.born;
+
+  const humanCentury = document.createElement('th');
+  humanCentury.textContent = Math.ceil(human.died / 100);
+
+  table.append(humanName, humanSex, humanBorn, humanDied, humanAge, humanCentury);
+
+  nodeHook.append(table);
+
+}
+
