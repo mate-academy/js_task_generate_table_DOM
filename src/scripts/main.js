f7f4925126bbdb2fc4fef6b91344a80e83978f1f
Vadym Tsyndra <Mr.Tsyndra@gmail.com>
2023-01-27 16:50:07 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,31 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (let i = 0; i < people.length; i++) {
+  const row = document.createElement('tr');
 
-// write your code here
+  const cellName = document.createElement('td');
+  const cellSex = document.createElement('td');
+  const cellBorn = document.createElement('td');
+  const cellDied = document.createElement('td');
+  const cellAge = document.createElement('td');
+  const cellCentury = document.createElement('td');
+
+  cellName.innerHTML = people[i].name;
+  cellSex.innerHTML = people[i].sex;
+  cellBorn.innerHTML = people[i].born;
+  cellDied.innerHTML = people[i].died;
+  cellAge.innerHTML = people[i].died - people[i].born;
+  cellCentury.innerHTML = Math.ceil(people[i].died / 100);
+
+  row.append(cellName);
+  row.append(cellSex);
+  row.append(cellBorn);
+  row.append(cellDied);
+  row.append(cellAge);
+  row.append(cellCentury);
+
+  const table = document.querySelector('.dashboard');
+
+  table.append(row);
+}
