cad82a86c2a28b25d0e1a774c4d510a764e45341
Kate <novak5683@gmail.com>
2023-02-07 12:51:35 +0100

    result fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,34 +2,19 @@
 
 const people = require('./lib/people');
 
-const tbl = document.getElementsByClassName('dashboard')[0];
+const table = document.querySelector('.dashboard');
 
-people.forEach(el => {
-  const tr = document.createElement('tr');
-  let td = document.createElement('td');
+for (const person of people) {
+  const row = document.createElement('tr');
 
-  td.innerHTML = el.name;
-  tr.append(td);
+  table.firstElementChild.append(row);
 
-  td = document.createElement('td');
-  td.innerHTML = el.sex === 'f' ? 'Female' : 'Male';
-  tr.append(td);
-
-  td = document.createElement('td');
-  td.innerHTML = el.born;
-  tr.append(td);
-
-  td = document.createElement('td');
-  td.innerHTML = el.died;
-  tr.append(td);
-
-  td = document.createElement('td');
-  td.innerHTML = el.died - el.born;
-  tr.append(td);
-
-  td = document.createElement('td');
-  td.innerHTML = Math.ceil(el.died / 100);
-  tr.append(td);
-
-  tbl.append(tr);
-});
+  row.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `;
+}
