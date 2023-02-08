df0745da54e6619cfa05a5907f7759ad04aa9ad5
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-02-08 15:38:51 +0200

    table done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const people = require('./lib/people');
+const tableBody = document.querySelector('tbody');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const createPersonRow = (person) => {
+  const row = document.createElement('tr');
 
-// write your code here
+  row.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex === 'f' ? 'Female' : 'Male'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `;
+
+  return row;
+};
+
+for (const person of people) {
+  tableBody.append(createPersonRow(person));
+}
