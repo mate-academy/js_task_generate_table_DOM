eab41fe12e27eb5b056d6ef315e19907f6197868
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-09 17:06:57 +0200

    task was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,36 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+function createCell(text) {
+  const cell = document.createElement('td');
 
-// write your code here
+  cell.innerHTML = text;
+
+  return cell;
+}
+
+function createRow(row, cells) {
+  for (let i = 0; i < cells.length; i++) {
+    row.append(cells[i]);
+  }
+}
+
+for (let i = 0; i < people.length; i++) {
+  const tableRow = document.createElement('tr');
+  const age = people[i].died - people[i].born;
+  const century = Math.ceil(people[i].died / 100);
+
+  const cells = [
+    createCell(people[i].name),
+    createCell(people[i].sex),
+    createCell(people[i].born),
+    createCell(people[i].died),
+    createCell(age),
+    createCell(century),
+  ];
+
+  createRow(tableRow, cells);
+  table.append(tableRow);
+}
