9cc81fe5651c76bb6dcb4c2076687de071df1369
Vasyl Pavlenko <genlus@ukr.net>
2023-02-09 13:16:39 +0200

    Table was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,36 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboard = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('td');
+  const genderCell = document.createElement('td');
+  const bornCell = document.createElement('td');
+  const diedCell = document.createElement('td');
+  const ageCell = document.createElement('td');
+  const centuryCell = document.createElement('td');
 
-// write your code here
+  nameCell.textContent = person.name;
+  row.append(nameCell);
+
+  genderCell.textContent = person.sex === 'm'
+    ? 'Male'
+    : 'Female';
+  row.append(genderCell);
+
+  bornCell.textContent = person.born;
+  row.append(bornCell);
+
+  diedCell.textContent = person.died;
+  row.append(diedCell);
+
+  ageCell.textContent = person.died - person.born;
+  row.append(ageCell);
+
+  centuryCell.textContent = Math.ceil(person.died / 100);
+  row.append(centuryCell);
+
+  dashboard.append(row);
+});
