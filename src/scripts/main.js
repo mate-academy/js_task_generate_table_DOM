b3b770f9c2138e07d7916e307861bba7ce76d921
Max Zelenskyi <rio1391@gmail.com>
2023-02-11 15:45:12 +0100

    Generate table by Rio

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,17 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('tbody');
+
+people.forEach(element => {
+  table.insertAdjacentHTML('beforeend', `
+  <tr>
+  <td>${element.name}</td>
+  <td>${element.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${element.born}</td>
+  <td>${element.died}</td>
+  <td>${element.died - element.born}</td>
+  <td>${Math.ceil(element.died / 100)}</td>
+</tr>
+  `);
+});
