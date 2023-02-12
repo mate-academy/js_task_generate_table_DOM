21fa40429a1085ae779c30e12fadf0f4cac419be
Oleksandr <98667890az@gmail.com>
2023-02-12 17:37:18 +0200

    generate_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,38 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const row = document.createElement('tr');
 
-// write your code here
+  const personName = document.createElement('td');
+
+  personName.textContent = person.name;
+
+  const gender = document.createElement('td');
+
+  gender.textContent
+    = person.sex === 'm'
+      ? 'Male'
+      : 'Female';
+
+  const born = document.createElement('td');
+
+  born.textContent = person.born;
+
+  const died = document.createElement('td');
+
+  died.textContent = person.died;
+
+  const age = document.createElement('td');
+
+  age.textContent = person.died - person.born;
+
+  const century = document.createElement('td');
+
+  century.textContent = Math.ceil(person.died / 100);
+
+  row.append(personName, gender, born, died, age, century);
+  table.append(row);
+}
