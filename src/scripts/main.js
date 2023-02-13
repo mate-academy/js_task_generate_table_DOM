df5eee12a6631e8c78fa8dcb0d3ec15dc7e2986a
Nigga <yomzfk@gmail.com>
2023-02-13 17:04:05 +0200

    Fixed )

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,18 @@ const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
 people.map(person => {
-  table.firstElementChild.insertAdjacentHTML('beforeend', `
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  table.children[0].insertAdjacentHTML('beforeend', `
     <tr>
       <th>${person.name}</th>
-      <th>${person.sex === 'm' ? 'Male' : 'Female'}</th>
+      <th>${sex}</th>
       <th>${person.born}</th>
       <th>${person.died}</th>
-      <th>${person.age = person.died - person.born}</th>
-      <th>${person.century = Math.ceil(person.died / 100)}</th>
+      <th>${age}</th>
+      <th>${century}</th>
     </tr>
   `);
 });
