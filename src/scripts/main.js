1b82016672d707fa31a5ce2049aca9b63b7e2878
KseniyaGonchar <kseniyadf888@gmail.com>
2023-02-15 13:10:37 +0200

    variables names changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,11 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-const result = document.querySelector('.dashboard');
-const arr = JSON.parse(JSON.stringify(people));
+const dashboard = document.querySelector('.dashboard');
+const arrPeople = JSON.parse(JSON.stringify(people));
 
-arr.map(person => {
-  const item = document.createElement('tr');
+arrPeople.map(person => {
+  const rowPerson = document.createElement('tr');
   const cellName = document.createElement('td');
 
   cellName.textContent = person.name;
@@ -34,7 +34,7 @@ arr.map(person => {
 
   cellCentury.textContent = Math.ceil(person.died / 100);
 
-  item.append(cellName, cellSex, cellBorn, cellDied, cellAge, cellCentury);
+  rowPerson.append(cellName, cellSex, cellBorn, cellDied, cellAge, cellCentury);
 
-  return result.append(item);
+  return dashboard.append(rowPerson);
 });
