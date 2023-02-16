150ed2539f396fc1fba9c7e6718200910aa8fa20
Max Zelenskyi <rio1391@gmail.com>
2023-02-16 20:20:54 +0100

    Update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,25 @@ const people = require('./lib/people');
 
 const table = document.querySelector('tbody');
 
-people.forEach(({
-  // eslint-disable-next-line no-shadow
-  name, sex, born, died, age = died - born, century = Math.ceil(died / 100),
-}) => {
+people.forEach(element => {
+  const {
+    // eslint-disable-next-line no-shadow
+    name,
+    sex,
+    born,
+    died,
+    age = died - born,
+    century = Math.ceil(died / 100),
+  } = element;
+
   table.insertAdjacentHTML('beforeend', `
-  <tr>
-  <td>${name}</td>
-  <td>${sex === 'm' ? 'Male' : 'Female'}</td>
-  <td>${born}</td>
-  <td>${died}</td>
-  <td>${age}</td>
-  <td>${century}</td>
-</tr>
-  `);
+    <tr>
+    <td>${name}</td>
+    <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${born}</td>
+    <td>${died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  </tr>
+    `);
 });
