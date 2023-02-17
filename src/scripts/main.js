5198dda70530cba1dfe4da8f15365bc67fb4dae2
Yevhenii Hura <yevheniihura@gmail.com>
2023-02-17 12:19:44 +0200

    the issues were solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,17 @@ const people = require('./lib/people');
 
 const table = document.querySelector('.dashboard');
 
-for (let i = 0; i < people.length; i++) {
+people.forEach(({ name, sex, born, died }) => {
   const tableRow = document.createElement('tr');
 
   tableRow.insertAdjacentHTML('beforeend', `
-  <td>${people[i].name}</td>
-  <td>${people[i].sex === 'm' ? 'Male' : 'Female'}</td>
-  <td>${people[i].born}</td>
-  <td>${people[i].died}</td>
-  <td>${people[i].died - people[i].born}</td>
-  <td>${Math.ceil(people[i].died / 100)}</td>
+  <td>${name}</td>
+  <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${born}</td>
+  <td>${died}</td>
+  <td>${died - born}</td>
+  <td>${Math.ceil(died / 100)}</td>
   `);
 
   table.append(tableRow);
-}
+});
