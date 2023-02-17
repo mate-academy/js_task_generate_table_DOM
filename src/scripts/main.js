421dfd2bee0680c236c616ef4288c55179c29ded
ivanm2706 <ivanmorhun664@gmail.com>
2023-02-17 08:22:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,38 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(({ name: namePerson, sex, born, died }) => {
+  let gender = null;
 
-// write your code here
+  if (sex === 'm') {
+    gender = 'Male';
+  } else {
+    gender = 'Female';
+  }
+
+  const tr = document.createElement('tr');
+  const tdName = document.createElement('td');
+  const tdGender = document.createElement('td');
+  const tdBorn = document.createElement('td');
+  const tdDied = document.createElement('td');
+  const tdAge = document.createElement('td');
+  const tdCentry = document.createElement('td');
+
+  tdName.textContent = namePerson;
+  tdGender.textContent = gender;
+  tdBorn.textContent = born;
+  tdDied.textContent = died;
+  tdAge.textContent = died - born;
+  tdCentry.textContent = Math.ceil(died / 100);
+
+  tr.append(tdName);
+  tr.append(tdGender);
+  tr.append(tdBorn);
+  tr.append(tdDied);
+  tr.append(tdAge);
+  tr.append(tdCentry);
+
+  table.append(tr);
+});
