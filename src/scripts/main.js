a9c4d190c4a87a6814cbb5857161f57111942b29
Dmytro Hubych <dh.hubych@gmail.com>
2023-02-19 21:06:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 const people = require('./lib/people');
+const board = document.querySelector('.dashboard').querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+  const row
+  = `
+    <tr>
+      <td>${person.name}</td>
+      <td>${gender}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `;
 
-// write your code here
+  board.insertAdjacentHTML('beforeend', row);
+});
