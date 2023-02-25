96ca7e5b3cac73018fe4f532f0d9528f07bbff0b
Mykola Butylkov <kokos.ibo.58@gmail.com>
2023-02-25 06:11:28 +0200

    Solution-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,31 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const dashboard = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  const fullName = document.createElement('td');
+  const sex = document.createElement('td');
+  const born = document.createElement('td');
+  const died = document.createElement('td');
+  const age = document.createElement('td');
+  const century = document.createElement('td');
+
+  fullName.textContent = `${person.name}`;
+  sex.textContent = `${person.sex}`;
+  born.textContent = `${person.born}`;
+  died.textContent = `${person.died}`;
+  age.textContent = `${person.died - person.born}`;
+
+  century.textContent = `${Math.ceil(person.died / 100)}`;
+
+  row.append(fullName);
+  row.append(sex);
+  row.append(born);
+  row.append(died);
+  row.append(age);
+  row.append(century);
+  dashboard.append(row);
+}
