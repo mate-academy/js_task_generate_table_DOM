b27d79407ca78ecc99bd3a1ce790d9b991f3ebe2
Andrew Litkovets <andriylao@gmail.com>
2023-03-02 22:50:12 +0200

    Task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,45 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const dashboard = document.querySelector('.dashboard');
+
+people
+  .sort((a, b) => (a.name).localeCompare(b.name))
+  .map((person) => {
+    const tableRow = document.createElement('tr');
+    const cellName = document.createElement('td');
+
+    cellName.textContent = person.name;
+
+    const cellGender = document.createElement('td');
+
+    cellGender.textContent = person.sex === 'm' ? 'Male' : 'Female';
+
+    const cellBorn = document.createElement('td');
+
+    cellBorn.textContent = person.born;
+
+    const cellDied = document.createElement('td');
+
+    cellDied.textContent = person.died;
+
+    const cellAge = document.createElement('td');
+
+    cellAge.textContent = person.died - person.born;
+
+    const cellCentury = document.createElement('td');
+
+    cellCentury.textContent = Math.ceil(person.died / 100);
+
+    tableRow.append(
+      cellName,
+      cellGender,
+      cellBorn,
+      cellDied,
+      cellAge,
+      cellCentury
+    );
+
+    return dashboard.append(tableRow);
+  });
