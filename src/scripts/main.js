2044172488d032b3f775884413a9f1d2f2b6472e
Roman Kulchytskyi-Dashynych <kdrs0812@gmail.com>
2023-03-02 14:57:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,17 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('tbody');
+
+people.forEach(el => {
+  table.insertAdjacentHTML('beforeend', `
+    <tr>
+      <td>${el.name}</td>
+      <td>${el.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${el.born}</td>
+      <td>${el.died}</td>
+      <td>${el.died - el.born}</td>
+      <td>${Math.ceil(el.died / 100)}</td>
+    </tr> `
+  );
+});
