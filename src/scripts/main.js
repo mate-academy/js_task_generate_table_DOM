c6f03ab799988c2c497cd1c977f7de5cfc8a5b8f
Tolik Petras <toliketras4@gmail.com>
2023-03-03 13:39:36 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('tbody');
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
 // write your code here
+for (const person of people) {
+  table.insertAdjacentHTML(`beforeend`, `
+    <tr>
+      <th>${person.name}</th>
+      <th>${person.sex}</th>
+      <th>${person.born}</th>
+      <th>${person.died}</th>
+      <th>${person.died - person.born}</th>
+      <th>${Math.ceil(person.died / 100)}</th>
+    </tr>
+  `);
+}
