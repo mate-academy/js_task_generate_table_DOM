51dd371bef70879805cf0a6fcc6f6b17480fb310
Andrew Litkovets <andriylao@gmail.com>
2023-03-03 12:21:36 +0200

    done with insertAdjacentHTML method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,44 +4,19 @@ const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
 
-const dashboard = document.querySelector('.dashboard');
+const tableRow = document.querySelector('.dashboard').firstElementChild;
 
 people
-  .sort((a, b) => (a.name).localeCompare(b.name))
   .map((person) => {
-    const tableRow = document.createElement('tr');
-    const cellName = document.createElement('td');
-
-    cellName.textContent = person.name;
-
-    const cellGender = document.createElement('td');
-
-    cellGender.textContent = person.sex === 'm' ? 'Male' : 'Female';
-
-    const cellBorn = document.createElement('td');
-
-    cellBorn.textContent = person.born;
-
-    const cellDied = document.createElement('td');
-
-    cellDied.textContent = person.died;
-
-    const cellAge = document.createElement('td');
-
-    cellAge.textContent = person.died - person.born;
-
-    const cellCentury = document.createElement('td');
-
-    cellCentury.textContent = Math.ceil(person.died / 100);
-
-    tableRow.append(
-      cellName,
-      cellGender,
-      cellBorn,
-      cellDied,
-      cellAge,
-      cellCentury
+    tableRow.insertAdjacentHTML(
+      'beforeend',
+      `<tr> 
+        <td>${person.name} </td>
+        <td>${person.sex === 'm' ? 'Male' : 'Female'} </td>
+        <td>${person.born} </td>
+        <td>${person.died} </td>
+        <td>${person.died - person.born} </td>
+        <td>${Math.ceil(person.died / 100)} </td>
+      </tr>`
     );
-
-    return dashboard.append(tableRow);
   });
