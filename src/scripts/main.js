cf3fa21b8d31a00a50020f3ad6928b14b7a5920b
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-14 14:57:12 +0100

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,33 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('table');
 
-// write your code here
+for (const person of people) {
+  const personRow = document.createElement('tr');
+  const personName = document.createElement('td');
+  const personGender = document.createElement('td');
+  const personBorn = document.createElement('td');
+  const personDied = document.createElement('td');
+  const personAge = document.createElement('td');
+  const personCentury = document.createElement('td');
+
+  personName.textContent = person.name;
+
+  person.sex === 'm'
+    ? personGender.textContent = 'Male'
+    : personGender.textContent = 'Female';
+
+  personBorn.textContent = person.born;
+  personDied.textContent = person.died;
+  personAge.textContent = person.died - person.born;
+  personCentury.textContent = Math.ceil(person.died / 100);
+
+  personRow.append(personName);
+  personRow.append(personGender);
+  personRow.append(personBorn);
+  personRow.append(personDied);
+  personRow.append(personAge);
+  personRow.append(personCentury);
+  table.append(personRow);
+}
