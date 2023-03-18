eac930ebde2596b1752b5a8d6215ecf2dd40fa07
anton_chornobai <antonchornobajj@gmail.com>
2023-03-18 19:35:58 +0100

    task was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,23 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+const dashboard = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const tableRow = document.createElement('tr');
+
+  const {name, sex, born, died} = person;
+
+  tableRow.innerHTML = `
+  <td>${name}</td>
+  <td>${sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${born}</td>
+  <td>${died}</td>
+  <td>${died - born}</td>
+  <td>${Math.ceil(died / 100)}</td>
+  `;
+
+  dashboard.append(tableRow);
+}
+
