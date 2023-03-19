8d3b36edf3960e83df067265fff6950dc9f82b82
anton_chornobai <antonchornobajj@gmail.com>
2023-03-19 10:20:12 +0100

    problem was solved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,6 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
 
 const dashboard = document.querySelector('.dashboard');
@@ -12,10 +9,10 @@ const dashboard = document.querySelector('.dashboard');
 for (const person of people) {
   const tableRow = document.createElement('tr');
 
-  const {name, sex, born, died} = person;
+  const { sex, born, died } = person;
 
   tableRow.innerHTML = `
-  <td>${name}</td>
+  <td>${person.name}}</td>
   <td>${sex === 'm' ? 'Male' : 'Female'}</td>
   <td>${born}</td>
   <td>${died}</td>
@@ -25,4 +22,3 @@ for (const person of people) {
 
   dashboard.append(tableRow);
 }
-
