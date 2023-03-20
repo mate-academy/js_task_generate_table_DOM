e328d3549704c9a74b56f0a6595979ae9f1d7076
Oleksandr Plias <pls777@ukr.net>
2023-03-20 22:20:03 +0200

    Fixed after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,14 +8,13 @@ for (const person of people) {
   const tableRow = document.createElement('tr');
 
   tableRow.innerHTML = `
-  <td>${person.name}</td>
-  <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
-  <td>${person.born}</td>
-  <td>${person.died}</td>
-  <td>${person.died - person.born}</td>
-  <td>${Math.ceil(person.died / 100)}</td>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
   `;
 
   table.append(tableRow);
 }
-
