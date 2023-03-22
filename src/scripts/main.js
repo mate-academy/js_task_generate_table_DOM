2b6727caaa3ecb0edfbda5c653f01cdf5097464a
Oleksandr Marchenko <sasha@mate.academy>
2023-03-22 18:48:08 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,17 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  table.insertAdjacentHTML('beforeend', `
+    <tr>
+      <th>${person.name}</th>
+      <th>${person.sex === 'm' ? 'Male' : 'Female'}</th>
+      <th>${person.born}</th>
+      <th>${person.died}</th>
+      <th>${person.died - person.born}</th>
+      <th>${Math.ceil(person.died / 100)}</th>
+    </tr>
+  `);
+}
