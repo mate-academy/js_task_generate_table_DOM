86700a52061bba292cb3b05f076656591cfe421b
Oleksandr Marchenko <sasha@mate.academy>
2023-03-22 19:04:12 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,12 @@ const table = document.querySelector('.dashboard');
 for (const person of people) {
   table.insertAdjacentHTML('beforeend', `
     <tr>
-      <th>${person.name}</th>
-      <th>${person.sex === 'm' ? 'Male' : 'Female'}</th>
-      <th>${person.born}</th>
-      <th>${person.died}</th>
-      <th>${person.died - person.born}</th>
-      <th>${Math.ceil(person.died / 100)}</th>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
     </tr>
   `);
 }
