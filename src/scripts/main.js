6e075a8ee0180a76c9cd2a5e1c72230873428805
Daria <hamdsad8@gmail.com>
2023-03-24 17:40:23 +0200

    create dymanic dashboard

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,25 @@
 
 const people = require('./lib/people');
 
+const GENDERS = {
+  'm': 'Male',
+  'f': 'Female',
+};
+
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const tableBody = document.querySelector('.dashboard > tbody');
+
+for (const person of people) {
+  tableBody.innerHTML += `
+    <tr>
+      <td>${person.name}</td>
+      <td>${GENDERS[person.sex]}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `
+}
