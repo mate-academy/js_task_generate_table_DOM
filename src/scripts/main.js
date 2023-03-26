23b485d01d36217c9582f5e9aff9a6a35e9d8cc0
Yaroslav Dudar <dudar.komod@gmail.com>
2023-03-26 06:59:44 +0300

    Rows was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,30 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+const addRow = function(person) {
+  person.age = person.died - person.born;
+  person.centyry = Math.ceil(person.died / 100);
+
+  if (person.sex === 'm') {
+    person.sex = 'Male';
+  } else {
+    person.sex = 'Female';
+  };
+
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+  <td>${person.name}</td>
+  <td>${person.sex}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.age}</td>
+  <td>${person.centyry}</td>
+  `;
+
+  return tr;
+};
+
+table.append(...people.map(addRow));
