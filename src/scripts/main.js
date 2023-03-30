8abc72323ecbdd44395d63f16b3286ae39ce71ce
DmytroHorshunov <cnimipignak@icloud.com>
2023-03-30 15:14:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const personAge = person.died - person.born;
+  const personCentury = Math.ceil(person.died / 100);
+  const row = document.createElement('tr');
 
-// write your code here
+  const personRow = `
+  <td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${personAge}</td>
+  <td>${personCentury}</td>
+`;
+
+  row.innerHTML = personRow;
+  table.append(row);
+}
