1ff206da3eae5caec05b3d4bcd9a521f83e93e4d
metja <mikolapenzanik@gmail.com>
2023-04-02 10:28:48 +0300

    Completed task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,34 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
 
-// write your code here
+  const row = document.createElement('tr');
+  const cellName = document.createElement('td');
+  const cellGender = document.createElement('td');
+  const cellBorn = document.createElement('td');
+  const cellDied = document.createElement('td');
+  const cellAge = document.createElement('td');
+  const cellCentury = document.createElement('td');
+
+  cellName.innerText = person.name;
+  cellGender.innerText = gender;
+  cellBorn.innerText = person.born;
+  cellDied.innerText = person.died;
+  cellAge.innerText = age;
+  cellCentury.innerText = century;
+
+  row.appendChild(cellName);
+  row.appendChild(cellGender);
+  row.appendChild(cellBorn);
+  row.appendChild(cellDied);
+  row.appendChild(cellAge);
+  row.appendChild(cellCentury);
+
+  table.append(row);
+}
