bdb59f4748ccea149840aa0a9b054bc6cfb765d0
Anna Peretiatko <tobossanna@gmail.com>
2023-04-03 22:16:39 +0200

    Generate table solution v.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,41 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+let tableHTML = '';
+
+for (const person of people) {
+  const personAge = person.died - person.born;
+  const personCentury = Math.ceil(person.died / 100);
+  let personGender;
+
+  if (person.sex === 'm') {
+    personGender = 'Male';
+  } else {
+    personGender = 'Female';
+  }
+
+  tableHTML += `
+    <tr>
+      <td>${person.name}</td>
+      <td>${personGender}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${personAge}</td>
+      <td>${personCentury}</td>
+    </tr>
+  `;
+
+  table.innerHTML = `
+  <tr>
+    <th>Name</th>
+    <th>Gender</th>
+    <th>Born</th>
+    <th>Died</th>
+    <th>Age</th>
+    <th>Century</th>
+  </tr>
+  ${tableHTML}
+  `;
+}
