e397fd8f44fba0e606a27a3d8a61a44039a31e56
Viktor <vasha2020@gmail.com>
2023-04-03 12:36:45 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,17 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
 const table = document.querySelector('.dashboard');
 
 people.forEach((person) => {
-  const genderCell = person.sex === 'f' ? 'Female' : 'Male';
+  const gender = person.sex === 'f' ? 'Female' : 'Male';
   const tableRow = document.createElement('tr');
   const age = person.died - person.born;
   const century = Math.ceil(person.died / 100);
 
   tableRow.innerHTML = `
     <td>${person.name}</td>
-    <td>${genderCell}</td>
+    <td>${gender}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
     <td>${age}</td>
