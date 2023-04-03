90bb16310d0edb66e26fe50a959e9c16a31372a5
Oleh Rybachuk <ribachuk202@gmail.com>
2023-04-03 18:26:17 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,23 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const rootElement = document.querySelector('table');
+
+for (const person of people) {
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+  person.gender = person.sex === 'm' ? 'Male' : 'Female';
+
+  const rows = document.createElement('tr');
+
+  rows.innerHTML = `
+  <td> ${person.name}</td>
+  <td> ${person.gender}</td>
+  <td> ${person.born}</td>
+  <td> ${person.died}</td>
+  <td> ${person.age}</td>
+  <td> ${person.century}</td>
+  `;
+
+  rootElement.append(rows);
+}
