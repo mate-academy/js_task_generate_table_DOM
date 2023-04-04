8a8e132a335544dab68445374fdd9e456a57ce01
Anna Grechana <grechanaanna@gmial.com>
2023-04-05 00:33:10 +0300

    1st version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,27 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const element = document.querySelectorAll('.dashboard');
+
+for (const person of people) {
+  for (const i of element) {
+    if (person.sex === 'm') {
+      person.sex = 'male';
+    } else {
+      person.sex = 'female';
+    };
+
+    person.age = person.died - person.born;
+    person.century = Math.ceil(person.died / 100);
+
+    i.children[0].insertAdjacentHTML('beforeend', `
+         <tr>
+         <th>${person.name}</th>
+         <th>${person.sex}</th>
+         <th>${person.born}</th>
+         <th>${person.died}</th>
+         <th>${person.age}</th>
+         <th>${person.century}</th>
+       </tr>`);
+  }
+}
