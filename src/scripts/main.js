fa65e342204440043d88f51f58b7797972d4a059
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-06 16:32:00 +0300

    js_task_generate_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,14 @@ const header = document.querySelector('.dashboard').rows[0];
 const newRow = document.createElement('tr');
 
 for (let i = 0; i < people.length; i++) {
+  let gender = '';
+
+  people[i].sex === 'f' ? gender = 'Male' : gender = 'Female';
+
   newRow.innerHTML = `
     <tr>
       <td>${people[i].name}</td>
-      <td>${people[i].sex}</td>
+      <td>${gender}</td>
       <td>${people[i].born}</td>
       <td>${people[i].died}</td>
       <td>${people[i].died - people[i].born}</td>
