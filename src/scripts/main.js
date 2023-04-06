5b661d1313f09bc45a64205c19db1c1f1140ef62
Oksana Tytanych <oksanatytanych@gmail.com>
2023-04-06 16:39:35 +0300

    forEach method was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,20 +5,20 @@ const people = require('./lib/people');
 const header = document.querySelector('.dashboard').rows[0];
 const newRow = document.createElement('tr');
 
-for (let i = 0; i < people.length; i++) {
+people.forEach(person => {
   let gender = '';
 
-  people[i].sex === 'f' ? gender = 'Male' : gender = 'Female';
+  person.sex === 'f' ? gender = 'Male' : gender = 'Female';
 
   newRow.innerHTML = `
     <tr>
-      <td>${people[i].name}</td>
+      <td>${person.name}</td>
       <td>${gender}</td>
-      <td>${people[i].born}</td>
-      <td>${people[i].died}</td>
-      <td>${people[i].died - people[i].born}</td>
-      <td>${Math.ceil(people[i].died / 100)}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
     </tr>
   `;
   header.after(newRow.cloneNode(true));
-}
+});
