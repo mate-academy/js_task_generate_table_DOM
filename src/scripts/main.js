81bb86bd7695d9f7c006221d9884df22310ea485
MarynaTovtyn <marina6782@gmail.com>
2023-04-06 21:58:30 +0300

    fixed spaces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,27 +7,27 @@ for (const person of people) {
   const newTableRow = document.createElement('tr');
 
   newTableRow.innerHTML = `
-      <tr>
-        <td>
-          ${person.name}
-        </td>
-        <td>
-          ${person.sex === 'm' ? 'Male' : 'Female'}
-        </td>
-        <td>
-          ${person.born}
-        </td>
-        <td>
-          ${person.died}
-        </td>
-        <td>
-          ${person.died - person.born}
-        </td>
-        <td>
-          ${Math.ceil(person.died / 100)}
-        </td>
-      </tr>
-    `;
+    <tr>
+      <td>
+        ${person.name}
+      </td>
+      <td>
+        ${person.sex === 'm' ? 'Male' : 'Female'}
+      </td>
+      <td>
+        ${person.born}
+      </td>
+      <td>
+        ${person.died}
+      </td>
+      <td>
+        ${person.died - person.born}
+      </td>
+      <td>
+        ${Math.ceil(person.died / 100)}
+      </td>
+    </tr>
+  `;
 
   dashboard.append(newTableRow);
 };
