f94783655482e31730d8b55ac29b45133b1376a7
Ilia Kolesnik <koleshnik160799@gmail.com>
2023-04-07 20:48:10 +0300

    Fix readme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,42 +3,41 @@
 const people = require('./lib/people');
 const tables = document.querySelector('.dashboard');
 
-const arrInfo = ['Name', 'Gender', 'Born', 'Died', 'Age', 'Century'];
+const arrInfoHeader = ['Name', 'Gender', 'Born', 'Died', 'Age', 'Century'];
 
 people.forEach(obj => {
   const tr = document.createElement('tr');
-  const keysObj = Object.keys(obj);
 
   tables.appendChild(tr);
 
-  for (let i = 0; i < arrInfo.length; i++) {
+  for (let i = 0; i < arrInfoHeader.length; i++) {
     const td = document.createElement('td');
 
     tr.appendChild(td);
 
-    if (arrInfo[i] === 'Name') {
+    if (arrInfoHeader[i] === 'Name') {
       td.innerText = obj.name;
     }
 
-    if (arrInfo[i] === 'Gender' && obj.sex === 'm') {
+    if (arrInfoHeader[i] === 'Gender' && obj.sex === 'm') {
       td.innerText = 'Male';
     } else if (obj.sex === 'f') {
       td.innerText = 'Female';
     }
 
-    if (arrInfo[i] === 'Born') {
+    if (arrInfoHeader[i] === 'Born') {
       td.innerText = obj.born;
     }
 
-    if (arrInfo[i] === 'Died') {
+    if (arrInfoHeader[i] === 'Died') {
       td.innerText = obj.died;
     }
 
-    if (arrInfo[i] === 'Age') {
+    if (arrInfoHeader[i] === 'Age') {
       td.innerText = obj.died - obj.born;
     }
 
-    if (arrInfo[i] === 'Century') {
+    if (arrInfoHeader[i] === 'Century') {
       td.innerText = Math.ceil(obj.born / 100);
     }
   }
