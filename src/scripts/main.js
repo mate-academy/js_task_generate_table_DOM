317d47682c1011fd6bed6275bacbca84c81a8b55
Max Vovk <vovkmax1@gmail.com>
2023-04-12 12:05:19 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const people = require('./lib/people');
+
 const table = document.querySelector('.dashboard');
 
-people.forEach(item =>
-  table.insertAdjacentHTML('beforeend', `
-  <tr>
+people.forEach(item => {
+  const row = document.createElement('tr');
+
+  row.insertAdjacentHTML('beforeend', `
   <td>${item.name}</td>
   <td>${item.sex === 'm' ? 'Male' : 'Female'}</td>
   <td>${item.born}</td>
   <td>${item.died}</td>
   <td>${item.died - item.born}</td>
   <td>${Math.ceil(item.died / 100)}</td>
-  </tr>
-`)
-);
+  `);
+
+  table.append(row);
+});
