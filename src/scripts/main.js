830fea9e61a34f0d64b943e4ad57e3ca6b32b822
Max Vovk <vovkmax1@gmail.com>
2023-04-12 12:46:08 +0300

    solition_V02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,12 +8,12 @@ people.forEach(item => {
   const row = document.createElement('tr');
 
   row.insertAdjacentHTML('beforeend', `
-  <td>${item.name}</td>
-  <td>${item.sex === 'm' ? 'Male' : 'Female'}</td>
-  <td>${item.born}</td>
-  <td>${item.died}</td>
-  <td>${item.died - item.born}</td>
-  <td>${Math.ceil(item.died / 100)}</td>
+    <td>${item.name}</td>
+    <td>${item.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${item.born}</td>
+    <td>${item.died}</td>
+    <td>${item.died - item.born}</td>
+    <td>${Math.ceil(item.died / 100)}</td>
   `);
 
   table.append(row);
