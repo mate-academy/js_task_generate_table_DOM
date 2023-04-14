33431508c49e5524bd2316739c47217503f5b528
meltourme <Meltourme@outlook.com>
2023-04-14 11:49:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,18 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.querySelector('.dashboard');
 
-// write your code here
+for (const every of people) {
+  const table = document.createElement('tr');
+
+  table.insertAdjacentHTML('beforeend', `
+  <td>${every.name}</td>
+  <td>${every.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${every.born}</td>
+  <td>${every.died}</td>
+  <td>${every.died - every.born}</td>
+  <td>${Math.ceil(every.died / 100)}</td>
+  `);
+  dashboard.append(table);
+}
