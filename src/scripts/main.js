5135f7696d8831c3cf047556497f3daaf7818a3c
Zibi95 <sylwester.sob95@gmail.com>
2023-04-17 17:50:11 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,29 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+function createRowWithData(person) {
+  const tableRow = document.createElement('tr');
+  // eslint-disable-next-line no-shadow
+  const { name, sex, born, died } = person;
 
-// write your code here
+  const markup = `
+  <td>${name}</td>
+  <td>${sex === 'f' ? 'Female' : 'Male'}</td>
+  <td>${born}</td>
+  <td>${died}</td>
+  <td>${died - born}</td>
+  <td>${Math.ceil(died / 100)}</td>
+  `;
+
+  tableRow.insertAdjacentHTML('beforeend', markup);
+
+  return tableRow;
+}
+
+function writeTable() {
+  const table = document.querySelector('.dashboard');
+
+  people.forEach(person => table.append(createRowWithData(person)));
+}
+
+writeTable();
