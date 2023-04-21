de32a20cf26d225c30ce7be76f5e25b50bb60b0b
Nadiein Oleh <olegnadein1994@gmail.com>
2023-04-21 12:08:24 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,41 @@
 'use strict';
 
 const people = require('./lib/people');
+// // {/* <tr>
+// <td>oleg</td>
+// <td>oleg</td>
+// <td>oleg</td>
+// <td>oleg</td>
+// <td>oleg</td>
+// </tr> */}
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const dashboard = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+
+  tr.append(td);
+
+  td.textContent = person.sex;
+
+  tr.append(td.cloneNode(true));
+  td.textContent = person.born;
+
+  tr.append(td.cloneNode(true));
+  td.textContent = person.died;
+
+  tr.append(td.cloneNode(true));
+  td.textContent = person.died - person.born;
+
+  tr.append(td.cloneNode(true));
+  td.textContent = Math.ceil(person.died / 100);
+
+  tr.append(td.cloneNode(true));
+  td.textContent = person.name;
+
+  dashboard.insertAdjacentElement('beforeend', tr);
+});
