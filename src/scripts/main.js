e0a544546a5feb7cf064a9f04e7ab26417fb681c
Hanna Vasylieva <ann0950368600@gmail.com>
2023-04-24 15:27:02 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,36 @@
 'use strict';
 
 const people = require('./lib/people');
+const tableBody = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  const newRow = document.createElement('tr');
 
-// write your code here
+  for (const key in person) {
+    const newCell = document.createElement('td');
+
+    newCell.textContent = person[key];
+
+    if (key === 'name'
+    || key === 'sex'
+    || key === 'born'
+    || key === 'died') {
+      newRow.appendChild(newCell);
+    }
+  }
+
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = age;
+  newRow.appendChild(ageCell);
+
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = century;
+  newRow.appendChild(centuryCell);
+
+  tableBody.appendChild(newRow);
+});
