48dae23ca0501c854a0a425a35d70183271b7d7d
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-26 16:49:41 +0200

    table was generated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,26 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  let gender;
 
-// write your code here
+  if (person.sex === 'm') {
+    gender = 'Male';
+  } else {
+    gender = 'Female';
+  }
+
+  table.insertAdjacentHTML('beforeend',
+    `
+    <tr>
+      <td>${person.name}</td>
+      <td>${gender}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+  `);
+});
