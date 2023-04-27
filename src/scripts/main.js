199d4783b53571860ecec3abe96158133a32e219
Antonio S <soi4.toxa@ukr.net>
2023-04-27 17:51:42 +0300

    Table-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,11 +11,12 @@ const tbody = table.firstElementChild;
 for (const person of people) {
   const personAge = person.died - person.born;
   const personCentury = Math.ceil(person.died / 100);
+  const gender = (person.sex === 'm') ? 'Male' : 'Female';
 
   tbody.insertAdjacentHTML('beforeend', `
     <tr>
       <td>${person.name}</td>
-      <td>${person.sex}</td>
+      <td>${gender}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
       <td>${personAge}</td>
