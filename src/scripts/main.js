e99af6cccba3e8d2c168bf8096885c2a53b12460
Roman <65686226+morozit@users.noreply.github.com>
2023-04-27 08:44:25 +0300

    add task moroz v3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,14 +6,15 @@ const table = document.querySelector('.dashboard');
 
 people.forEach(human => {
   const { sex, born, died } = human;
+  const gender = (sex) => sex === 'm' ? 'Male' : 'Famele';
   const nameHuman = human.name;
   const age = human.died - human.born;
   const century = Math.ceil(human.died / 100);
-  const arr = [nameHuman, sex, born, died, age, century];
+  const arr = [nameHuman, gender(sex), born, died, age, century];
   const row = document.createElement('tr');
 
   for (let i = 0; i < arr.length; i++) {
-    const cell = document.createElement('th');
+    const cell = document.createElement('td');
 
     cell.textContent = `${arr[i]}`;
     row.appendChild(cell);
