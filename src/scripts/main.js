909b7dc8a5b96ba868e990fcb61713dd6d2c753a
Nick <tortickk8475@gmail.com>
2023-04-28 15:12:25 +0300

    chore: add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,49 @@
 'use strict';
 
 const people = require('./lib/people');
+const tabledata = [];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+class Person {
+  constructor(params) {
+    this.name = params.name;
+    this.born = params.born;
+    this.died = params.died;
+    this.sex = params.sex;
+  }
 
-// write your code here
+  get gender() {
+    return this.sex === 'm' ? 'male' : 'female';
+  }
+  get age() {
+    return this.died - this.born;
+  }
+  get century() {
+    return Math.ceil(this.died / 100);
+  }
+}
+
+const setData = (crowd) => {
+  for (const per of crowd) {
+    tabledata.push(new Person(per));
+  }
+};
+
+const layoutTable = (database) => {
+  const table = document.querySelector('.dashboard');
+
+  for (const item of database) {
+    table.insertAdjacentHTML('beforeend', `
+       <tr>
+        <td>${item.name}</td>
+        <td>${item.gender}</td>
+        <td>${item.born}</td>
+        <td>${item.died}</td>
+        <td>${item.age}</td>
+        <td>${item.century}</td>
+      </tr>
+  `);
+  }
+};
+
+setData(people);
+layoutTable(tabledata);
