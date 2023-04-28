bf6e26f1a78e8f08559ea00e817206dc94f88446
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-29 00:51:18 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,25 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashboard = document.querySelector('.dashboard');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const row = document.createElement('tr');
 
-// write your code here
+  appendCell(row, person.name);
+  appendCell(row, person.sex);
+  appendCell(row, person.born);
+  appendCell(row, person.died);
+  appendCell(row, person.died - person.born);
+  appendCell(row, Math.ceil(person.died / 100));
+
+  dashboard.append(row);
+}
+
+function appendCell(row, content) {
+  const cell = document.createElement('td');
+
+  cell.innerText = content;
+  row.append(cell);
+}
