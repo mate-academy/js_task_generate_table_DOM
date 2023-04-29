c843eafcbac61f49b17a3b6e11e5440c80121b42
Roman <65686226+morozit@users.noreply.github.com>
2023-04-29 09:19:09 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,18 +6,15 @@ const table = document.querySelector('.dashboard');
 
 people.forEach(human => {
   const { sex, born, died } = human;
-  const gender = (personSex) => personSex === 'm' ? 'Male' : 'Femele';
+  const gender = (personSex) => personSex === 'm' ? 'Male' : 'Famele';
   const nameHuman = human.name;
-  const age = human.died - human.born;
+  const age = died - born;
   const century = Math.ceil(human.died / 100);
   const arr = [nameHuman, gender(sex), born, died, age, century];
   const row = document.createElement('tr');
 
   for (let i = 0; i < arr.length; i++) {
-    const cell = document.createElement('td');
-
-    cell.textContent = `${arr[i]}`;
-    row.appendChild(cell);
+    row.innerHTML += `<td>${arr[i]}</td>`;
   }
 
   if (table) {
