8d206793b9439b466915a9daa1fa667d5df1f789
Anastasiia Vorobets <anastasiiavorobets03@gmail.com>
2023-04-30 23:44:26 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const people = require('./lib/people');
+const dashTable = document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+people.forEach(person => {
+  dashTable.firstElementChild.insertAdjacentHTML('beforeend',
+    `<tr>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+    </tr>`
+  );
+});
