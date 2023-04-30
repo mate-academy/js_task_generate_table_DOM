46e9bd4f88a927d37265c5a635df94d7bbc1464a
Maria Shmakova <masha.shmakova@gmail.com>
2023-04-30 21:43:09 +0300

    changes were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,17 @@ const people = require('./lib/people');
 
 const tElement = document.querySelector('tbody');
 
-people.forEach(person => {
-  const gender = person.sex === 'm' ? 'Male' : 'Female';
+people.forEach(({ sex, names, born, died }) => {
+  const gender = sex === 'm' ? 'Male' : 'Female';
 
   tElement.insertAdjacentHTML('beforeend', `
     <tr>
-      <td>${person.name}</td>
+      <td>${names}</td>
       <td>${gender}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
-      <td>${person.died - person.born}</td>
-      <td>${Math.ceil(person.died / 100)}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${died - born}</td>
+      <td>${Math.ceil(died / 100)}</td>
     </tr>
   `);
 });
