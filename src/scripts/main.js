64c614896a0f0c19d93d05e5bda2669642d048d4
Roman <65686226+morozit@users.noreply.github.com>
2023-05-01 13:06:28 +0300

    update v9

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,8 @@ const people = require('./lib/people.json');
 const table = document.querySelector('.dashboard');
 
 people.forEach(human => {
-  const { sex, born, died } = human;
+  const { sex, born, died, name: nameHuman } = human;
   const gender = (personSex) => personSex === 'm' ? 'Male' : 'Female';
-  const nameHuman = human.name;
   const age = died - born;
   const century = Math.ceil(died / 100);
   const arr = [nameHuman, gender(sex), born, died, age, century];
