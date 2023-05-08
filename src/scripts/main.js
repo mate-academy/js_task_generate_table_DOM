a44f583370248886a9e81215b163c8a1dc690c55
Oleksii Lavka <lavka.alexey37gmail.com>
2023-05-08 15:07:12 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,41 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableElement = document.querySelector('.dashboard');
 
-// write your code here
+const generateTableData = (data, row) => {
+  for (const value of Object.values(data)) {
+    const tableData = document.createElement('td');
+
+    tableData.textContent = value;
+
+    row.appendChild(tableData);
+  }
+};
+
+const generateTableRows = (table, data) => {
+  for (const person of data) {
+    const tableRow = document.createElement('tr');
+
+    const { name: fullName, sex, born, died } = person;
+
+    const age = died - born;
+    const century = Math.ceil(died / 100);
+    const gender = sex === 'm' ? 'Male' : 'Female';
+
+    const personData = {
+      fullName,
+      gender,
+      born,
+      died,
+      age,
+      century,
+    };
+
+    generateTableData(personData, tableRow);
+
+    table.appendChild(tableRow);
+  }
+};
+
+generateTableRows(tableElement, people);
