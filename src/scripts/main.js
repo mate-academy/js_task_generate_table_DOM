782fa6d88e367f69d261862e9ebac2f9e11e13e5
DefaultGitUser <usingforgit@gmail.com>
2023-05-10 12:10:03 +0300

    Sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,27 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableEl = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  row.insertAdjacentHTML('beforeend', `
+        <td>${ person.name }</td>
+        <td>${ getGender(person.sex) }</td>
+        <td>${ person.born }</td>
+        <td>${ person.died }</td>
+        <td>${ person.died - person.born }</td>
+        <td>${ getCentury(person.died) }</td>
+    `);
+
+  tableEl.append(row);
+}
+
+function getGender(sex) {
+  return sex === 'f' ? 'Female' : 'Male';
+}
+
+function getCentury(died) {
+  return Math.ceil(died / 100);
+}
