a045979b19035b5154d4c92f0f4512c4c64fcd47
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-05-12 19:01:06 +0300

    table created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,21 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const row = document.querySelector('.dashboard').lastChild;
 
-// write your code here
+const genders = (gender) => {
+  return (gender === 'm') ? 'Male' : 'Feminine';
+};
+
+for (const person of people) {
+  row.insertAdjacentHTML('beforeend', `
+  <tr>
+    <th>${person.name}</th>
+    <th>${genders(person.sex)}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>
+  </tr>
+  `);
+};
