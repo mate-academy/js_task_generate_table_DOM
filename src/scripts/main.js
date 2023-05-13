ff73243b7889b8a394a57cf09e0c364e6e1879be
Vasyl Zhyliakov <snak871@gmail.com>
2023-05-13 13:03:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,17 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
+    <td>${person.name}</td>
+    <td>${(person.sex === 'm') ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td> 
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>  
+  `;
+
+  document.querySelector('.dashboard').append(row);
+}
