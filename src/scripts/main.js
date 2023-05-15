aa3b472c9f87164886e6ed7cc3cbcc521dae7bc5
Denys Didenko <ddidenko441@gmail.com>
2023-05-15 11:52:30 +0300

    add generate_table_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,19 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const dashboard = document.querySelector('.dashboard');
+
+people.map(person => {
+  dashboard.firstElementChild.insertAdjacentHTML('beforeend',
+    `
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+    `
+  );
+});
