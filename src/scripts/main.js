1912753be0d4212010be814229dabbbcd1aab543
Yelyzaveta <molochiza@gmail.com>
2023-05-16 13:24:31 +0300

    issues fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,20 @@
 const people = require('./lib/people');
 const table = document.querySelector('.dashboard');
 
-people.forEach(person => {
-  person.age = person.died - person.born;
-  person.century = Math.ceil(person.died / 100);
-  person.gender = person.sex === 'm' ? 'Male' : 'Female';
+people.forEach(({ name: personName, sex, born, died }) => {
+  const age = died - born;
+  const century = Math.ceil(died / 100);
+  const gender = sex === 'm' ? 'Male' : 'Female';
 
   const row = document.createElement('tr');
 
   row.innerHTML = `
-    <td> ${person.name}</td>
-    <td> ${person.gender}</td>
-    <td> ${person.born}</td>
-    <td> ${person.died}</td>
-    <td> ${person.age}</td>
-    <td> ${person.century}</td>
+    <td> ${personName}</td>
+    <td> ${gender}</td>
+    <td> ${born}</td>
+    <td> ${died}</td>
+    <td> ${age}</td>
+    <td> ${century}</td>
   `;
 
   table.append(row);
