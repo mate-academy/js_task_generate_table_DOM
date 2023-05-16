ab74e54e81b2aff90d840c02343c57389393f880
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-05-16 20:49:24 +0300

    Fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,6 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 document.addEventListener('DOMContentLoaded', () => {
   const peoplesTable = document.querySelector('.dashboard');
 
@@ -15,23 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   function createPersonInfoRow(person) {
-    // eslint-disable-next-line no-shadow
-    const { name, sex: gender, born, died } = person;
+    const { name: personName, sex: gender, born, died } = person;
     const century = Math.ceil(died / 100);
     const age = died - born;
-    const rowPersonInfo
-      = `
-        <td>${name}</td>
-        <td>${gender === 'm' ? 'Male' : 'Female'}</td>
-        <td>${born}</td>
-        <td>${died}</td>
-        <td>${age}</td>
-        <td>${century}</td>
-      `;
+    const rowPersonInfo = `
+      <td>${personName}</td>
+      <td>${gender === 'm' ? 'Male' : 'Female'}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    `;
     const rowPerson = document.createElement('tr');
 
     rowPerson.innerHTML = rowPersonInfo;
-    ;
 
     return rowPerson;
   }
