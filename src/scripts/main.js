a68734984517e26406e0a84454c25e27e2c6ea55
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-05-18 21:46:41 +0300

    Solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,62 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const bord = document.querySelector('.dashboard');
 
-// write your code here
+function dashboardGenerator(list) {
+  for (const value of [...list]) {
+    const newRow = document.createElement('tr');
+
+    delete value.slug;
+
+    for (const key in value) {
+      const rowData = document.createElement('th');
+
+      switch (key) {
+        case 'name':
+          rowData.textContent = value[key];
+          break;
+
+        case 'sex':
+          switch (value[key]) {
+            case 'f':
+              rowData.textContent = 'female';
+              break;
+
+            case 'm':
+              rowData.textContent = 'male';
+              break;
+
+            default:
+              break;
+          }
+          break;
+
+        case 'born':
+          rowData.textContent = value[key];
+          break;
+
+        case 'died':
+          rowData.textContent = value[key];
+          break;
+
+        case 'fatherName':
+          rowData.textContent = value.died - value.born;
+          break;
+
+        case 'motherName':
+          rowData.textContent = Math.ceil(value.died / 100);
+          break;
+
+        default:
+          break;
+      }
+
+      newRow.append(rowData);
+    }
+
+    bord.append(newRow);
+  }
+}
+
+dashboardGenerator(people);
