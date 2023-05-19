5b57a76c2858f0702b4fbf9c893b0e0ee8464d95
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-05-19 11:44:21 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,24 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+for (const person of people) {
+  const table = document.querySelector('.dashboard');
+  const tableRow = document.createElement('tr');
 
-// write your code here
+  const nam = person.name;
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const { sex, born, died } = person;
+
+  const personArrey = [nam, sex, born, died, age, century];
+
+  for (const el of personArrey) {
+    const tableData = document.createElement('td');
+
+    tableData.textContent = el;
+    tableRow.append(tableData);
+  }
+
+  table.append(tableRow);
+}
