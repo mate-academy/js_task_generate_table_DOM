accd1c4c99bb68aba6fd088187a6a85d2673bf97
Antonina Klishch <tonya91t@gmail.com>
2023-05-20 21:23:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,32 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
+const rootElement = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const row = document.createElement('tr');
+
+  const cellName = document.createElement('td');
+  const cellGender = document.createElement('td');
+  const cellBorn = document.createElement('td');
+  const cellDied = document.createElement('td');
+  const cellAge = document.createElement('td');
+  const cellCentery = document.createElement('td');
+
+  cellName.textContent = person.name;
+  cellGender.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  cellBorn.textContent = person.born;
+  cellDied.textContent = person.died;
+  cellAge.textContent = person.died - person.born;
+  cellCentery.textContent = Math.ceil(person.died / 100);
+
+  row.append(cellName);
+  row.append(cellGender);
+  row.append(cellBorn);
+  row.append(cellDied);
+  row.append(cellAge);
+  row.append(cellCentery);
+
+  rootElement.append(row);
+});
 // write your code here
