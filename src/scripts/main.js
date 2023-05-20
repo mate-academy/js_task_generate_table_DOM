4f204922a632b977899ec27b523d820258f68a47
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2023-05-20 14:53:08 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,22 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
 
 for (const person of people) {
-  const table = document.querySelector('.dashboard');
   const tableRow = document.createElement('tr');
 
-  const nam = person.name;
   const age = person.died - person.born;
   const century = Math.ceil(person.died / 100);
 
-  const { sex, born, died } = person;
-
-  const personArrey = [nam, sex, born, died, age, century];
+  const personArrey = [
+    person.name,
+    person.sex,
+    person.born,
+    person.died,
+    age,
+    century,
+  ];
 
   for (const el of personArrey) {
     const tableData = document.createElement('td');
