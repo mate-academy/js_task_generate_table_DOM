6c8e1411539a75323baabf60f2cca6b40208d677
AndriiYelieva <andrii.yelieva@gmail.com>
2023-05-21 18:43:46 +0300

    done, but tests not work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,30 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const current = document.querySelector('tr');
+
+people.forEach((person) => {
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+
+  if (person.sex === 'f') {
+    person.sex = 'female';
+  } else {
+    person.sex = 'male';
+  }
+
+  const table = document.createElement('tr');
+
+  current.parentElement.append(table);
+
+  table.insertAdjacentHTML(
+    'beforeend', `
+    <th>${person.name}</th>
+    <th>${person.sex}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.age}</th>
+    <th>${person.century}</th>
+    `
+  );
+});
