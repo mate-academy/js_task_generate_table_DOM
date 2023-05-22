f1989dbba1fbe2c5193c7cbee3ff693d542f3245
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-22 18:21:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,57 +5,20 @@ const people = require('./lib/people');
 const dash = document.querySelector('.dashboard');
 
 function addData(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    const newRow = dash.insertRow(i + 1);
-    let personBorn;
+  for (const item of arr) {
+    const count = 0;
+    const newRow = dash.insertRow(count + 1);
+    const { sex, born, died } = item;
+    const gender = sex === 'm' ? 'Male' : 'Female';
+    const age = died - born;
+    const century = Math.ceil(died / 100);
 
-    for (const j in arr[i]) {
-      let newCell;
-      let newText;
-
-      switch (j) {
-        case 'name':
-          newCell = newRow.insertCell(0);
-          newText = document.createTextNode(`${arr[i][j]}`);
-
-          newCell.append(newText);
-          break;
-        case 'sex':
-          newCell = newRow.insertCell(1);
-
-          if (arr[i][j] === 'm') {
-            newText = document.createTextNode(`Male`);
-          } else {
-            newText = document.createTextNode(`Female`);
-          }
-
-          newCell.append(newText);
-          break;
-        case 'born':
-          newCell = newRow.insertCell(2);
-          newText = document.createTextNode(`${arr[i][j]}`);
-          personBorn = arr[i][j];
-
-          newCell.append(newText);
-          break;
-        case 'died':
-          newCell = newRow.insertCell(3);
-          newText = document.createTextNode(`${arr[i][j]}`);
-
-          const ageCell = newRow.insertCell(4);
-          const ageText = arr[i][j] - personBorn;
-
-          const centuryCell = newRow.insertCell(5);
-          const centuryText = Math.ceil(arr[i][j] / 100);
-
-          newCell.append(newText);
-          ageCell.append(ageText);
-          centuryCell.append(centuryText);
-          break;
-        default:
-          continue;
-      }
-    }
+    newRow.insertAdjacentHTML('beforeend', `<td>${item.name}</td>`);
+    newRow.insertAdjacentHTML('beforeend', `<td>${gender}</td>`);
+    newRow.insertAdjacentHTML('beforeend', `<td>${born}</td>`);
+    newRow.insertAdjacentHTML('beforeend', `<td>${died}</td>`);
+    newRow.insertAdjacentHTML('beforeend', `<td>${age}</td>`);
+    newRow.insertAdjacentHTML('beforeend', `<td>${century}</td>`);
   }
 }
 
