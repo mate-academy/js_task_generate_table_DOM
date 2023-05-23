0879cde3b24b4cc3124a381dc4911d64571dbffe
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-23 10:28:29 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,23 +2,27 @@
 
 const people = require('./lib/people');
 
-const dash = document.querySelector('.dashboard');
+const dashboardRef = document.querySelector('.dashboard');
 
 function addData(arr) {
   for (const item of arr) {
     const count = 0;
-    const newRow = dash.insertRow(count + 1);
+    const newRow = dashboardRef.insertRow(count + 1);
     const { sex, born, died } = item;
     const gender = sex === 'm' ? 'Male' : 'Female';
     const age = died - born;
     const century = Math.ceil(died / 100);
 
-    newRow.insertAdjacentHTML('beforeend', `<td>${item.name}</td>`);
-    newRow.insertAdjacentHTML('beforeend', `<td>${gender}</td>`);
-    newRow.insertAdjacentHTML('beforeend', `<td>${born}</td>`);
-    newRow.insertAdjacentHTML('beforeend', `<td>${died}</td>`);
-    newRow.insertAdjacentHTML('beforeend', `<td>${age}</td>`);
-    newRow.insertAdjacentHTML('beforeend', `<td>${century}</td>`);
+    const rowHtml = `
+      <td>${item.name}</td>
+      <td>${gender}</td>
+      <td>${born}</td>
+      <td>${died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    `;
+
+    newRow.insertAdjacentHTML('beforeend', rowHtml);
   }
 }
 
