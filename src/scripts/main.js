852405622ce3c89211a218f69fc5948e171ece45
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-05-25 16:06:47 -0700

    js_task_generate_table_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,15 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => dashboard.insertAdjacentHTML('beforeend',
+  `<tr>
+    <td>${person.name}</td>
+    <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  </tr>
+`));
