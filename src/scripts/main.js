daa8b18948b4790012462bd3ca18d6d55beeeed9
Antonio Zuccon Ghiotto <antonio.zucconghiotto@gmail.com>
2023-05-25 16:40:12 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,31 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tr = document.querySelector('tr');
 
-// write your code here
+const putLibraryOnTable = () => {
+  for (let i = 0; i < people.length; i++) {
+    const row = document.createElement('tr');
+    const objectKeys = Object.keys(people[i]);
+
+    for (let j = 0; j < 6; j++) {
+      const element = document.createElement('td');
+
+      element.textContent = people[i][objectKeys[j]];
+
+      if (j === 5) {
+        element.textContent = Math.ceil(people[i].died / 100);
+      }
+
+      if (j === 4) {
+        element.textContent = people[i].died - people[i].born;
+      }
+
+      row.appendChild(element);
+    }
+
+    tr.insertAdjacentElement('afterend', row);
+  }
+};
+
+putLibraryOnTable();
