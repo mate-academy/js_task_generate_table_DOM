762f9b61236c5cfc2845c3f73df2be061403469c
Alessandra Nitti <alessandrannitti@gmail.com>
2023-05-25 20:23:01 +0300

    table done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,22 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+function addRow(arg) {
+  const table = document.querySelector('table');
+
+  for (const person of people) {
+    const newRow = table.insertRow();
+
+    newRow.innerHTML = `
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    `;
+  }
+}
+
+addRow('table');
