4cbedabe6339bf07bebb03b02cdc8441fc26a2bc
Jane Shavrukova <eva.shavrukova@gmail.com>
2023-05-26 11:26:35 -0700

    js_task_generate_table_DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,19 @@
 
 const people = require('./lib/people');
 
-const dashboard = document.querySelector('.dashboard');
+const table = document.querySelector('.dashboard');
 
-people.forEach(person => dashboard.insertAdjacentHTML('beforeend',
-  `<tr>
+people.forEach(person => {
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
     <td>${person.name}</td>
     <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
     <td>${person.died - person.born}</td>
     <td>${Math.ceil(person.died / 100)}</td>
-  </tr>
-`));
+  `;
+
+  table.append(row);
+});
