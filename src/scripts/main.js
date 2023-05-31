8142de61f7f2b65d81ae95acae5b5b9d8c8b8503
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2023-05-31 17:20:53 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,30 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+people.map(person => {
+  person.century = Math.ceil(person.died / 100);
+  person.age = person.died - person.born;
+
+  (person.sex === 'f') ? person.sex = 'Female' : person.sex = 'Male';
+
+  return person;
+});
+
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const string = document.createElement('tr');
+
+  table.append(string);
+
+  string.innerHTML = `
+      <tr>
+        <th>${person.name}</th>
+        <th>${person.sex}</th>
+        <th>${person.born}</th>
+        <th>${person.died}</th>
+        <th>${person.age}</th>
+        <th>${person.century}</th>
+      </tr>
+    `;
+});
