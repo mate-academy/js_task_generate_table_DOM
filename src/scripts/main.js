79452b42cc68fa6e23ce33b3c3e9d39ed875a1ae
domkab <dominykas.bobinas@gmail.com>
2023-06-04 17:21:40 +0300

    1.0.0 initial solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,33 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+console.log(people);
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+  const sexCell = document.createElement('td');
+  const bornCell = document.createElement('td');
+  const diedCell = document.createElement('td');
+  const AgeCell = document.createElement('td');
+  const CenturyCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+  sexCell.textContent = person.sex;
+  bornCell.textContent = person.born;
+  diedCell.textContent = person.died;
+  AgeCell.textContent = (person.died - person.born);
+  CenturyCell.textContent = Number(Math.ceil(person.died / 100));
+
+  row.appendChild(nameCell);
+  row.appendChild(sexCell);
+  row.appendChild(bornCell);
+  row.appendChild(diedCell);
+  row.appendChild(AgeCell);
+  row.appendChild(CenturyCell);
+
+  table.appendChild(row);
+});
