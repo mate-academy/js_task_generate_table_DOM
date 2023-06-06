3c391a8d5878fcd952c9e61be7c59f140be54a49
Hishchak Tetiana <tanaiosered@ukr.net>
2023-06-06 13:42:00 +0300

    table2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,14 +3,23 @@
 const people = require('./lib/people');
 const tableContainer = document.querySelector('.dashboard');
 
+function Gender(el) {
+  if (el === 'm') {
+    return 'Male';
+  }
+
+  return 'Female';
+}
+
 for (const person of people) {
   const century = Math.ceil(person.died / 100);
   const age = person.died - person.born;
+  const sex = person.sex;
 
-  tableContainer.insertAdjacentHTML('beforeend', `
+  tableContainer.children[0].insertAdjacentHTML('beforeend', `
   <tr>
     <td>${person.name}</td>
-    <td>${person.sex}</td>
+    <td>${Gender(sex)}</td>
     <td>${person.born}</td>
     <td>${person.died}</td>
     <td>${age}</td>
