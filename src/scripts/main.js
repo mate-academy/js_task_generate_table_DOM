29c890121625c4c950f404525fa6198401a77bc5
domkab <dominykas.bobinas@gmail.com>
2023-06-07 15:59:14 +0300

    1.0.1 fixed camel case

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,6 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people);
-
 const table = document.querySelector('.dashboard');
 
 people.forEach(person => {
@@ -14,22 +11,22 @@ people.forEach(person => {
   const sexCell = document.createElement('td');
   const bornCell = document.createElement('td');
   const diedCell = document.createElement('td');
-  const AgeCell = document.createElement('td');
-  const CenturyCell = document.createElement('td');
+  const ageCell = document.createElement('td');
+  const centuryCell = document.createElement('td');
 
   nameCell.textContent = person.name;
   sexCell.textContent = person.sex;
   bornCell.textContent = person.born;
   diedCell.textContent = person.died;
-  AgeCell.textContent = (person.died - person.born);
-  CenturyCell.textContent = Number(Math.ceil(person.died / 100));
+  ageCell.textContent = (person.died - person.born);
+  centuryCell.textContent = Number(Math.ceil(person.died / 100));
 
   row.appendChild(nameCell);
   row.appendChild(sexCell);
   row.appendChild(bornCell);
   row.appendChild(diedCell);
-  row.appendChild(AgeCell);
-  row.appendChild(CenturyCell);
+  row.appendChild(ageCell);
+  row.appendChild(centuryCell);
 
   table.appendChild(row);
 });
