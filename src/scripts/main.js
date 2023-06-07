3e69be604a4963f2d0ef98049897470105fe5f3d
Vo7kov <jegor.volkov.a@gmail.com>
2023-06-07 16:13:12 -0700

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 const people = require('./lib/people');
+const table = document.querySelector('.dashboard');
+const tableBody = table.querySelector('tbody');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach(person => {
+  if (person.sex === 'm') {
+    person.sex = 'Male';
+  } else {
+    person.sex = 'Female';
+  }
 
-// write your code here
+  tableBody.insertAdjacentHTML('beforeend', `
+    <tr>
+      <th>${person.name}</th>
+      <th>${person.sex}</th>
+      <th>${person.born}</th>
+      <th>${person.died}</th>
+      <th>${person.died - person.born}</th>
+      <th>${Math.ceil(person.died / 100)}</th>
+    </tr>
+  `);
+});
