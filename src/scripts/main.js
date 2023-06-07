403093a49c335912a60d3ed06e29dab28773a6ca
Mykola Plekan <zikzar@gmail.com>
2023-06-07 23:02:41 +0300

    Fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,24 +7,12 @@ const table = document.querySelector('tbody');
 people.forEach(person => {
   table.insertAdjacentHTML('beforeend', `
     <tr>
-      <td>
-        ${person.name}
-      </td>
-      <td>
-        ${person.sex === 'f' ? 'Female' : 'Male'}
-      </td>
-      <td>
-        ${person.born}
-      </td>
-      <td>
-        ${person.died}
-      </td>
-      <td>
-        ${person.died - person.born}
-      </td>
-      <td>
-        ${Math.ceil(person.died / 100)}
-      </td>
+      <td>${person.name}</td>
+      <td>${person.sex === 'f' ? 'Female' : 'Male'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
     </tr>
   `);
 });
