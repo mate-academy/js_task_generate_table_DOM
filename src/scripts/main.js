71e637eb2e0140b57b79f96b6ee49388a4f3d895
Leonid Krainik <rialleons@gmail.com>
2023-06-08 18:13:56 +0300

    Simplify with insertAdjacentHTML

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,26 +6,27 @@ const table = document.querySelector('.dashboard');
 
 for (const person of people) {
   const row = document.createElement('tr');
-  const fullName = document.createElement('td');
-  const gender = document.createElement('td');
-  const born = document.createElement('td');
-  const died = document.createElement('td');
-  const age = document.createElement('td');
-  const century = document.createElement('td');
 
-  fullName.innerText = person.name;
-  gender.innerText = person.sex === 'm' ? 'Male' : 'Female';
-  born.innerText = person.born;
-  died.innerText = person.died;
-  age.innerText = person.died - person.born;
-  century.innerText = Math.ceil(person.died / 100);
-
-  row.append(fullName);
-  row.append(gender);
-  row.append(born);
-  row.append(died);
-  row.append(age);
-  row.append(century);
+  row.insertAdjacentHTML('beforeend', `
+    <td>
+      ${person.name}
+    </td>
+    <td>
+      ${person.sex === 'm' ? 'Male' : 'Female'}
+    </td>
+    <td>
+      ${person.born}
+    </td>
+    <td>
+      ${person.died}
+    </td>
+    <td>
+      ${person.died - person.born}
+    </td>
+    <td>
+      ${Math.ceil(person.died / 100)}
+    </td>
+  `);
 
   table.append(row);
 }
