a1049c0bbb824a0cdf4fe5394818f31c0819648f
Leonid Krainik <rialleons@gmail.com>
2023-06-08 17:38:35 +0300

    Generate dashboard from JSON

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,30 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+  const fullName = document.createElement('td');
+  const gender = document.createElement('td');
+  const born = document.createElement('td');
+  const died = document.createElement('td');
+  const age = document.createElement('td');
+  const century = document.createElement('td');
+
+  fullName.innerText = person.name;
+  gender.innerText = person.sex === 'm' ? 'Male' : 'Female';
+  born.innerText = person.born;
+  died.innerText = person.died;
+  age.innerText = person.died - person.born;
+  century.innerText = Math.ceil(person.died / 100);
+
+  row.append(fullName);
+  row.append(gender);
+  row.append(born);
+  row.append(died);
+  row.append(age);
+  row.append(century);
+
+  table.append(row);
+}
