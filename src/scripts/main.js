6ff82a6e274226fbe35840a18f165ad6cc8984a7
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-08 18:06:14 +0300

    code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,16 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const newTable = document.querySelector(`.dashboard`);
+const newTableBody = newTable.firstElementChild;
 
-// write your code here
+people.forEach(element => {
+  newTableBody.insertAdjacentHTML('beforeend', `
+  <td>${element.name}</td>
+  <td>${element.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${element.born}</td>
+  <td>${element.died}</td>
+  <td>${element.died - element.born}</td>
+  <td>${Math.ceil(element.died / 100)}</td>
+  `);
+});
