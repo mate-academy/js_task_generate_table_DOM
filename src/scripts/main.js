0f6bd20913f4585d1153ccefc43bba51e7960db1
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-06-11 23:38:07 +0300

    js_task_generate_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,40 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => {
+  const newRow = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+  newRow.appendChild(nameCell);
+
+  const sexCell = document.createElement('td');
+
+  sexCell.textContent = person.sex;
+  newRow.appendChild(sexCell);
+
+  const bornCell = document.createElement('td');
+
+  bornCell.textContent = person.born;
+  newRow.appendChild(bornCell);
+
+  const deathCell = document.createElement('td');
+
+  deathCell.textContent = person.died;
+  newRow.appendChild(deathCell);
+
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = person.died - person.born;
+  newRow.appendChild(ageCell);
+
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = Math.ceil(person.died / 100);
+  newRow.appendChild(centuryCell);
+
+  table.appendChild(newRow);
+});
