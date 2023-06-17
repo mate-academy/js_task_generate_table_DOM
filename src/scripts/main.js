14a284130b18eeb0ca21334dd52746b763486bcb
Hanna Anishyna <ann.986@gmail.com>
2023-06-16 17:00:18 -0700

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,4 +5,28 @@ const people = require('./lib/people');
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+const peopleData = people.map(person => {
+  return {
+    name: person.name,
+    gender: person.sex === 'm' ? 'Male' : 'Female',
+    born: person.born,
+    died: person.died,
+    age: person.died - person.born,
+    century: Math.ceil(person.died / 100),
+  };
+});
+
+function createTable(peopleList) {
+  for (const item of peopleList) {
+    const row = table.insertRow();
+
+    for (const key in item) {
+      const cell = row.insertCell();
+
+      cell.append(item[key]);
+    }
+  }
+}
+
+createTable(peopleData);
