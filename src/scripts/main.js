b2fbc9d8fa908eb72b0c3ec6bf8bd3994ead972b
spencerspe <spencerspe@gmail.com>
2023-07-04 12:57:27 +0100

    first  solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,41 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+
+people.forEach(person => {
+  const table = document.querySelector('.dashboard');
+
+  const newRow = document.createElement('tr');
+
+  const nameInput = document.createElement('td');
+
+  nameInput.textContent = person.name;
+  newRow.appendChild(nameInput);
+
+  const sexInput = document.createElement('td');
+
+  sexInput.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  newRow.appendChild(sexInput);
+
+  const bornInput = document.createElement('td');
+
+  bornInput.textContent = person.born;
+  newRow.appendChild(bornInput);
+
+  const diedInput = document.createElement('td');
+
+  diedInput.textContent = person.died;
+  newRow.appendChild(diedInput);
+
+  const ageInput = document.createElement('td');
+
+  ageInput.textContent = person.died - person.born;
+  newRow.appendChild(ageInput);
+
+  const centuryInput = document.createElement('td');
+
+  centuryInput.textContent = Math.ceil(person.died / 100);
+  newRow.appendChild(centuryInput);
+
+  table.appendChild(newRow);
+});
