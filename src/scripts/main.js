bc4cd1725189bf627ecc7438cf7721b97ccab7f1
spencerspe <spencerspe@gmail.com>
2023-07-18 12:22:53 +0100

    destructuring added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,34 +2,34 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
 
+const table = document.querySelector('.dashboard');
+
 people.forEach(person => {
-  const table = document.querySelector('.dashboard');
+  // eslint-disable-next-line no-shadow
+  const { name, sex, born, died } = person;
 
   const newRow = document.createElement('tr');
 
   const nameInput = document.createElement('td');
 
-  nameInput.textContent = person.name;
+  nameInput.textContent = name;
   newRow.appendChild(nameInput);
 
   const sexInput = document.createElement('td');
 
-  sexInput.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  sexInput.textContent = sex === 'm' ? 'Male' : 'Female';
   newRow.appendChild(sexInput);
 
   const bornInput = document.createElement('td');
 
-  bornInput.textContent = person.born;
+  bornInput.textContent = born;
   newRow.appendChild(bornInput);
 
   const diedInput = document.createElement('td');
 
-  diedInput.textContent = person.died;
+  diedInput.textContent = died;
   newRow.appendChild(diedInput);
 
   const ageInput = document.createElement('td');
