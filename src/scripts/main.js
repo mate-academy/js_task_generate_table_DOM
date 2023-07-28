3eb8c75ba6af915977c8429f0384a3a37f41f6c3
Artem Velychko <avelychko@icloud.com>
2023-07-28 17:26:08 -0400

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,34 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+for (const person of people) {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+
+  const newRow = document.createElement('tr');
+  const tdName = document.createElement('td');
+  const tdSex = document.createElement('td');
+  const tdBorn = document.createElement('td');
+  const tdDied = document.createElement('td');
+  const tdAge = document.createElement('td');
+  const tdCentury = document.createElement('td');
+
+  tdName.textContent = person.name;
+  tdSex.textContent = gender;
+  tdBorn.textContent = person.born;
+  tdDied.textContent = person.died;
+  tdAge.textContent = age;
+  tdCentury.textContent = century;
+
+  newRow.appendChild(tdName);
+  newRow.appendChild(tdSex);
+  newRow.appendChild(tdBorn);
+  newRow.appendChild(tdDied);
+  newRow.appendChild(tdAge);
+  newRow.appendChild(tdCentury);
+
+  table.appendChild(newRow);
+}
