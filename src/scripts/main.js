95edc77dd9fbfb8914f5c9e1a8bdd4a79f12df39
Karolina <karolinabednarek888@gmail.com>
2023-07-28 14:39:47 +0200

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,25 +3,27 @@
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
-console.log(people);
 
 people.forEach(item => {
   const age = item.died - item.born;
 
   const century = Math.ceil(item.died / 100);
 
+  const sex = item.sex === 'm' ? 'Male'
+    : item.sex === 'f' ? 'Female' : 'Unknown';
+
   const tr = document.createElement('tr');
 
   tr.innerHTML = `
   <td>${item.name}</td>
-  <td>${item.sex}</td>
+  <td>${sex}</td>
   <td>${item.born}</td>
   <td>${item.died}</td>
   <td>${age}</td>
   <td>${century}</td>
   `;
 
-  const a = document.querySelector('.dashboard');
+  const dashboard = document.querySelector('.dashboard');
 
-  a.lastChild.append(tr);
+  dashboard.lastChild.append(tr);
 });
