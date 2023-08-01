f44deb3adf810a7b0f89cd73bf9cb4c57d92aa94
Volodymyr Biletskyi <volodymyrbiletskyi92@gmail.com>
2023-08-01 14:43:52 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,45 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+function calculateAge(person) {
+  return person.died - person.born;
+}
 
-// write your code here
+function calculateCentury(person) {
+  return Math.ceil(person.died / 100);
+}
+
+function createTableRows() {
+  const table = document.querySelector('.dashboard');
+
+  people.forEach((person) => {
+    const age = calculateAge(person);
+    const century = calculateCentury(person);
+
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+    const genderCell = document.createElement('td');
+    const bornCell = document.createElement('td');
+    const diedCell = document.createElement('td');
+    const ageCell = document.createElement('td');
+    const centuryCell = document.createElement('td');
+
+    nameCell.textContent = person.name;
+    genderCell.textContent = person.sex;
+    bornCell.textContent = person.born;
+    diedCell.textContent = person.died;
+    ageCell.textContent = age;
+    centuryCell.textContent = century;
+
+    row.appendChild(nameCell);
+    row.appendChild(genderCell);
+    row.appendChild(bornCell);
+    row.appendChild(diedCell);
+    row.appendChild(ageCell);
+    row.appendChild(centuryCell);
+
+    table.appendChild(row);
+  });
+}
+
+createTableRows();
