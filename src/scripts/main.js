e762e8bb80762ba5d02102ea8b70255f677b3148
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-08-13 20:34:24 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,14 +5,18 @@ const table = document.querySelector('.dashboard');
 const tableBody = table.querySelector('tbody');
 
 people.forEach(person => {
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
   const row = `
     <tr>
       <td>${person.name}</td>
-      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${sex}</td>
       <td>${person.born}</td>
       <td>${person.died}</td>
-      <td>${person.died - person.born}</td>
-      <td>${Math.ceil(person.died / 100)}</td>
+      <td>${age}</td>
+      <td>${century}</td>
     </tr>
   `;
 
