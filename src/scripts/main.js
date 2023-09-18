e97506e17151b1d528933458278e9d441d8045f2
Eugene Ivchenko <ivchenkoevhen@gmail.com>
2023-09-19 01:11:33 +0300

    fixed tests

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,18 @@
 
 const people = require('./lib/people');
 
-const tableElement = document.querySelector('table');
+const tableElement = document.querySelector('.dashboard');
 
 for (const person of people) {
   const tableRowElement = document.createElement('tr');
 
   tableRowElement.innerHTML = `
-    <th>${person.name}</th>
-    <th>${person.sex === 'f' ? 'Female' : 'Male'}</th>
-    <th>${person.born}</th>
-    <th>${person.died}</th>
-    <th>${person.died - person.born}</th>
-    <th>${Math.ceil(person.died / 100)}</th>`;
+    <td>${person.name}</td>
+    <td>${person.sex === 'f' ? 'Female' : 'Male'}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>`;
 
   tableElement.append(tableRowElement);
 }
