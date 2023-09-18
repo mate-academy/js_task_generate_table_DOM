f50df2017d8fe4e7e0fddc59deb2fec564affa58
Eugene Ivchenko <ivchenkoevhen@gmail.com>
2023-09-19 01:03:46 +0300

    working version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,18 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableElement = document.querySelector('table');
 
-// write your code here
+for (const person of people) {
+  const tableRowElement = document.createElement('tr');
+
+  tableRowElement.innerHTML = `
+    <th>${person.name}</th>
+    <th>${person.sex === 'f' ? 'Female' : 'Male'}</th>
+    <th>${person.born}</th>
+    <th>${person.died}</th>
+    <th>${person.died - person.born}</th>
+    <th>${Math.ceil(person.died / 100)}</th>`;
+
+  tableElement.append(tableRowElement);
+}
