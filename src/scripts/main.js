aa57db69dd720ffb13922890f295ef260df199ec
Ivan <your@mail.com>
2023-09-19 20:01:24 +0300

    Table are generate!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,19 @@ const people = require('./lib/people');
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
+  <th>${person.name}</th>
+  <th>${person.sex}</th>
+  <th>${person.born}</th>
+  <th>${person.died}</th>
+  <th>${person.died - person.born}</th>
+  <th>${Math.ceil(person.died / 100)}</th>
+  `;
+
+  table.appendChild(row);
+});
