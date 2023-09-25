78176efe3332486609ca067fe1ea0ecc0dfe8fd5
meryam wazeen <maryamwazeen2014@gmail.com>
2023-09-25 15:07:43 +0300

    changed gender to SEX

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,11 +20,33 @@ people.forEach(person => {
   const { age, century } = calculateAgeAndCentury(person);
 
   const row = document.createElement('tr');
+  // Create and append table cells for each person's data:
+
+  // const nameCell = document.createElement('td');
+  // nameCell.textContent = person.name;
+  // const genderCell = document.createElement('td');
+  // genderCell.textContent = person.gender;
+  // const bornCell = document.createElement('td');
+  // bornCell.textContent = person.born;
+  // const diedCell = document.createElement('td');
+  // diedCell.textContent = person.died;
+  // const ageCell = document.createElement('td');
+  // ageCell.textContent = age;
+  // const centuryCell = document.createElement('td');
+  // centuryCell.textContent = century;
+
+  // Append the cells to the row
+  // row.appendChild(nameCell);
+  // row.appendChild(genderCell);
+  // row.appendChild(bornCell);
+  // row.appendChild(diedCell);
+  // row.appendChild(ageCell);
+  // row.appendChild(centuryCell);
 
   row.innerHTML = `
 
   <th>${person.name}</th>
-  <th>${person.gender}</th>
+  <th>${person.sex}</th>
   <th>${person.born}</th>
   <th>${person.died}</th>
   <th>${age}</th>
