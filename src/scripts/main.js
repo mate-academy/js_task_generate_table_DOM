8c851b24bed642ef0690d17b0730b8d5994083f2
AndriiYelieva <andrew.yelieva@gmail.com>
2023-10-12 20:12:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,29 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => {
+  const wrapperTable = document.createElement('tr');
+  const tdName = document.createElement('td');
+  const tdSex = document.createElement('td');
+  const tdBorn = document.createElement('td');
+  const tdDied = document.createElement('td');
+  const tdAge = document.createElement('td');
+  const tdCentery = document.createElement('td');
+
+  tdName.innerText = person.name;
+  tdSex.innerText = person.sex;
+  tdBorn.innerText = person.born;
+  tdDied.innerText = person.died;
+  tdAge.innerText = person.died - person.born;
+  tdCentery.innerText = Math.ceil(person.died / 100);
+
+  table.append(wrapperTable);
+  wrapperTable.append(tdName);
+  wrapperTable.append(tdSex);
+  wrapperTable.append(tdBorn);
+  wrapperTable.append(tdDied);
+  wrapperTable.append(tdAge);
+  wrapperTable.append(tdCentery);
+});
