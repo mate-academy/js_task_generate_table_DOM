91ae0ee297a9f7723604291021f3c465cc2530b1
Kristina Bekher <bekherkr@gmail.com>
2023-10-16 21:43:29 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,24 @@
 
 const people = require('./lib/people');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableElement = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach(person => {
+  const row = tableElement.insertRow();
+
+  const nameCell = row.insertCell(0);
+  const genderCell = row.insertCell(1);
+  const bornCell = row.insertCell(2);
+  const diedCell = row.insertCell(3);
+  const ageCell = row.insertCell(4);
+  const centuryCell = row.insertCell(5);
+
+  nameCell.textContent = person.name;
+  genderCell.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  bornCell.textContent = person.born;
+  diedCell.textContent = person.died;
+  ageCell.textContent = person.died - person.born;
+  centuryCell.textContent = Math.ceil(person.died / 100);
+
+  tableElement.appendChild(row);
+});
