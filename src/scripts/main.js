f39f29862ec3328f4dc9992eb700d55aaecadd79
Kristina Bekher <bekherkr@gmail.com>
2023-10-17 17:51:58 +0200

    update solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,19 +7,18 @@ const tableElement = document.querySelector('.dashboard');
 people.forEach(person => {
   const row = tableElement.insertRow();
 
-  const nameCell = row.insertCell(0);
-  const genderCell = row.insertCell(1);
-  const bornCell = row.insertCell(2);
-  const diedCell = row.insertCell(3);
-  const ageCell = row.insertCell(4);
-  const centuryCell = row.insertCell(5);
+  const cells = [];
 
-  nameCell.textContent = person.name;
-  genderCell.textContent = person.sex === 'm' ? 'Male' : 'Female';
-  bornCell.textContent = person.born;
-  diedCell.textContent = person.died;
-  ageCell.textContent = person.died - person.born;
-  centuryCell.textContent = Math.ceil(person.died / 100);
+  [0, 1, 2, 3, 4, 5].forEach((cell) => {
+    cells.push(row.insertCell(cell));
+  });
+
+  cells[0].textContent = person.name;
+  cells[1].textContent = person.sex === 'm' ? 'Male' : 'Female';
+  cells[2].textContent = person.born;
+  cells[3].textContent = person.died;
+  cells[4].textContent = person.died - person.born;
+  cells[5].textContent = Math.ceil(person.died / 100);
 
   tableElement.appendChild(row);
 });
