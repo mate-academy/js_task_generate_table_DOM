ad2a49995bfacb10c5ceaa028476ce44ca5e31c9
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-04 11:19:29 +0100

    formatted code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,36 +5,24 @@ const tableElement = document.querySelector('.dashboard');
 
 for (const person of people) {
   const trElement = document.createElement('tr');
-
-  tableElement.append(trElement);
-
   const tdName = document.createElement('td');
-
-  tdName.textContent = `${person.name}`;
-  trElement.append(tdName);
-
   const tdGender = document.createElement('td');
-
-  tdGender.textContent = `${person.sex}`;
-  trElement.append(tdGender);
-
   const tdBorn = document.createElement('td');
-
-  tdBorn.textContent = `${person.born}`;
-  trElement.append(tdBorn);
-
   const tdDied = document.createElement('td');
-
-  tdDied.textContent = `${person.died}`;
-  trElement.append(tdDied);
-
   const tdAge = document.createElement('td');
-
-  tdAge.textContent = `${person.died - person.born}`;
-  trElement.append(tdAge);
-
   const tdCentury = document.createElement('td');
 
+  tableElement.append(trElement);
+  tdName.textContent = `${person.name}`;
+  trElement.append(tdName);
+  tdGender.textContent = `${person.sex}`;
+  trElement.append(tdGender);
+  tdBorn.textContent = `${person.born}`;
+  trElement.append(tdBorn);
+  tdDied.textContent = `${person.died}`;
+  trElement.append(tdDied);
+  tdAge.textContent = `${person.died - person.born}`;
+  trElement.append(tdAge);
   tdCentury.textContent = `${Math.ceil(person.died / 100)}`;
   trElement.append(tdCentury);
 }
