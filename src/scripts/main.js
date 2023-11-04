752dc661195f8207a859307ca89dd1109f8a433e
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-11-04 13:23:29 +0100

    sol2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,24 +5,15 @@ const tableElement = document.querySelector('.dashboard');
 
 for (const person of people) {
   const trElement = document.createElement('tr');
-  const tdName = document.createElement('td');
-  const tdGender = document.createElement('td');
-  const tdBorn = document.createElement('td');
-  const tdDied = document.createElement('td');
-  const tdAge = document.createElement('td');
-  const tdCentury = document.createElement('td');
 
   tableElement.append(trElement);
-  tdName.textContent = `${person.name}`;
-  trElement.append(tdName);
-  tdGender.textContent = `${person.sex}`;
-  trElement.append(tdGender);
-  tdBorn.textContent = `${person.born}`;
-  trElement.append(tdBorn);
-  tdDied.textContent = `${person.died}`;
-  trElement.append(tdDied);
-  tdAge.textContent = `${person.died - person.born}`;
-  trElement.append(tdAge);
-  tdCentury.textContent = `${Math.ceil(person.died / 100)}`;
-  trElement.append(tdCentury);
+
+  trElement.innerHTML = `
+  <td>${person.name}</td>
+  <td>${person.sex}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${person.died - person.born}</td>
+  <td>${Math.ceil(person.died / 100)}</td>
+  `;
 }
