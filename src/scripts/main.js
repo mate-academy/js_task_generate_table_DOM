32b9696017a2c968a6db6239f258e3a41c0d7675
Aleksandr <kartanovaleks@gmail.com>
2023-11-05 12:20:24 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,34 @@
 'use strict';
 
+const table = document.querySelector('.dashboard');
+
 const people = require('./lib/people');
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+for (const person of people) {
+  const tr = document.createElement('tr');
+
+  for (const key in person) {
+    if (key === 'fatherName' || key === 'motherName' || key === 'slug') {
+      continue;
+    }
+
+    const td = document.createElement('td');
+
+    td.textContent = person[key];
+
+    tr.append(td);
+  }
+
+  const tdWithAge = document.createElement('td');
+  const tdWithCentury = document.createElement('td');
+  const { born, died } = person;
+
+  tdWithAge.textContent = died - born;
+  tdWithCentury.textContent = Math.ceil(died / 100);
+  tr.append(tdWithAge);
+  tr.append(tdWithCentury);
+  table.append(tr);
+}
