ce29c75a10973bc5aa5a561f2bb2c956ae560da3
Damian <damian.adamczewski92@gmail.com>
2023-11-08 12:34:26 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,33 @@
 'use strict';
 
 const people = require('./lib/people');
+const tableElements = document.querySelector('.dashboard');
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const COLUMNS = {
+  NAME: 0,
+  GENDER: 1,
+  BORN: 2,
+  DIED: 3,
+  AGE: 4,
+  CENTURY: 5,
+};
+
+people.forEach(person => {
+  const row = tableElements.insertRow();
+  const cells = [];
+
+  Object.values(COLUMNS).forEach(column => {
+    cells.push(row.insertCell(column));
+  });
+
+  cells[COLUMNS.NAME].textContent = person.name;
+  cells[COLUMNS.GENDER].textContent = person.sex === 'f' ? 'Female' : 'Male';
+  cells[COLUMNS.BORN].textContent = person.born;
+  cells[COLUMNS.DIED].textContent = person.died;
+  cells[COLUMNS.AGE].textContent = person.died - person.born;
+  cells[COLUMNS.CENTURY].textContent = Math.ceil(person.died / 100);
+
+  tableElements.appendChild(row);
+});
