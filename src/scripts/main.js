a780d87340849103e87547c6f3ab44e725d0f23a
Akilover <119785153+liza-strykharchuk@users.noreply.github.com>
2023-11-20 14:41:19 +0200

    code2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,16 +2,18 @@
 
 const people = require('./lib/people');
 
-const newTable = document.querySelector(`.dashboard`);
+const newTable = document.querySelector('.dashboard');
 const newTableBody = newTable.firstElementChild;
 
 people.forEach(element => {
   newTableBody.insertAdjacentHTML('beforeend', `
-  <td>${element.name}</td>
-  <td>${element.sex === 'm' ? 'Male' : 'Female'}</td>
-  <td>${element.born}</td>
-  <td>${element.died}</td>
-  <td>${element.died - element.born}</td>
-  <td>${Math.ceil(element.died / 100)}</td>
+    <tr>
+      <td>${element.name}</td>
+      <td>${element.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${element.born}</td>
+      <td>${element.died}</td>
+      <td>${element.died - element.born}</td>
+      <td>${Math.ceil(element.died / 100)}</td>
+    </tr>
   `);
 });
