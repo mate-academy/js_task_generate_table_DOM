c2be6f2ce727d156d01289216fe15be39331ecf8
Vadim <volandemortuwka@gmail.com>
2024-05-13 12:58:20 +0300

    first save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,18 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tr = document.querySelector('tr');
 
-// write your code here
+people.forEach((person) => {
+  tr.insertAdjacentHTML(
+    'afterend',
+    `<tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'f' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>`,
+  );
+});
