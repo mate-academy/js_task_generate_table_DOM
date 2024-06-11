bf469b8381ba744bf04a83e4a86a8e38fb435908
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-11 20:48:04 +0300

    The dashboard from JSON has been generated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -353,8 +353,24 @@ const people = [
     slug: 'jacobus-bernardus-van-brussel-1736',
   },
 ];
+const dashboard = document
+  .querySelector('.dashboard')
+  .contains(document.querySelector('tbody'))
+  ? document.querySelector('tbody')
+  : document.querySelector('.dashboard');
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
-// write your code here
+people.forEach((person) => {
+  dashboard.insertAdjacentHTML(
+    'beforeend',
+    `
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>
+    `,
+  );
+});
