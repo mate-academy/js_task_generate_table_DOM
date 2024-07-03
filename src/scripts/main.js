609e65dbb8152c2660f183763557940d15ea8847
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-07-03 16:03:12 +0300

    js_task_generate_table_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,26 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const dashboard = document.querySelector('.dashboard');
+
+  for (const person of people) {
+    const columns = [
+      person.name,
+      person.sex === 'm' ? 'Male' : 'Female',
+      person.born,
+      person.died,
+      person.died - person.born,
+      Math.ceil(person.died / 100),
+    ];
+
+    const tr = dashboard.insertRow();
+
+    for (const column of columns) {
+      const td = tr.insertCell();
+
+      td.textContent = column;
+    }
+  }
+});
