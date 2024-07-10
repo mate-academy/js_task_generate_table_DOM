ad66c38ecc5a7300566472e0b968f29924640947
Artem Pinchuk <103887088+Artem193@users.noreply.github.com>
2024-07-10 23:09:55 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,22 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('table');
+const nameColumns = document.querySelectorAll('th');
+
+people.forEach((person) => {
+  const newRaw = document.createElement('tr');
+
+  person.age = person.died - person.born;
+  person.century = Math.ceil(person.died / 100);
+
+  nameColumns.forEach((nameColumn) => {
+    const newCell = document.createElement('td');
+
+    newCell.textContent = person[nameColumn.textContent.toLowerCase()];
+    newRaw.appendChild(newCell);
+  });
+  table.appendChild(newRaw);
+});
