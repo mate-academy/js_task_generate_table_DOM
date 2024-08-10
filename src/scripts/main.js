9ba7ed06b4c0cd5534c8df3568bc93650ad6f6e3
yursprava <yursprava@gmail.com>
2024-08-10 19:47:34 -0400

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,39 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+people.forEach((person) => {
+  const personRow = document.createElement('tr');
+  const table = document.querySelector('.dashboard');
 
-// write your code here
+  table.append(personRow);
+
+  const nameCell = document.createElement('td');
+
+  nameCell.innerHTML = person.name;
+  personRow.append(nameCell);
+
+  const genderCell = document.createElement('td');
+
+  genderCell.innerHTML = person.sex;
+  personRow.append(genderCell);
+
+  const bornCell = document.createElement('td');
+
+  bornCell.innerHTML = person.born;
+  personRow.append(bornCell);
+
+  const diedCell = document.createElement('td');
+
+  diedCell.innerHTML = person.died;
+  personRow.append(diedCell);
+
+  const ageCell = document.createElement('td');
+
+  ageCell.innerHTML = person.died - person.born;
+  personRow.append(ageCell);
+
+  const centuryCell = document.createElement('td');
+
+  centuryCell.innerHTML = Math.ceil(person.died / 100);
+  personRow.append(centuryCell);
+});
