6560b808bd35bc040465bf30705c8c3635681eb5
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-11 17:52:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,35 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const dashboard = document.querySelector('.dashboard');
+
+function createTableRow(person) {
+  const row = document.createElement('tr');
+
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const cells = [
+    person.name,
+    person.sex,
+    person.born,
+    person.died,
+    age,
+    century,
+  ];
+
+  cells.forEach((cellData) => {
+    const cell = document.createElement('td');
+
+    cell.textContent = cellData;
+    row.appendChild(cell);
+  });
+
+  return row;
+}
+
+people.forEach((person) => {
+  const row = createTableRow(person);
+
+  dashboard.appendChild(row);
+});
