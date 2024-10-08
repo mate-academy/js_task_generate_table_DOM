dd301e0cef2a8b9deba70a904ea9bca8a8f600ba
Igor <4430704@gmail.com>
2024-10-08 23:29:25 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,42 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('tbody');
+
+for (const person of people) {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+
+  const genderCell = document.createElement('td');
+
+  genderCell.textContent = person.sex === 'm' ? 'Male' : 'Female';
+
+  const bornCell = document.createElement('td');
+
+  bornCell.textContent = person.born;
+
+  const diedCell = document.createElement('td');
+
+  diedCell.textContent = person.died;
+
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = person.died - person.born;
+
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = Math.ceil(person.died / 100);
+
+  row.append(nameCell);
+  row.append(genderCell);
+  row.append(bornCell);
+  row.append(diedCell);
+  row.append(ageCell);
+  row.append(centuryCell);
+
+  table.append(row);
+}
