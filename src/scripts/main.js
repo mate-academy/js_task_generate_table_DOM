331bd3acdf2416669a59399b6fc2e32440ea5cf4
Volodymyr Makhonin <zavozi@gmail.com>
2025-01-13 16:09:58 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,24 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const tableBody = document.querySelector('table.dashboard tbody');
+
+const rowsHTML = people
+  .map((person) => {
+    const age = person.died - person.born; // Обчислюємо вік
+    const century = Math.ceil(person.died / 100); // Обчислюємо століття
+
+    return `
+    <tr>
+      <td>${person.name}</td>
+      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${age}</td>
+      <td>${century}</td>
+    </tr>
+  `;
+  })
+  .join('');
+
+tableBody.innerHTML += rowsHTML;
