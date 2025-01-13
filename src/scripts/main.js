bfdce772d7cdcff6dcd039c11aa4f7cad9e3d3e0
Саша <martinecsasa5@gmail.com>
2025-01-13 13:31:11 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,21 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const row = document.createElement('tr');
+
+  row.innerHTML = `
+  <td>${person.name}</td>
+  <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+  <td>${person.born}</td>
+  <td>${person.died}</td>
+  <td>${age}</td>
+  <td>${century}</td>
+  `;
+
+  table.appendChild(row);
+});
