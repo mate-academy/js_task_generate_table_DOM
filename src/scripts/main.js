26bd1a3dcc8d0d4dacaf8edd68b7aecfdcdd5895
Yulian47 <78698459+Yulian47@users.noreply.github.com>
2025-02-19 12:50:46 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,48 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboardTable = document.querySelector('.dashboard');
 
-// write your code here
+function createTableRow(person) {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+  row.appendChild(nameCell);
+
+  const genderCell = document.createElement('td');
+
+  genderCell.textContent = person.gender;
+  row.appendChild(genderCell);
+
+  const bornCell = document.createElement('td');
+
+  bornCell.textContent = person.born;
+  row.appendChild(bornCell);
+
+  const diedCell = document.createElement('td');
+
+  diedCell.textContent = person.died;
+  row.appendChild(diedCell);
+
+  const age = person.died - person.born;
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = age;
+  row.appendChild(ageCell);
+
+  const century = Math.ceil(person.died / 100);
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = century;
+  row.appendChild(centuryCell);
+
+  return row;
+}
+
+people.forEach((person) => {
+  const row = createTableRow(person);
+
+  dashboardTable.appendChild(row);
+});
