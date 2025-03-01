935c34ff7a71ba4d5d55d87adb1358519699cfdd
Owk <kkoctuk2005@gmail.com>
2025-03-01 20:07:51 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,24 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach((person, index) => {
+  const row = table.insertRow(index);
+
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  const cell4 = row.insertCell(3);
+  const cell5 = row.insertCell(4);
+  const cell6 = row.insertCell(5);
+
+  cell1.innerHTML = person.name;
+  cell2.innerHTML = person.sex;
+  cell3.innerHTML = person.born;
+  cell4.innerHTML = person.died;
+  cell5.innerHTML = person.died - person.born;
+  cell6.innerHTML = Math.ceil(person.died / 100);
+
+  table.appendChild(row);
+});
