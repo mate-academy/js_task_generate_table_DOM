94a47d76137c9e44d28f41973edddeaacd542bf4
Mariia Koval <m.kovalmaria@gmail.com>
2025-03-05 19:13:41 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
 const people = [
@@ -355,6 +356,30 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+  const sex = person.sex === 'm' ? 'Male' : 'Female';
+
+  const data = [
+    person.name,
+    sex,
+    person.born,
+    person.died,
+    age,
+    century,
+  ];
+
+  data.forEach((el) => {
+    const td = document.createElement('td');
+
+    td.textContent = el;
+    row.appendChild(td);
+  });
+
+  table.appendChild(row);
+});
