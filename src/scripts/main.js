60fafb738f5528546292fa9d2ee68a336eeab507
nadia vlasenko <nadiia@MacBook-Air-nadia.local>
2025-03-07 12:30:50 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,45 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+document.addEventListener('DOMContentLoaded', () => {
+  const table = document.querySelector('.dashboard');
 
-// write your code here
+  people.forEach((person) => {
+    const row = document.createElement('tr');
+
+    const nameCell = document.createElement('td');
+
+    nameCell.textContent = person.name;
+    row.appendChild(nameCell);
+
+    const genderCell = document.createElement('td');
+
+    genderCell.textContent = person.gender;
+    row.appendChild(genderCell);
+
+    const bornCell = document.createElement('td');
+
+    bornCell.textContent = person.born;
+    row.appendChild(bornCell);
+
+    const diedCell = document.createElement('td');
+
+    diedCell.textContent = person.died;
+    row.appendChild(diedCell);
+
+    const ageCell = document.createElement('td');
+
+    const age = person.died - person.born;
+
+    ageCell.textContent = age;
+    row.appendChild(ageCell);
+
+    const centuryCell = document.createElement('td');
+    const century = Math.ceil(person.died / 100);
+
+    centuryCell.textContent = century;
+    row.appendChild(centuryCell);
+
+    table.appendChild(row);
+  });
+});
