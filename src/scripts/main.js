8364a4d7c3d49d2fb0f35e06dea2db3c7467e310
Dmytro Kolesnikov <dimon4ik1231731@gmail.com>
2025-04-01 19:15:47 +0300

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,28 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+document.addEventListener('DOMContentLoaded', () => {
+  for (const person of people) {
+    const table = document.querySelector('#dashboard');
 
-// write your code here
+    const tr = document.createElement('tr');
+
+    const tdName = document.createElement('td');
+    const tdSex = document.createElement('td');
+    const tdBorn = document.createElement('td');
+    const tdDied = document.createElement('td');
+    const tdAge = document.createElement('td');
+    const tdCentury = document.createElement('td');
+
+    tdName.textContent = person.name;
+    tdSex.textContent = person.sex;
+    tdBorn.textContent = person.born;
+    tdDied.textContent = person.died;
+    tdAge.textContent = person.died - person.born;
+    tdCentury.textContent = Math.ceil(person.died / 100);
+
+    tr.append(tdName, tdSex, tdBorn, tdDied, tdAge, tdCentury);
+
+    table.appendChild(tr);
+  }
+});
