e027020ccf9fbef2229131cb2037b49bf27ef9ab
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-04-01 22:19:25 +0300

    add Generated dashboard from JSON

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,29 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard tbody');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+  const fullName = document.createElement('td');
+  const gender = document.createElement('td');
+  const born = document.createElement('td');
+  const died = document.createElement('td');
+  const age = document.createElement('td');
+  const century = document.createElement('td');
+
+  fullName.textContent = person.name;
+  gender.textContent = person.sex === 'f' ? 'Female' : 'Male';
+  born.textContent = person.born;
+  died.textContent = person.died;
+  age.textContent = person.died - person.born;
+  century.textContent = Math.ceil(person.died / 100);
+
+  row.appendChild(fullName);
+  row.appendChild(gender);
+  row.appendChild(born);
+  row.appendChild(died);
+  row.appendChild(age);
+  row.appendChild(century);
+  table.appendChild(row);
+});
