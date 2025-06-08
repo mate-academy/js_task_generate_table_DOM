cc73832e04ce79474845597752ba4652aa3e3905
Yura <yurii.st11@gmail.com>
2025-06-08 15:46:23 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,31 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+
+  const persName = document.createElement('td');
+  const gender = document.createElement('td');
+  const born = document.createElement('td');
+  const died = document.createElement('td');
+  const age = document.createElement('td');
+  const century = document.createElement('td');
+
+  persName.textContent = person.name;
+  gender.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  born.textContent = person.born;
+  died.textContent = person.died;
+  age.textContent = person.died - person.born;
+  century.textContent = Math.ceil(person.died / 100);
+
+  row.appendChild(persName);
+  row.appendChild(gender);
+  row.appendChild(born);
+  row.appendChild(died);
+  row.appendChild(age);
+  row.appendChild(century);
+
+  table.appendChild(row);
+});
