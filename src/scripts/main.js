3eb472262241037af44e4d8d75457469b4d65ca4
Rustam Herheman <gergemannn@gmail.com>
2025-06-16 20:15:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,43 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+
+function createRow(person) {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+
+  const genderCell = document.createElement('td');
+
+  genderCell.textContent = person.sex === 'm' ? 'male' : 'female';
+
+  const bornCell = document.createElement('td');
+
+  bornCell.textContent = person.born;
+
+  const diedCell = document.createElement('td');
+
+  diedCell.textContent = person.died;
+
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = person.died - person.born;
+
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = Math.ceil(person.died / 100);
+
+  row.append(nameCell, genderCell, bornCell, diedCell, ageCell, centuryCell);
+
+  return row;
+}
+
+const table = document.querySelector('.dashboard');
+
+for (const person of people) {
+  const row = createRow(person);
+
+  table.appendChild(row);
+}
