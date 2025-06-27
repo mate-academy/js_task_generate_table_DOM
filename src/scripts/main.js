3ea17cdc38f1ec8eec1b37990c0cfef7014e8d7e
Alina Yamchuk <alina.utc@gmail.com>
2025-06-27 15:16:45 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,34 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+import people from './lib/people.json';
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach(person => {
+const row = document.createElement('tr');
+
+const tdName = document.createElement('td');
+tdName.textContent = person.name;
+
+const tdGender = document.createElement('td');
+tdGender.textContent = person.sex === 'm' ? 'Male' : 'Female';
+
+const tdBorn = document.createElement('td');
+tdBorn.textContent = person.born;
+
+const tdDied = document.createElement('td');
+tdDied.textContent = person.died;
+
+const tdAge = document.createElement('td');
+tdAge.textContent = person.died - person.born;
+
+const tdCentury = document.createElement('td');
+tdCentury.textContent = Math.ceil(person.died / 100);
+
+
+row.append(tdName, tdGender, tdBorn, tdDied, tdAge, tdCentury);
+table.appendChild(row);
+});
+
+
