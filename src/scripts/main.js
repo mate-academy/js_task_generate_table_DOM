75b3f71e9ca7e21d43a3d84df7ba60675ad00823
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-07-03 17:08:35 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,23 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableElement = document.querySelector('.dashboard');
 
-// write your code here
+function creatRow(person) {
+  const tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <td>${person.name}</td>
+    <td>${person.sex.toUpperCase()}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${person.died - person.born}</td>
+    <td>${Math.ceil(person.died / 100)}</td>
+  `;
+
+  return tr;
+}
+
+people.forEach((person) => {
+  tableElement.append(creatRow(person));
+});
