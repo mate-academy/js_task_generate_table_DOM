93b5ea123b50a7f56fff758d603265db1bbf98a6
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-07 16:29:27 +0200

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,21 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const dashboard = document.querySelector('.dashboard');
+const tbody = dashboard.querySelector('tbody');
 
-// write your code here
+people.forEach((person) => {
+  tbody.insertAdjacentHTML(
+    'beforeend',
+    `
+      <tr>
+        <td>${person.name}</td>
+        <td>${person.sex === 'm' ? 'Male' : person.sex === 'f' ? 'Female' : ''}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+      </tr>
+    `,
+  );
+});
