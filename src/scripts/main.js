234c95dd501514a3a271ebc1d21444e96190b1d0
Matvey Turluk <matychencia.ua@gmail.com>
2025-08-18 10:31:45 +0300

    problems with radius

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,54 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+function sortedPeople(humans) {
+  for (const person of humans) {
+    const age = person.died - person.born;
+    const century = Math.ceil(person.died / 100);
+
+    person.age = age;
+    person.century = century;
+  }
+
+  return people.sort((person1, person2) => person2.died - person1.died);
+}
+
+sortedPeople(people);
+
+function createPeopleInfoRows(humans) {
+  for (const human of humans) {
+    const row = document.createElement('tr');
+
+    const nameCell = document.createElement('th');
+
+    nameCell.textContent = human.name;
+
+    const gender = document.createElement('th');
+
+    gender.textContent = human.sex;
+
+    const born = document.createElement('th');
+
+    born.textContent = human.born;
+
+    const died = document.createElement('th');
+
+    died.textContent = human.died;
+
+    const age = document.createElement('th');
+
+    age.textContent = human.age;
+
+    const century = document.createElement('th');
+
+    century.textContent = human.century;
+
+    row.append(nameCell, gender, born, died, age, century);
+
+    table.appendChild(row);
+  }
+}
+
+createPeopleInfoRows(people);
