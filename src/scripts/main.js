446931c00da59a0530354b03612a8d8d31f4134b
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-08-31 15:50:46 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,40 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+if (table) {
+  people.forEach((person) => {
+    const row = document.createElement('tr');
+    const nameCell = document.createElement('td');
+
+    nameCell.textContent = person.name;
+    row.appendChild(nameCell);
+
+    const genderCell = document.createElement('td');
+
+    genderCell.textContent = person.sex === 'm' ? 'male' : 'female';
+    row.appendChild(genderCell);
+
+    const bornCell = document.createElement('td');
+
+    bornCell.textContent = person.born;
+    row.appendChild(bornCell);
+
+    const diedCell = document.createElement('td');
+
+    diedCell.textContent = person.died;
+    row.appendChild(diedCell);
+
+    const ageCell = document.createElement('td');
+
+    ageCell.textContent = person.died - person.born;
+    row.appendChild(ageCell);
+
+    const centuryCell = document.createElement('td');
+
+    centuryCell.textContent = Math.ceil(person.died / 100);
+    row.appendChild(centuryCell);
+    table.appendChild(row);
+  });
+}
