d748c2df57a4ab99cc69c0f4b839eee84fc55e19
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-08 20:39:55 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,27 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const finder = document.querySelector('.dashboard');
 
-// write your code here
+function createTableRows(peoples) {
+  return peoples
+    .map((person) => {
+      return `
+      <tr>
+        <td>${person.name}</td>
+        <td>${person.sex === 'm' ? 'Male' : person.sex === 'f' ? 'Female' : person.sex}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+      </tr>
+    `;
+    })
+    .join('');
+}
+
+const adder = finder ? finder.querySelector('tbody') : null;
+
+if (adder) {
+  adder.insertAdjacentHTML('beforeend', createTableRows(people));
+}
