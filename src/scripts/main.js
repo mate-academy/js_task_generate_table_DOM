0e607dc0f00dc2863e4108a53a943821b286c590
Iryna <Yurchak.Iryna2017@gmail.com>
2025-09-25 13:50:01 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,47 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+function GenerateDashboard() {
+  people.forEach((person) => {
+    const age = person.died - person.born;
+    const century = Math.ceil(person.died / 100);
+
+    const row = document.createElement('tr');
+
+    const nameCell = document.createElement('td');
+
+    nameCell.textContent = person.name;
+    row.appendChild(nameCell);
+
+    const sexCell = document.createElement('td');
+
+    sexCell.textContent = person.sex;
+    row.appendChild(sexCell);
+
+    const bornCell = document.createElement('td');
+
+    bornCell.textContent = person.born;
+    row.appendChild(bornCell);
+
+    const diedCell = document.createElement('td');
+
+    diedCell.textContent = person.died;
+    row.appendChild(diedCell);
+
+    const ageCell = document.createElement('td');
+
+    ageCell.textContent = age;
+    row.appendChild(ageCell);
+
+    const centuryCell = document.createElement('td');
+
+    centuryCell.textContent = century;
+    row.appendChild(centuryCell);
+
+    table.appendChild(row);
+  });
+}
+
+GenerateDashboard();
