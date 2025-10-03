657a50e7dec170d275e8cbcacb53adbc9251bd13
Yaroslav Molchanov <y.molchanoff@gmail.com>
2025-10-03 12:35:53 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,47 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+
+  // name
+  const nameCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+  row.appendChild(nameCell);
+
+  // gender
+  const genderCell = document.createElement('td');
+
+  genderCell.textContent = person.sex;
+  row.appendChild(genderCell);
+
+  // born
+  const bornCell = document.createElement('td');
+
+  bornCell.textContent = person.born;
+  row.appendChild(bornCell);
+
+  // died
+  const diedCell = document.createElement('td');
+
+  diedCell.textContent = person.died;
+  row.appendChild(diedCell);
+
+  // age = died - born
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = person.died - person.born;
+  row.appendChild(ageCell);
+
+  // century = ceil(died / 100)
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = Math.ceil(person.died / 100);
+  row.appendChild(centuryCell);
+
+  // append row to table
+  table.appendChild(row);
+});
