41c07e3997da8f5f98f79ac793b66303cca67f32
Iryna Knyzh <iryna.knyzh@gmail.com>
2025-10-23 14:31:25 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,23 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('table.dashboard');
+
+people.forEach((person) => {
+  const tr = document.createElement('tr');
+
+  const arr = [
+    person.name,
+    person.sex,
+    person.born,
+    person.died,
+    person.died - person.born,
+    Math.ceil(person.died / 100),
+  ];
+
+  arr.map((x) => {
+    tr.append(document.createElement('td').textContent(x));
+  });
+
+  table.append(tr);
+});
