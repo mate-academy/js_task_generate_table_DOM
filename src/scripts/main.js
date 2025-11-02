b90a7982a0c0e84381e204b6c91b63a131d81175
Anastasia <ahinata1717@gmail.com>
2025-11-02 15:59:13 +0200

    Implement table rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,26 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach((person) => {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+  const genderCell = document.createElement('td');
+  const bornCell = document.createElement('td');
+  const diedCell = document.createElement('td');
+  const ageCell = document.createElement('td');
+  const centuryCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+  genderCell.textContent = person.sex;
+  bornCell.textContent = person.born;
+  diedCell.textContent = person.died;
+  ageCell.textContent = person.died - person.born;
+  centuryCell.textContent = Math.ceil(person.died / 100);
+
+  row.append(nameCell, genderCell, bornCell, diedCell, ageCell, centuryCell);
+
+  table.appendChild(row);
+});
