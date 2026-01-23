fa1f02834e8fb0ed4bcb42be42e80051b524fa7e
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2026-01-23 21:29:08 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -361,16 +361,17 @@ const dashboard = document.querySelector('.dashboard');
 
 for (const person of people) {
   const tr = document.createElement('tr');
+  let sex = person.sex;
 
-  if (person.sex === 'f') {
-    person.sex = 'Female';
+  if (sex === 'f') {
+    sex = 'Female';
   } else {
-    person.sex = 'Male';
+    sex = 'Male';
   }
 
   const values = [
     person.name,
-    person.sex,
+    sex,
     person.born,
     person.died,
     person.died - person.born,
