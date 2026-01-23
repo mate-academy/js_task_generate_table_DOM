02532a27e745a69208bd24c814ca8c4db2fed4a8
Oleksii <olexijukraina@gmail.com>
2026-01-23 16:08:27 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,23 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+people.forEach((person) => {
+  const table = document.querySelector('.dashboard');
+  const row = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  // Перетворення статі
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+
+  const values = [person.name, gender, person.born, person.died, age, century];
+
+  values.forEach((value) => {
+    const cell = document.createElement('td');
+
+    cell.textContent = value;
+    row.appendChild(cell);
+  });
+
+  table.appendChild(row);
+});
