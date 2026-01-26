5181b02493dc4b9399ceaca99af3aa93dc6de3e7
Olia Shymanska <oliaa.shymanska@gmail.com>
2026-01-26 19:56:54 +0200

    convert array people to table rows

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,26 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableDashboard = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach((person) => {
+  const rowValue = document.createElement('tr');
+
+  const dataValues = [
+    person.name,
+    person.sex === 'm' ? 'Male' : 'Female',
+    person.born,
+    person.died,
+    person.died - person.born,
+    Math.ceil(person.died / 100),
+  ];
+
+  dataValues.forEach((value) => {
+    const td = document.createElement('td');
+
+    td.textContent = value;
+    rowValue.append(td);
+  });
+
+  tableDashboard.append(rowValue);
+});
