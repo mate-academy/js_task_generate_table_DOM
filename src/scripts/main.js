6f1b4e181b31190c1e02b2f0fddc8ac5be421ac2
Anastasiia Tkachenko <nastya240509@gmail.com>
2026-02-04 20:19:01 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,28 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const cells = [
+    person.name,
+    person.sex,
+    person.born,
+    person.died,
+    age,
+    century,
+  ];
+
+  cells.forEach((value) => {
+    const cell = document.createElement('td');
+
+    cell.textContent = value;
+    row.appendChild(cell);
+  });
+
+  table.appendChild(row);
+});
