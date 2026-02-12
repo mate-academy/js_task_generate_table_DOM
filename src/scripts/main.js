475f954be1c14fd5bd30c2df91c452142bf41a9c
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-02-12 22:26:12 +0200

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,26 @@ import people from '../data/people.json';
 
 const table = document.querySelector('.dashboard');
 
-if (!table) {
-  throw new Error('Table dashboard not found');
+createAndFillTable();
+
+function createAndFillTable() {
+  if (!table) {
+    console.error('Table .dashboard not found');
+    return;
+  }
+
+  const tBody =
+    table.tBodies[0] || table.appendChild(document.createElement('tbody'));
+
+  people.forEach((person) => {
+    const row = tBody.insertRow();
+
+    row.insertCell().textContent = person.name;
+    row.insertCell().textContent =
+      person.sex === 'f' ? 'Female' : person.sex === 'm' ? 'Male' : 'Unknown';
+    row.insertCell().textContent = person.born;
+    row.insertCell().textContent = person.died;
+    row.insertCell().textContent = person.died - person.born;
+    row.insertCell().textContent = Math.ceil(person.died / 100);
+  });
 }
-
-const tBody =
-  table.tBodies[0] || table.appendChild(document.createElement('tBody'));
-
-people.forEach((person) => {
-  const row = tBody.insertRow();
-
-  row.insertCell().textContent = person.name;
-  row.insertCell().textContent = person.sex === 'f' ? 'Female' : 'Male';
-  row.insertCell().textContent = person.born;
-  row.insertCell().textContent = person.died;
-  row.insertCell().textContent = person.died - person.born;
-  row.insertCell().textContent = Math.ceil(person.died / 100);
-});
