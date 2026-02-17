91130ff16af8b88c91da6e4f32478c52b55fe0e0
Volodymyr Shuper <vshuper@gmail.com>
2026-02-17 16:42:18 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,6 +354,43 @@ const people = [
   },
 ];
 
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const tr = document.createElement('tr');
+
+  const tdName = document.createElement('td');
+
+  tdName.textContent = person.name;
+  tr.appendChild(tdName);
+
+  const tdSex = document.createElement('td');
+
+  tdSex.textContent = person.sex;
+  tr.appendChild(tdSex);
+
+  const tdBorn = document.createElement('td');
+
+  tdBorn.textContent = person.born;
+  tr.appendChild(tdBorn);
+
+  const tdDied = document.createElement('td');
+
+  tdDied.textContent = person.died;
+  tr.appendChild(tdDied);
+
+  const tdAge = document.createElement('td');
+
+  tdAge.textContent = person.died - person.born;
+  tr.appendChild(tdAge);
+
+  const tdCentury = document.createElement('td');
+
+  tdCentury.textContent = Math.ceil(person.died / 100);
+  tr.appendChild(tdCentury);
+
+  table.appendChild(tr);
+});
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
