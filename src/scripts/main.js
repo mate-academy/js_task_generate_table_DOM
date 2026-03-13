e2eaba964b1371ad7137d2e836a4881f04e01b15
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-03-13 12:29:02 +0200

    Solution 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,21 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+import people from './people.json';
+
+const table = document.querySelector('.dashboard');
+people.forEach((person) => {
+  const row = document.createElement('tr');
+  const age = person.died - person.born;
+  const centuey = Math.ceil(person.died / 100);
+
+  row.innerHTML = `
+     <td>${person.name}</td>
+    <td>${person.sex}</td>
+    <td>${person.born}</td>
+    <td>${person.died}</td>
+    <td>${age}</td>
+    <td>${century}</td>
+  `;
+  table.appendChild(row);
+});
