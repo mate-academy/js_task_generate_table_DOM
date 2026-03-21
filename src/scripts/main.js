2d97d017844d4225cba7b27b932ae6fad00c3680
Yurii Leniv <supusman32@gmail.com>
2026-03-21 22:08:54 +0200

    dsfsdgsd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -356,5 +356,37 @@ const people = [
 
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
+let node = document.querySelector('.dashboard');
 
-// write your code here
+people.forEach((peop) => {
+  let tr = document.createElement('tr');
+  let td1 =  document.createElement('td');
+  td1.textContent = peop.name;
+  tr.append(td1);
+
+  let td2 =  document.createElement('td');
+  if (peop.sex === 'm') {
+    td2.textContent = 'Male';
+  } else {
+    td2.textContent = 'Female';
+  }
+  tr.append(td2)
+
+  let td3 =  document.createElement('td');
+  td3.textContent = peop.born;
+  tr.append(td3);
+
+  let td4 =  document.createElement('td');
+  td4.textContent = peop.died;
+  tr.append(td4);
+
+  let td5 =  document.createElement('td');
+  td5.textContent = peop.died - peop.born;
+  tr.append(td5);
+
+  let td6 =  document.createElement('td');
+  td6.textContent = Math.ceil(peop.died / 100);
+  tr.append(td6);
+
+  node.append(tr);
+});
