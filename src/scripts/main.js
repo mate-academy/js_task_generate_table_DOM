f5de77ca4d74ecf34a0760605b714b0bd0a92321
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-04 23:23:49 +0200

    generate table dom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,32 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+// you can remove it
 
 // write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  const row = document.createElement('tr');
+
+  const data = [
+    person.name,
+    person.sex,
+    person.born,
+    person.died,
+    age,
+    century,
+  ];
+
+  data.forEach((value) => {
+    const cell = document.createElement('td');
+
+    cell.textContent = value;
+    row.appendChild(cell);
+  });
+
+  table.appendChild(row);
+});
