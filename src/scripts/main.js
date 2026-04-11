3e611e39c806032baee6ca082e6fc428cfb232a4
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-04-11 23:03:28 +0300

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,32 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const table = document.querySelector('table');
 
-// write your code here
+for (const person of people) {
+  const row = document.createElement('tr');
+  const cell1 = document.createElement('td');
+  const cell2 = document.createElement('td');
+  const cell3 = document.createElement('td');
+  const cell4 = document.createElement('td');
+  const cell5 = document.createElement('td');
+  const cell6 = document.createElement('td');
+
+  const age = person.died - person.born;
+  const century = Math.ceil(person.died / 100);
+
+  if (person.sex === 'm') {
+    cell2.textContent = 'Male';
+  } else {
+    cell2.textContent = 'Female';
+  }
+
+  cell1.textContent = person.name;
+  cell3.textContent = person.born;
+  cell4.textContent = person.died;
+  cell5.textContent = age;
+  cell6.textContent = century;
+
+  row.append(cell1, cell2, cell3, cell4, cell5, cell6);
+  table.append(row);
+}
