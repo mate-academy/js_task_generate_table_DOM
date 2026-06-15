ca7e8b87db0e511abda00745db7f865c136d43ac
Tendeeer <leifisisidj747@gmail.com>
2026-06-15 17:37:37 +0300

    create dashboard

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,21 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+  const gender = person.sex === 'm' ? 'Male' : 'Female';
+
+  row.innerHTML = `
+  <td> ${person.name} </td>
+  <td> ${gender} </td>
+  <td> ${person.born} </td>
+  <td> ${person.died} </td>
+  <td> ${person.died - person.born} </td>
+  <td> ${Math.ceil(person.died / 100)} </td>
+  `;
+
+  table.append(row);
+});
