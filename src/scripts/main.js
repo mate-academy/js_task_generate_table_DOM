31ecbaa92628dfb0fe7e3162a2c67ab37f9b197f
Victoria Neustroeva <neustrvk@gmail.com>
2026-06-16 11:43:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,36 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard tbody');
+
+people.forEach((person) => {
+  const tr = document.createElement('tr');
+
+  const values = [
+    person.name,
+    genderConvert(person.sex),
+    person.born,
+    person.died,
+    person.died - person.born,
+    Math.ceil(person.died / 100),
+  ];
+
+  values.forEach((value) => {
+    const td = document.createElement('td');
+
+    td.textContent = value;
+
+    tr.append(td);
+  });
+
+  table.append(tr);
+});
+
+function genderConvert(sex) {
+  const genders = {
+    f: 'Female',
+    m: 'Male',
+  };
+
+  return genders[sex];
+}
