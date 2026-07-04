4d775949ad4ab1aede2d9810021773a11c709cd9
student1232354 <tarasromanenko33@gmail.com>
2026-07-04 12:51:31 +0300

    first update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -355,6 +355,40 @@ const people = [
 ];
 
 // eslint-disable-next-line no-console
-console.log(people); // you can remove it
+
 
 // write your code here
+//people масив об'єктів
+
+const table = document.querySelector('.dashboard');
+
+people.forEach(function(eachobj) {
+
+  const newtr = document.createElement('tr');
+
+  const maling = eachobj.sex === 'm' ? 'Male' : (eachobj.sex === 'f' ? 'Female' : eachobj.sex);
+  const age = String(eachobj.died - eachobj.born);
+  const centuria = String(Math.ceil(eachobj.died / 100));
+
+  const normalobj = {
+    Name: eachobj.name,
+    sex: maling,
+    born: eachobj.born,
+    died: eachobj.died,
+    age: age,
+    century: centuria,
+
+  };
+
+  for (const key in normalobj) {
+
+    const current = normalobj[key];
+    const newtd = document.createElement('td');
+    newtd.textContent = current;
+    newtr.append(newtd);
+  }
+
+  table.append(newtr);
+
+});
+
