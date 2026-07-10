ac4a44065682846a32c497996b6578f372589933
Olga Bilokur <olyanderbilokur@gmail.com>
2026-07-10 21:32:26 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,26 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const dashboard = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const tr = document.createElement('tr');
+
+  const values = [
+    person.name,
+    person.sex === 'm' ? 'Male' : 'Female',
+    person.born,
+    person.died,
+    person.died - person.born,
+    Math.ceil(person.died / 100),
+  ];
+
+  values.forEach((value) => {
+    const td = document.createElement('td');
+
+    tr.appendChild(td);
+    td.textContent = value;
+  });
+
+  dashboard.append(tr);
+});
