b43451b652d8ab364b97e607fafb572c5d7d0d09
Vladyslav <vladislavkukhar66@gmail.com>
2026-07-14 19:54:38 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,46 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+people.forEach((person) => {
+  const table = document.querySelector('.dashboard');
+  const tableRow = document.createElement('tr');
+
+  const personName = document.createElement('td');
+
+  personName.textContent = person.name;
+
+  const personGender = document.createElement('td');
+
+  if (person.sex === 'm') {
+    personGender.textContent = 'Male';
+  } else {
+    personGender.textContent = 'Female';
+  }
+
+  const personBorn = document.createElement('td');
+
+  personBorn.textContent = person.born;
+
+  const personDied = document.createElement('td');
+
+  personDied.textContent = person.died;
+
+  const personAge = document.createElement('td');
+
+  personAge.textContent = person.died - person.born;
+
+  const personCentury = document.createElement('td');
+
+  personCentury.textContent = Math.ceil(person.died / 100);
+
+  tableRow.append(
+    personName,
+    personGender,
+    personBorn,
+    personDied,
+    personAge,
+    personCentury,
+  );
+
+  table.append(tableRow);
+});
