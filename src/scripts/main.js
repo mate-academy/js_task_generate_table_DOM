ba5c92bf3e98541e4b1b5bb170c95bcaf9948b8a
Alexander Okhotnychenko <shashaohota@gmail.com>
2026-07-30 12:47:45 +0300

    solution: generate table DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,30 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+function generateTable(dashboard) {
+  const table = document.querySelector(dashboard);
+
+  people.forEach((person) => {
+    const tr = document.createElement('tr');
+    const data = [
+      person?.name || null,
+      person?.sex.toUpperCase() || null,
+      person?.born || 0,
+      person?.died || 0,
+      person?.died - person?.born || 0,
+      Math.ceil(person?.died / 100) || 0,
+    ];
+
+    data.forEach((value) => {
+      const td = document.createElement('td');
+
+      td.textContent = value;
+
+      tr.append(td);
+    });
+
+    table.append(tr);
+  });
+}
+
+generateTable('.dashboard');
