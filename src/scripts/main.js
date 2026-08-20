2368b40800f5b5dc3106d0188b5a057295c06a40
Vitalii <vitaliy.doronin4@gmail.com>
2026-08-20 14:41:05 +0300

    Add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,29 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+function fillUpTable(arr) {
+  arr.forEach((item) => {
+    const row = document.createElement('tr');
+    const tdName = document.createElement('td');
+    const tdGender = document.createElement('td');
+    const tdBorn = document.createElement('td');
+    const tdDied = document.createElement('td');
+    const tdAge = document.createElement('td');
+    const tdCentury = document.createElement('td');
+
+    tdName.textContent = item.name;
+    tdGender.textContent = item.sex === 'm' ? 'Male' : 'Female';
+    tdBorn.textContent = item.born;
+    tdDied.textContent = item.died;
+    tdAge.textContent = item.died - item.born;
+    tdCentury.textContent = Math.ceil(item.died / 100);
+
+    row.append(tdName, tdGender, tdBorn, tdDied, tdAge, tdCentury);
+
+    table.appendChild(row);
+  });
+}
+
+fillUpTable(people);
