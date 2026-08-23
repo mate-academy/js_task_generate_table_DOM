4b869bd4a7cd5005e8a774436a743f90bc88ca7c
Sumar Illya <illasumar002@gmail.com>
2026-08-23 22:50:21 +0300

    Tasks complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,7 +357,7 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-const table = document.querySelector('table');
+const table = document.querySelector('.dashboard');
 
 people.forEach((person) => {
   const newRow = table.insertRow(-1);
