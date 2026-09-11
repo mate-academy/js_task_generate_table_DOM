c9f004eeb0ef0d3d19995abf82628ac6873839c0
unknown <marian.fedun3423@gmail.com>
2026-09-11 17:31:54 +0200

    added new task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,4 +357,47 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-// write your code here
+const table = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+  const cell = document.createElement('td');
+
+  cell.textContent = person.name;
+  row.append(cell);
+  table.append(row);
+
+  const genderCell = document.createElement('td');
+
+  if (person.sex === 'm') {
+    genderCell.textContent = 'Male';
+  } else {
+    genderCell.textContent = 'Female';
+  }
+  row.append(genderCell);
+  table.append(row);
+
+  const bornCell = document.createElement('td');
+
+  bornCell.textContent = person.born;
+  row.append(bornCell);
+  table.append(row);
+
+  const diedCell = document.createElement('td');
+
+  diedCell.textContent = person.died;
+  row.append(diedCell);
+  table.append(row);
+
+  const ageCell = document.createElement('td');
+
+  ageCell.textContent = person.died - person.born;
+  row.append(ageCell);
+  table.append(row);
+
+  const centuryCell = document.createElement('td');
+
+  centuryCell.textContent = Math.ceil(person.died / 100);
+  row.append(centuryCell);
+  table.append(row);
+});
