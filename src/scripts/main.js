3efaaa716afaa069906ab8dbf1f116ebd1b15448
Max <143881777+ZloyHoma@users.noreply.github.com>
2026-09-13 12:56:33 +0300

    *

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -358,3 +358,34 @@ const people = [
 console.log(people); // you can remove it
 
 // write your code here
+
+const dashboard = document.querySelector('.dashboard');
+
+people.forEach((person) => {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+  const genderCell = document.createElement('td');
+  const bornCell = document.createElement('td');
+  const diedCell = document.createElement('td');
+  const ageCell = document.createElement('td');
+  const centuryCell = document.createElement('td');
+
+  nameCell.textContent = person.name;
+  genderCell.textContent = person.sex === 'm' ? 'Male' : 'Female';
+  bornCell.textContent = person.born;
+  diedCell.textContent = person.died;
+  ageCell.textContent = person.died - person.born;
+  centuryCell.textContent = Math.ceil(person.died / 100);
+
+  row.append(
+    nameCell,
+    genderCell,
+    bornCell,
+    diedCell,
+    ageCell,
+    centuryCell,
+  );
+
+  dashboard.append(row);
+});
